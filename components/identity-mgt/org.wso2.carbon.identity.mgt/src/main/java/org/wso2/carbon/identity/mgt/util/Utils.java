b0da9153cec0d0f45cafa2e7ef24fd384233654f
godwin <godwinamila@gmail.com>
2016-03-17 00:15:52 +0530

    Remove identity provider dependency

--- components/identity-mgt/org.wso2.carbon.identity.mgt/src/main/java/org/wso2/carbon/identity/mgt/util/Utils.java
+++ components/identity-mgt/org.wso2.carbon.identity.mgt/src/main/java/org/wso2/carbon/identity/mgt/util/Utils.java
@@ -19,7 +19,6 @@
 package org.wso2.carbon.identity.mgt.util;
 
 import org.apache.axiom.om.util.Base64;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.neethi.Policy;
