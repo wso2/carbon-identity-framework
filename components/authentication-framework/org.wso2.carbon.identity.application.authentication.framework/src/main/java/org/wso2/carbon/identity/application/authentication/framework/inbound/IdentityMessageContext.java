5c907379bb5832b1028554648f4d3d98bdae9934
Johann Dilantha Nallathamby <johann@Johanns-MacBook-Pro.local>
2016-06-09 11:13:22 +0100

    Fixing comilation failure in 5.2.0-refactoring

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/inbound/IdentityMessageContext.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/inbound/IdentityMessageContext.java
@@ -24,20 +24,20 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class IdentityMessageContext<T1 extends Serializable, T2 extends Serializable> extends MessageContext
+public class IdentityMessageContext<T1 extends Serializable, T2 extends Serializable> extends MessageContext<T1,T2>
         implements Serializable {
 
     private static final long serialVersionUID = 104614801932285909L;
 
 	protected IdentityRequest request;
-    protected Map<T1,T2> parameters = new HashMap<>();
 
-    public IdentityMessageContext(IdentityRequest request, Map<T1, T2> parameters) {
+    public IdentityMessageContext(IdentityRequest request, Map<T1,T2> parameters) {
         super(parameters);
         this.request = request;
     }
 
     public IdentityMessageContext(IdentityRequest request) {
+        super(new HashMap<T1,T2>());
         this.request = request;
     }
 
