1a245b8abc58d8b98162e1bcd897a76218ad84e7
Darshana Gunawardana <darshanagunawardana@DarRays-MacBook-Pro.local>
2016-06-22 23:56:18 +0530

    Adding identity claim metadata management component

---
+++ components/claim-mgt/org.wso2.carbon.identity.claim.metadata.mgt/src/main/java/org/wso2/carbon/identity/claim/metadata/mgt/dto/AttributeMappingDTO.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.identity.claim.metadata.mgt.dto;
+
+/**
+ * Data transfer representation for a attribute mapping of a local claim.
+ */
+public class AttributeMappingDTO {
+    private String userStoreDomain;
+    private String attributeName;
+
+    public String getUserStoreDomain() {
+        return userStoreDomain;
+    }
+
+    public void setUserStoreDomain(String userStoreDomain) {
+        this.userStoreDomain = userStoreDomain;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String attributeName) {
+        this.attributeName = attributeName;
+    }
+}
---
+++ components/claim-mgt/org.wso2.carbon.identity.claim.metadata.mgt/src/main/java/org/wso2/carbon/identity/claim/metadata/mgt/internal/IdentityClaimManagementServiceComponent.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.identity.claim.metadata.mgt.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataManagementService;
+import org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataManagementServiceImpl;
+import org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataStoreFactory;
+import org.wso2.carbon.registry.core.service.RegistryService;
+import org.wso2.carbon.user.core.claim.ClaimManagerFactory;
+import org.wso2.carbon.user.core.service.RealmService;
+
+/**
+ * @scr.component name="identity.claim.metadata.component" immediate="true"
+ * @scr.reference name="registry.service"
+ * interface="org.wso2.carbon.registry.core.service.RegistryService"
+ * cardinality="1..1" policy="dynamic"
+ * bind="setRegistryService"
+ * unbind="unsetRegistryService"
+ * @scr.reference name="user.realmservice.default"
+ * interface="org.wso2.carbon.user.core.service.RealmService"
+ * cardinality="1..1" policy="dynamic"
+ * bind="setRealmService"
+ * unbind="unsetRealmService"
+ */
+@SuppressWarnings("unused")
+public class IdentityClaimManagementServiceComponent {
+
+    private static final Log log = LogFactory.getLog(IdentityClaimManagementServiceComponent.class);
+
+    protected void activate(ComponentContext ctxt) {
+        try {
+            IdentityClaimManagementServiceDataHolder.getInstance().setBundleContext(ctxt.getBundleContext());
+
+            ClaimMetadataStoreFactory claimMetadataStoreFactory = new ClaimMetadataStoreFactory();
+            ctxt.getBundleContext().registerService(ClaimManagerFactory.class.getName(), claimMetadataStoreFactory,
+                    null);
+
+            ClaimMetadataManagementService claimManagementService = new ClaimMetadataManagementServiceImpl();
+            ctxt.getBundleContext().registerService(ClaimMetadataManagementService.class.getName(),
+                    claimManagementService, null);
+            IdentityClaimManagementServiceDataHolder.getInstance().setClaimManagementService(claimManagementService);
+        } catch (Throwable e) {
+            log.error("Error occurred while activating Identity Claim Management Service Component", e);
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("Identity Claim Management Core bundle is activated");
+        }
+    }
+
+    protected void deactivate(ComponentContext ctxt) {
+        if (log.isDebugEnabled()) {
+            log.debug("Identity Claim Management bundle is deactivated");
+        }
+    }
+
+    protected void setRealmService(RealmService realmService) {
+        IdentityClaimManagementServiceDataHolder.getInstance().setRealmService(realmService);
+        if (log.isDebugEnabled()) {
+            log.debug("RealmService set in Identity Claim Management bundle");
+        }
+    }
+
+    protected void unsetRealmService(RealmService realmService) {
+        IdentityClaimManagementServiceDataHolder.getInstance().setRealmService(null);
+        if (log.isDebugEnabled()) {
+            log.debug("RealmService unset in Identity Claim Management bundle");
+        }
+    }
+
+    protected void setRegistryService(RegistryService registryService) {
+        IdentityClaimManagementServiceDataHolder.getInstance().setRegistryService(registryService);
+        if (log.isDebugEnabled()) {
+            log.debug("RegistryService set in Identity Claim Management bundle");
+        }
+    }
+
+    protected void unsetRegistryService(RegistryService registryService) {
+        IdentityClaimManagementServiceDataHolder.getInstance().setRegistryService(null);
+        if (log.isDebugEnabled()) {
+            log.debug("RegistryService unset in Identity Claim Management bundle");
+        }
+    }
+}
