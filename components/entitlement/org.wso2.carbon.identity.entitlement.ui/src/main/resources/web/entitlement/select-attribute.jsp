wso2/carbon-identity-framework

commit 541a974e82ef5c0a0af0bc81019181a8c3bb0c00

Fixing Open Redirect vulnerability.

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/select-attribute.jsp
@@ -34,6 +34,7 @@
         import="java.io.IOException" %>
 <%@ page import="java.util.ResourceBundle" %>
 <%@ page import="java.util.Set" %>
+<%@ page import="org.apache.commons.lang.StringUtils" %>
 <jsp:useBean id="entitlementPolicyBean" type="org.wso2.carbon.identity.entitlement.ui.EntitlementPolicyBean"
              class="org.wso2.carbon.identity.entitlement.ui.EntitlementPolicyBean" scope="session"/>
 <jsp:setProperty name="entitlementPolicyBean" property="*" />
@@ -94,10 +95,14 @@
     String cookie = (String) session.getAttribute(ServerConstants.ADMIN_SERVICE_COOKIE);
 
     String ruleId = request.getParameter("ruleId");
-    String returnPage = request.getParameter("returnPage");
-    if(returnPage == null || returnPage.trim().length() == 0){
+    String initiatedPage = request.getParameter("initiatedFrom");
+    String returnPage;
+    if (StringUtils.equals("create-policy-set", initiatedPage)) {
+        returnPage = "create-policy-set.jsp";
+    } else {
         returnPage = "policy-editor.jsp";
     }
+
     selectedFinderModule = request.getParameter("finderModule");
     if(selectedFinderModule == null || selectedFinderModule.trim().length() < 1){
         selectedFinderModule = EntitlementPolicyConstants.DEFAULT_META_DATA_MODULE_NAME;

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/basic-policy-editor.jsp
@@ -15,6 +15,7 @@
 * limitations under the License.
 */
 -->
+<%@ page import="org.owasp.encoder.Encode" %>
 <%@ page import="org.wso2.balana.utils.policy.dto.BasicRuleDTO" %>
 <%@ page import="org.wso2.balana.utils.policy.dto.BasicTargetDTO" %>
 <%@ page import="org.wso2.carbon.identity.entitlement.common.EntitlementConstants" %>
@@ -25,7 +26,6 @@
 <%@ page import="java.util.ArrayList" %>
 <%@ page import="java.util.List" %>
 <%@ page import="java.util.Set" %>
-<%@ page import="org.owasp.encoder.Encode" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib uri="http://wso2.org/projects/carbon/taglibs/carbontags.jar" prefix="carbon" %>
 <jsp:useBean id="entitlementPolicyBean" type="org.wso2.carbon.identity.entitlement.ui.EntitlementPolicyBean"
@@ -416,7 +416,7 @@
         if(doValidationPolicyNameOnly()){
             document.getElementsByName("ruleId")[0].value = "";
             preSubmit();
-            document.dataForm.action = "basic-policy-update.jsp?nextPage=basic-policy-finish";
+            document.dataForm.action = "basic-policy-update.jsp?action=completePolicy";
             document.dataForm.submit();
         }
     }
@@ -473,34 +473,33 @@
     function doUpdate(){
         if(doValidation()){
             preSubmit();
-            document.dataForm.action = "basic-policy-update.jsp?nextPage=basic-policy-editor&completedRule=true&updateRule=true";
+            document.dataForm.action = "basic-policy-update.jsp?action=updateRule&completedRule=true&updateRule=true";
             document.dataForm.submit();
         }
     }
 
     function doCancelRule(){
         preSubmit();
-        document.dataForm.action = "basic-policy-update.jsp?nextPage=basic-policy-editor&ruleId=";
+        document.dataForm.action = "basic-policy-update.jsp?action=cancelRule&ruleId=";
         document.dataForm.submit();
     }
 
     function deleteRule(ruleId) {
         preSubmit();
-        document.dataForm.action = "basic-policy-update.jsp?nextPage=delete-rule-entry&ruleId="
-                + ruleId + "&returnPage=basic-policy-editor";
+        document.dataForm.action = "basic-policy-update.jsp?action=deleteRule&ruleId=" + ruleId;
         document.dataForm.submit();
     }
 
     function editRule(ruleId){
         preSubmit();
-        document.dataForm.action = "basic-policy-update.jsp?nextPage=basic-policy-editor&editRule=true&ruleId=" + ruleId;
+        document.dataForm.action = "basic-policy-update.jsp?action=editRule&editRule=true&ruleId=" + ruleId;
         document.dataForm.submit();
     }
 
     function doAdd() {
         if(doValidation()){
             preSubmit();
-            document.dataForm.action = "basic-policy-update.jsp?nextPage=basic-policy-editor&completedRule=true";
+            document.dataForm.action = "basic-policy-update.jsp?action=addRule&completedRule=true";
             document.dataForm.submit();
         }
     }
@@ -508,8 +507,8 @@
     function selectAttributes(attributeType){
         if(doValidationPolicyNameOnly()){
             preSubmit();
-            document.dataForm.action = "basic-policy-update.jsp?nextPage=select-attribute-values&updateRule=true&category="
-                    + attributeType + "&returnPage=basic-policy-editor.jsp";
+            document.dataForm.action =
+                    "basic-policy-update.jsp?action=selectAttributes&updateRule=true&category=" + attributeType;
             document.dataForm.submit();
         }
     }
@@ -518,8 +517,7 @@
     function selectAttributesForTarget(attributeType){
         if(doValidationPolicyNameOnly()){
             preSubmit();
-            document.dataForm.action = "basic-policy-update.jsp?nextPage=select-attribute-values&ruleId=&attributeType="
-                    + attributeType  + "&returnPage=basic-policy-editor.jsp";
+            document.dataForm.action = "basic-policy-update.jsp?action=selectAttributes&ruleId=&attributeType=" + attributeType;
             document.dataForm.submit();
         }
     }

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/basic-policy-update.jsp
@@ -28,15 +28,11 @@
     BasicTargetDTO basicTargetDTO = new BasicTargetDTO();
     entitlementPolicyBean.setRuleElementOrder(null);
 
-    String policyName = request.getParameter("policyName");
-    String algorithmName = request.getParameter("algorithmName");
-    String policyDescription = request.getParameter("policyDescription");
+    String action = request.getParameter("action");
 
     String category = request.getParameter("category");
     String ruleElementOrder = request.getParameter("ruleElementOrder");
     String updateRule = request.getParameter("updateRule");
-    String nextPage = request.getParameter("nextPage");
-    String returnPage = request.getParameter("returnPage");
     // rules
     String ruleId = request.getParameter("ruleId");
     String ruleEffect = request.getParameter("ruleEffect");
@@ -272,8 +268,6 @@
     }
 
     entitlementPolicyBean.setBasicTargetDTO(basicTargetDTO);
-    
-    String forwardTo;
 
     if(ruleElementOrder != null && ruleElementOrder.trim().length() > 0){
         if(basicRuleDTO.isCompletedRule() && !"true".equals(updateRule)){
@@ -284,15 +278,26 @@
         }
     }
 
-    if(completedRule != null && completedRule.equals("true")){
-        forwardTo = nextPage + ".jsp?";
-    } else {
-        forwardTo = nextPage + ".jsp?ruleId=" + Encode.forUriComponent(ruleId);
-        if(category != null && category.trim().length() > 0){
+    String forwardTo = "basic-policy-editor.jsp";
+    if ("completePolicy".equals(action)) {
+        forwardTo = "basic-policy-finish.jsp";
+    } else if ("updateRule".equals(action) || "addRule".equals(action) || "cancelRule".equals(action) ||
+               "editRule".equals(action)) {
+        forwardTo = "basic-policy-editor.jsp";
+    } else if ("deleteRule".equals(action)) {
+        forwardTo = "delete-rule-entry.jsp";
+    } else if ("selectAttributes".equals(action)) {
+        forwardTo = "select-attribute-values.jsp";
+    }
+
+    if (completedRule == null || !Boolean.parseBoolean(completedRule)) {
+        forwardTo = forwardTo + "?ruleId=" + Encode.forUriComponent(ruleId);
+        if (category != null && category.trim().length() > 0) {
             forwardTo = forwardTo + "&category=" + Encode.forUriComponent(category);
         }
-        if(returnPage != null && returnPage.trim().length() > 0){
-            forwardTo = forwardTo + "&returnPage=" + Encode.forUriComponent(returnPage);
+
+        if ("deleteRule".equals(action)) {
+            forwardTo = forwardTo + "&initiatedFrom=basic-policy-editor";
         }
     }
 

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/create-policy-set.jsp
@@ -288,7 +288,7 @@
     function submitForm() {
         preSubmit();
         if (doValidationPolicyNameOnly()) {
-            document.dataForm.action = "update-policy-set.jsp?nextPage=finish-policy-set";
+            document.dataForm.action = "update-policy-set.jsp?action=complete";
             document.dataForm.submit();
         }
     }
@@ -330,7 +330,7 @@
 
     function deletePolicyRef(policyId) {
         preSubmit();
-        document.dataForm.action = "update-policy-set.jsp?delete=true&policyRefId=" + policyId;
+        document.dataForm.action = "update-policy-set.jsp?action=delete&policyRefId=" + policyId;
         document.dataForm.submit();
     }
 
@@ -359,7 +359,7 @@
     function selectAttributesForTarget(index) {
         preSubmit();
         if (doValidationPolicyNameOnly()) {
-            document.dataForm.action = "update-policy-set.jsp?nextPage=select-attribute&targetRowIndex="
+            document.dataForm.action = "update-policy-set.jsp?action=selectAttribute&targetRowIndex="
                     + index;
             document.dataForm.submit();
         }
@@ -395,7 +395,7 @@
     function searchServices() {
         preSubmit();
         if (doValidationPolicyNameOnly()) {
-            document.dataForm.action = "update-policy-set.jsp?nextPage=create-policy-set";
+            document.dataForm.action = "update-policy-set.jsp?action=search";
             document.dataForm.submit();
         }
     }
@@ -404,7 +404,7 @@
     function doPaginate(page, pageNumberParameterName, pageNumber){
         preSubmit();
         if (doValidationPolicyNameOnly()) {
-            document.dataForm.action =  "update-policy-set.jsp?nextPage=create-policy-set&" +
+            document.dataForm.action =  "update-policy-set.jsp?action=paginate&" +
                     pageNumberParameterName + "=" + pageNumber + "&";
             document.dataForm.submit();
         }
@@ -428,7 +428,7 @@
 
         if (doValidationPolicyNameOnly()) {
             document.dataForm.action = "update-policy-set.jsp?policyRefId=" + policyId +
-                    "&policyType=" + policyType + "&nextPage=create-policy-set";
+                    "&policyType=" + policyType + "&action=add";
             document.dataForm.submit();
         }
     }

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/delete-rule-entry.jsp
@@ -1,3 +1,4 @@
+<%@ page import="org.apache.commons.lang.StringUtils" %>
 <!--
 ~ Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 ~
@@ -22,8 +23,8 @@
 <%
     String forwardTo = null;
     String ruleId = request.getParameter("ruleId");
-    String returnPage = request.getParameter("returnPage");
-    if(returnPage != null && returnPage.trim().length() > 0){
+    String initiatedPage = request.getParameter("initiatedFrom");
+    if(StringUtils.equals("basic-policy-editor", initiatedPage)){
         if(ruleId != null && ruleId.trim().length() > 0){
             entitlementPolicyBean.removeBasicRuleElement(ruleId);
         }

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/eval-policy-submit.jsp
@@ -28,7 +28,6 @@
     if("true".equals(withPDP)){
         evaluatedWithPDP = true; 
     }
-    String forwardTo = request.getParameter("forwardTo");
     String serverURL = CarbonUIUtil.getServerURL(config.getServletContext(), session);
     ConfigurationContext configContext =
             (ConfigurationContext) config.getServletContext().
@@ -141,18 +140,11 @@
 
         session.setAttribute("txtRequest", requestString);
         session.setAttribute("txtResponse", resp);
-    	if (forwardTo == null) {
-            CarbonUIMessage.sendCarbonUIMessage(responseValue, CarbonUIMessage.INFO, request);
-            forwardTo = "create-evaluation-request.jsp";
-    	} else {
-            forwardTo = "eval-policy.jsp?isResponse=true";
-        }
+
+        CarbonUIMessage.sendCarbonUIMessage(responseValue, CarbonUIMessage.INFO, request);
     } catch (Exception e) {
     	String message = resourceBundle.getString("invalid.request");
         CarbonUIMessage.sendCarbonUIMessage(message, CarbonUIMessage.ERROR, request);
-        if (forwardTo == null) {
-            forwardTo = "create-evaluation-request.jsp";
-     	}       
     }
 %>
 
@@ -166,14 +158,13 @@
 <%@ page import="java.util.ArrayList" %>
 <%@ page import="java.util.List" %>
 <%@ page import="java.util.ResourceBundle" %>
-<%@ page import="org.owasp.encoder.Encode" %>
 <script
-	type="text/javascript">
+        type="text/javascript">
     function forward() {
-        location.href = "<%=Encode.forJavaScriptBlock(forwardTo)%>";
-	}
+        location.href = "create-evaluation-request.jsp";
+    }
 </script>
 
 <script type="text/javascript">
-	forward();
+    forward();
 </script>

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/policy-editor.jsp
@@ -450,7 +450,7 @@ function submitForm() {
     if (doValidationPolicyNameOnly()) {
         preSubmit();
         document.getElementsByName("ruleId")[0].value = "";
-        document.dataForm.action = "update-rule.jsp?nextPage=finish&ruleId=";
+        document.dataForm.action = "update-rule.jsp?action=completePolicy&ruleId=";
         document.dataForm.submit();
     }
 }
@@ -508,7 +508,7 @@ function doValidationPolicyNameOnly() {
 function doUpdate() {
     preSubmit();
     if (doValidation()) {
-        document.dataForm.action = "update-rule.jsp?nextPage=policy-editor&completedRule=true&updateRule=true";
+        document.dataForm.action = "update-rule.jsp?action=updateRule&completedRule=true&updateRule=true";
         document.dataForm.submit();
     }
 }
@@ -516,7 +516,7 @@ function doUpdate() {
 function doCancelRule() {
     preSubmit();
     document.getElementsByName("ruleId")[0].value = "";
-    document.dataForm.action =  "update-rule.jsp?nextPage=policy-editor&ruleId=";
+    document.dataForm.action =  "update-rule.jsp?action=cancelRule&ruleId=";
     document.dataForm.submit();
 
   //  location.href = "policy-editor.jsp";
@@ -524,20 +524,20 @@ function doCancelRule() {
 
 function deleteRule(ruleId) {
     preSubmit();
-    document.dataForm.action = "update-rule.jsp?nextPage=delete-rule-entry&ruleId=" + ruleId;
+    document.dataForm.action = "update-rule.jsp?action=deleteRule&ruleId=" + ruleId;
     document.dataForm.submit();
 }
 
 function editRule(ruleId) {
     preSubmit();
-    document.dataForm.action = "update-rule.jsp?nextPage=policy-editor&editRule=true&ruleId=" + ruleId;
+    document.dataForm.action = "update-rule.jsp?action=editRule&editRule=true&ruleId=" + ruleId;
     document.dataForm.submit();
 }
 
 function doAdd() {
     preSubmit();
     if (doValidation()) {
-        document.dataForm.action = "update-rule.jsp?nextPage=policy-editor&completedRule=true";
+        document.dataForm.action = "update-rule.jsp?action=addRule&completedRule=true";
         document.dataForm.submit();
     }
 }
@@ -545,7 +545,7 @@ function doAdd() {
 function selectAttributesForRule(index) {
     preSubmit();
     if (doValidationPolicyNameOnly()) {
-        document.dataForm.action = "update-rule.jsp?nextPage=select-attribute&updateRule=true&ruleRowIndex="
+        document.dataForm.action = "update-rule.jsp?action=selectAttribute&updateRule=true&ruleRowIndex="
                 + index ;
         document.dataForm.submit();
     }
@@ -554,7 +554,7 @@ function selectAttributesForRule(index) {
 function selectAttributesForRuleTarget(index) {
     preSubmit();
     if (doValidationPolicyNameOnly()) {
-        document.dataForm.action = "update-rule.jsp?nextPage=select-attribute&updateRule=true&targetRuleRowIndex="
+        document.dataForm.action = "update-rule.jsp?action=selectAttribute&updateRule=true&targetRuleRowIndex="
                 + index;
         document.dataForm.submit();
     }
@@ -600,7 +600,7 @@ function preSubmit(){
     function selectAttributesForTarget(index) {
         preSubmit();
         if (doValidationPolicyNameOnly()) {
-            document.dataForm.action = "update-rule.jsp?nextPage=select-attribute&ruleId=&targetRowIndex="
+            document.dataForm.action = "update-rule.jsp?action=selectAttribute&ruleId=&targetRowIndex="
                     + index;
             document.dataForm.submit();
         }

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/policy-view.jsp
@@ -37,7 +37,6 @@
 	ConfigurationContext configContext = (ConfigurationContext) config.getServletContext().
                                         getAttribute(CarbonConstants.CONFIGURATION_CONTEXT);
 	String cookie = (String) session.getAttribute(ServerConstants.ADMIN_SERVICE_COOKIE);
-	String forwardTo = "index";
     String policy = "";
 	String policyId = null;
     String BUNDLE = "org.wso2.carbon.identity.entitlement.ui.i18n.Resources";
@@ -83,7 +82,7 @@
         <textarea id="txtPolicy" rows="50" cols="50"><%=Encode.forHtmlContent(policy)%>
         </textarea>
         <input type="hidden" name="callbackURL"
-               value="../entitlement/update-policy-submit.jsp?forwardTo=<%=Encode.forUriComponent(forwardTo)%>&policyid=<%=Encode.forUriComponent(policyId)%>"/>
+               value="../entitlement/update-policy-submit.jsp?policyid=<%=Encode.forUriComponent(policyId)%>"/>
        </form>
     </div> 
     

commit b617720d247d177c90e24d68c057176adf193e95

Fix regression introduced by 541a974e82ef5c0a0af0bc81019181a8c3bb0c00

components/entitlement/org.wso2.carbon.identity.entitlement.ui/src/main/resources/web/entitlement/select-attribute.jsp
@@ -285,7 +285,7 @@
 
     function preSubmit(){
 
-        jQuery('#attributeValueTable > tbody:last').append('<tr><td><input type="hidden" name="category" id="category" value="<%=Encode.forJavaScript(Encode.forHtmlAttribute(category))%>" /><input type="hidden" name="ruleId" id="ruleId" value="<%=Encode.forJavaScript(Encode.forHtmlAttribute(ruleId))%>" /><input type="hidden" name="returnPage" id="returnPage" value="<%=Encode.forJavaScript(Encode.forHtmlAttribute(returnPage))%>" /></td></tr>') ;
+        jQuery('#attributeValueTable > tbody:last').append('<tr><td><input type="hidden" name="category" id="category" value="<%=Encode.forJavaScript(Encode.forHtmlAttribute(category))%>" /><input type="hidden" name="ruleId" id="ruleId" value="<%=Encode.forJavaScript(Encode.forHtmlAttribute(ruleId))%>" /><input type="hidden" name="initiatedFrom" id="initiatedFrom" value="<%=Encode.forJavaScript(Encode.forHtmlAttribute(initiatedPage))%>" /></td></tr>') ;
 
     }
 