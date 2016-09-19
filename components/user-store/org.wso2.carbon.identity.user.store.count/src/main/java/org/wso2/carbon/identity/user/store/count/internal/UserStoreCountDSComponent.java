17779eb52ead30168a52fee91e3d4a220369ce25
IndunilR <indunil@wso2.com>
2016-09-19 15:46:08 +0530

    Permission level refactoring

--- components/user-store/org.wso2.carbon.identity.user.store.count/src/main/java/org/wso2/carbon/identity/user/store/count/internal/UserStoreCountDSComponent.java
+++ components/user-store/org.wso2.carbon.identity.user.store.count/src/main/java/org/wso2/carbon/identity/user/store/count/internal/UserStoreCountDSComponent.java
@@ -23,6 +23,7 @@ import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.identity.core.util.IdentityCoreInitializedEvent;
 import org.wso2.carbon.identity.user.store.count.AbstractCountRetrieverFactory;
 import org.wso2.carbon.identity.user.store.count.UserStoreCountRetriever;
 import org.wso2.carbon.identity.user.store.count.exception.UserStoreCounterException;
@@ -42,6 +43,10 @@ import org.wso2.carbon.user.core.service.RealmService;
  * interface="org.wso2.carbon.identity.user.store.count.AbstractCountRetrieverFactory"
  * cardinality="0..n" policy="dynamic" bind="setCountRetrieverFactory"
  * unbind="unsetCountRetrieverFactory"
+ * @scr.reference name="identityCoreInitializedEventService"
+ * interface="org.wso2.carbon.identity.core.util.IdentityCoreInitializedEvent" cardinality="1..1"
+ * policy="dynamic" bind="setIdentityCoreInitializedEventService" unbind="unsetIdentityCoreInitializedEventService"
+ *
  */
 
 public class UserStoreCountDSComponent {
@@ -126,6 +131,16 @@ public class UserStoreCountDSComponent {
         if (log.isDebugEnabled()) {
             log.debug("Removed count retriever : " + countRetrieverFactory.getCounterType());
         }
+    }
+
+    protected void setIdentityCoreInitializedEventService(IdentityCoreInitializedEvent identityCoreInitializedEvent) {
+        /* reference IdentityCoreInitializedEvent service to guarantee that this component will wait until identity core
+         is started */
+    }
+
+    protected void unsetIdentityCoreInitializedEventService(IdentityCoreInitializedEvent identityCoreInitializedEvent) {
+        /* reference IdentityCoreInitializedEvent service to guarantee that this component will wait until identity core
+         is started */
     }
 
 }
