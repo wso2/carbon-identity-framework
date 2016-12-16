932dc0578bc0757a53a8161d6f285d0c0f84f889
Rushmin Fernando <rushmin@wso2.com>
2016-12-16 17:15:18 +0530

    IDENTITY-5387 : Adding a new service method with relaxed permission.

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/services/SessionManagementService.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/services/SessionManagementService.java
@@ -20,6 +20,7 @@ package org.wso2.carbon.identity.application.authentication.framework.services;
 import org.apache.commons.lang.StringUtils;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.wso2.carbon.context.CarbonContext;
 import org.wso2.carbon.core.AbstractAdmin;
 import org.wso2.carbon.identity.application.authentication.framework.cache.SessionContextCache;
 import org.wso2.carbon.identity.application.authentication.framework.context.SessionContext;
@@ -38,11 +39,63 @@ public class SessionManagementService extends AbstractAdmin {
             return false;
         }
 
+        // Retrieve session information from cache in order to publish event
+        SessionContext sessionContext = FrameworkUtils.getSessionContextFromCache(sessionId);
+        terminateSession(sessionContext, sessionId);
+
+        return true;
+    }
+
+    /**
+     *
+     * Terminates the requested session, after validating whether the session belongs to the logged in user.
+     *
+     * @param sessionId
+     * @return
+     */
+    public boolean removeMySession(String sessionId) {
+
+        if (StringUtils.isBlank(sessionId)) {
+            return false;
+        }
+
+        SessionContext sessionContext = FrameworkUtils.getSessionContextFromCache(sessionId);
+
+        // Check whether the session belongs to the logged in user.
+
+        CarbonContext carbonContext = CarbonContext.getThreadLocalCarbonContext();
+        String username = carbonContext.getUsername();
+
+        // Extract the user store domain if there is any or set to 'PRIMARY'.
+        String userStoreDomain = "PRIMARY";
+        String[] usernameTokens = username.split("/");
+        if(usernameTokens.length > 1){
+            userStoreDomain = usernameTokens[0];
+            username = usernameTokens[1];
+        }
+
+        AuthenticatedUser authenticatedUser = (AuthenticatedUser) sessionContext
+                .getProperty(FrameworkConstants.AUTHENTICATED_USER);
+
+        if(username.equals(authenticatedUser.getUserName())
+                && userStoreDomain.equals(authenticatedUser.getUserStoreDomain())
+                && carbonContext.getTenantDomain().equals(authenticatedUser.getTenantDomain())){
+            terminateSession(sessionContext, sessionId);
+        }else{ // TODO : Handle federated scenario.
+            log.warn(String.format("Trying to terminate a session which does not belong to logged in user (%s). " +
+                    "This might be an attempt for a security breach", username));
+            return false;
+        }
+
+        return true;
+    }
+
+    // TODO : Session ID should be a property of the SessionContext.
+    private void terminateSession(SessionContext sessionContext, String sessionId){
+
         if (FrameworkServiceDataHolder.getInstance().getAuthnDataPublisherProxy() != null && FrameworkServiceDataHolder
-                .getInstance().getAuthnDataPublisherProxy().isEnabled(null)) {
-            // Retrieve session information from cache in order to publish event
-            SessionContext sessionContext = FrameworkUtils.getSessionContextFromCache(sessionId);
-            if (sessionContext != null) {
+                .getInstance().getAuthnDataPublisherProxy().isEnabled(null) && sessionContext != null) {
+
                 Object authenticatedUserObj = sessionContext.getProperty(FrameworkConstants.AUTHENTICATED_USER);
                 AuthenticatedUser authenticatedUser = new AuthenticatedUser();
                 if (authenticatedUserObj != null) {
@@ -50,12 +103,7 @@ public class SessionManagementService extends AbstractAdmin {
                 }
                 FrameworkUtils.publishSessionEvent(sessionId, null, null, sessionContext, authenticatedUser,
                         FrameworkConstants.AnalyticsAttributes.SESSION_TERMINATE);
-            }
         }
-
         SessionContextCache.getInstance().clearCacheEntry(sessionId);
-
-        return true;
-
     }
 }
