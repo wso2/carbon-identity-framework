7c96bb4c4662bb11aebae6cf510116cc2330e140
Harsha Thirimanna <harsha.thirimanna@gmail.com>
2017-02-07 08:13:22 +0530

    Update Missing Fields in IDP Objects

--- components/org.wso2.carbon.identity.gateway.common/src/main/java/org/wso2/carbon/identity/gateway/common/model/idp/ProvisioningClaimConfig.java
+++ components/org.wso2.carbon.identity.gateway.common/src/main/java/org/wso2/carbon/identity/gateway/common/model/idp/ProvisioningClaimConfig.java
@@ -1,7 +1,25 @@
 package org.wso2.carbon.identity.gateway.common.model.idp;
 
-/**
- * Created by harshat on 2/6/17.
- */
+
+
 public class ProvisioningClaimConfig {
+
+    private String claimId ;
+    private String defaultValue ;
+
+    public String getClaimId() {
+        return claimId;
+    }
+
+    public void setClaimId(String claimId) {
+        this.claimId = claimId;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
 }
