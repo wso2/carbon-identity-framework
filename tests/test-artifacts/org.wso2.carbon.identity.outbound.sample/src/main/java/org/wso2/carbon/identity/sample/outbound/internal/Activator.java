0236fba4c776136302438bf510f69dd60f93dd7f
hasinthaindrajee <hasintha@wso2.com>
2017-02-24 10:14:01 +0530

    test implementation

---
+++ tests/test-artifacts/org.wso2.carbon.identity.outbound.sample/src/main/java/org/wso2/carbon/identity/sample/outbound/internal/Activator.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wso2.carbon.identity.sample.outbound;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.identity.gateway.api.request.GatewayRequestBuilderFactory;
+import org.wso2.carbon.identity.gateway.api.response.GatewayResponseBuilderFactory;
+import org.wso2.carbon.identity.gateway.processor.handler.request.AbstractRequestHandler;
+import org.wso2.carbon.identity.gateway.processor.handler.response.AbstractResponseHandler;
+import org.wso2.carbon.identity.sample.outbound.request.SampleProtocolIdentityRequestBuilderFactory;
+import org.wso2.carbon.identity.sample.outbound.response.SampleProtocolResponseBuilderFactory;
+import org.wso2.carbon.identity.sample.outbound.response.SampleProtocolResponseHandler;
+import org.wso2.carbon.identity.sample.outbound.validator.SampleProtocolValidator;
+
+@Component(
+        name = "org.wso2.carbon.identity.sample.outbound.component",
+        immediate = true
+)
+public class Activator implements BundleActivator {
+
+    private Logger log = LoggerFactory.getLogger(Activator.class);
+
+    @Activate
+    public void start(BundleContext bundleContext) throws Exception {
+        try {
+            bundleContext.registerService(GatewayRequestBuilderFactory.class, new SampleProtocolIdentityRequestBuilderFactory(), null);
+            bundleContext.registerService(GatewayResponseBuilderFactory.class, new SampleProtocolResponseBuilderFactory(), null);
+            bundleContext.registerService(AbstractRequestHandler.class, new SampleProtocolValidator(), null);
+            bundleContext.registerService(AbstractResponseHandler.class, new SampleProtocolResponseHandler(), null);
+        } catch (Throwable e) {
+            System.out.println("Error while activating saml outbound component");
+        }
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
---
+++ tests/test-artifacts/org.wso2.carbon.identity.outbound.sample/src/main/java/org/wso2/carbon/identity/sample/outbound/validator/SampleProtocolValidator.java
@@ -0,0 +1,42 @@
+package org.wso2.carbon.identity.sample.outbound.validator;
+
+import org.wso2.carbon.identity.common.base.message.MessageContext;
+import org.wso2.carbon.identity.gateway.api.context.GatewayMessageContext;
+import org.wso2.carbon.identity.gateway.context.AuthenticationContext;
+import org.wso2.carbon.identity.gateway.processor.FrameworkHandlerResponse;
+import org.wso2.carbon.identity.gateway.processor.handler.authentication.AuthenticationHandlerException;
+import org.wso2.carbon.identity.gateway.processor.handler.request.AbstractRequestHandler;
+import org.wso2.carbon.identity.gateway.processor.handler.request.RequestHandlerException;
+import org.wso2.carbon.identity.sample.outbound.request.SampleProtocolRequest;
+
+public class SampleProtocolValidator extends AbstractRequestHandler {
+    @Override
+    public FrameworkHandlerResponse validate(AuthenticationContext authenticationContext) throws RequestHandlerException {
+        authenticationContext.setUniqueId("travelocity.com");
+        try {
+            if (authenticationContext.getServiceProvider() == null) {
+                throw new RequestHandlerException("No Service Provider Found for this Unique ID");
+            }
+        } catch (AuthenticationHandlerException e) {
+            throw new RequestHandlerException("No Service Provider Found for this Unique ID");
+        }
+        return FrameworkHandlerResponse.CONTINUE;
+    }
+
+    @Override
+    public boolean canHandle(MessageContext messageContext) {
+        if (messageContext instanceof GatewayMessageContext) {
+            GatewayMessageContext gatewayMessageContext = (GatewayMessageContext) messageContext;
+            if (gatewayMessageContext.getIdentityRequest() instanceof SampleProtocolRequest) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    protected String getValidatorType() {
+        return "SampleProtocolRequestValidator";
+    }
+}
