c20347fac3da35224f2fe85cc56924d5d26e8132
hasinthaindrajee <hasintha@wso2.com>
2017-02-24 10:24:59 +0530

    adding tests

---
+++ tests/test-artifacts/org.wso2.carbon.identity.outbound.sample/src/main/java/org/wso2/carbon/identity/sample/inbound/response/SampleProtocolResponseBuilderFactory.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.identity.sample.inbound.response;
+
+import org.wso2.carbon.identity.gateway.api.response.GatewayResponseBuilderFactory;
+
+public class SampleProtocolResponseBuilderFactory extends GatewayResponseBuilderFactory {
+
+}
