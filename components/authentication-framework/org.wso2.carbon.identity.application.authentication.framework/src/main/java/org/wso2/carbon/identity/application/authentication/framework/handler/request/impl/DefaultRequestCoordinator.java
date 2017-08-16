69d1a03d984e6091d8e6ce159b3d52dfd5c9df8f
Gayan <gmgunawardana@gmail.com>
2017-08-16 18:21:15 +0530

    Fixing reauthenticate issues with different ACR values

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/handler/request/impl/DefaultRequestCoordinator.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/handler/request/impl/DefaultRequestCoordinator.java
@@ -473,7 +473,7 @@ public class DefaultRequestCoordinator implements RequestCoordinator {
         if(previousAcrList == null || previousAcrList.size() != newAcrList.size()) {
             return true;
         }
-        return newAcrList.get(0).equals(previousAcrList.get(0)); //TODO: Scan All list.
+        return !(newAcrList.get(0).equals(previousAcrList.get(0))); //TODO: Scan All list.
     }
 
     private void buildOutboundQueryString(HttpServletRequest request, AuthenticationContext context)
