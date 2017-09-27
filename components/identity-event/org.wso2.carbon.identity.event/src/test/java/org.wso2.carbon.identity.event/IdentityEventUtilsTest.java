b6ff16a9708d4d6aa040d66e0788085af7f569c7
maduranga <madurangar@wso2.com>
2017-09-27 09:27:03 +0530

    Adding_unit_tests_for_identity-events

--- components/identity-event/org.wso2.carbon.identity.event/src/test/java/org.wso2.carbon.identity.event/IdentityEventUtilsTest.java
+++ components/identity-event/org.wso2.carbon.identity.event/src/test/java/org.wso2.carbon.identity.event/IdentityEventUtilsTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.identity.event;
 
 
@@ -8,7 +25,6 @@ import java.io.*;
 import java.util.Properties;
 
 
-
 public class IdentityEventUtilsTest {
 
     private Properties properties;
@@ -16,12 +32,10 @@ public class IdentityEventUtilsTest {
 
     @BeforeMethod
     public void setUp() throws Exception {
-
         setProperties();
     }
 
     private void setProperties() {
-
         properties = new Properties();
         properties.setProperty("key", "replaced");
 
@@ -38,22 +52,18 @@ public class IdentityEventUtilsTest {
     }
 
     @Test
-    public  void testreplacePlaceHolder(){
+    public void testreplacePlaceHolder() {
 
-        String replaceRegexStartsWit="start";
-        String replaceRegexEndsWit="end";
+        String replaceRegexStartsWit = "start";
+        String replaceRegexEndsWit = "end";
         String key = "key";
         String finalContent = "replacedextra";
         String contentBefore = "startkeyendextra";
-        String replacePlaceHolder = IdentityEventUtils.replacePlaceHolders(contentBefore,replaceRegexStartsWit,replaceRegexEndsWit,properties);
-        Assert.assertEquals(finalContent,replacePlaceHolder);
+        String replacePlaceHolder = IdentityEventUtils.replacePlaceHolders(contentBefore, replaceRegexStartsWit, replaceRegexEndsWit, properties);
+        Assert.assertEquals(finalContent, replacePlaceHolder);
     }
 
-    @Test
-    public void testgetPropertiesWithPrefix(){
 
-
-    }
 }
 
 
