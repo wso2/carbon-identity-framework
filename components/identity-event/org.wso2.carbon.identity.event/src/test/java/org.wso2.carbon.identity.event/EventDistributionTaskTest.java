4626d95c9f792151f130451c24348a2f0fccb5f9
maduranga <madurangar@wso2.com>
2017-09-28 08:00:40 +0530

    Adding_unit_tests_for_identity-events

---
+++ components/identity-event/org.wso2.carbon.identity.event/src/test/java/org.wso2.carbon.identity.event/EventDistributionTaskTest.java
@@ -0,0 +1,4 @@
+package org.wso2.carbon.identity.event;
+
+public class EventDistributionTaskTest {
+}
---
+++ components/identity-event/org.wso2.carbon.identity.event/src/test/java/org.wso2.carbon.identity.event/IdentityEventConfigBuilderTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.identity.event;
+
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.junit.runner.RunWith;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static org.testng.Assert.*;
+
+public class IdentityEventConfigBuilderTest {
+
+    @BeforeSuite
+    public void setup(){
+        String home = IdentityEventConfigBuilder.class.getResource("/").getFile();
+        String config = IdentityEventConfigBuilder.class.getResource("/").getFile();
+        System.setProperty("carbon.home", home);
+        System.setProperty("carbon.config.dir.path",config);
+
+    }
+
+   @Test
+    public void testGetInstance() throws IdentityEventException {
+
+        IdentityEventConfigBuilder identityEventConfigBuilder =  IdentityEventConfigBuilder.getInstance();
+        assertNotNull(identityEventConfigBuilder);
+
+
+
+    }
+
+
+
+
+}
