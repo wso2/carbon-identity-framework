79468470f1cf2ebfa502c9cc4ee153d6dda8ff5d
Ruwan Abeykoon <ruwana@wso2.com>
2017-11-07 15:14:57 +0530

    Adding some comments to NullAppender.

--- test-utils/org.wso2.carbon.identity.testutil/src/main/java/org/wso2/carbon/identity/testutil/log/NullAppender.java
+++ test-utils/org.wso2.carbon.identity.testutil/src/main/java/org/wso2/carbon/identity/testutil/log/NullAppender.java
@@ -5,6 +5,13 @@ import org.apache.log4j.spi.LoggingEvent;
 
 /**
  * Log4J Log Appender, which does nothing. Logs simply goes into a black hole.
+ * This is primarily used to skip DEBUG and INFO logs being printed by testNg unit tests.
+ * We create multiple unit test(s) and suite(s) for each DEBUG and INFO log levels to get the proper
+ * coverage.
+ *
+ * Printing all the logs into the console clutters the console, makes the test execution slow and causes OOM.
+ *
+ * Null Appender helps to prevent the above problems.
  */
 public class NullAppender extends AppenderSkeleton {
 
