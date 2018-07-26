c7181051ca74eb96651b8e73f7e5448ca0512ecd
thusithakalugamage <thusitha32@yahoo.com>
2018-07-26 12:25:35 +0530

    style fixes for custom templates

--- components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint/src/main/webapp/assets/js/consent_template_1.js
+++ components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint/src/main/webapp/assets/js/consent_template_1.js
@@ -1,9 +1,4 @@
 var tempStr1 =
-    "<div class='consent-container-1'><div>We need your your {{pii:Educational Purposes:Full Name}} and" +
-    " {{pii:Educational" +
-    " Purposes:Address}} for {{purpose:Educational Purposes}}.</div>" +
-    "<div>We would also like to get your approval to use your {{pii:Research Purposes:Full Name}} and {{pii:Research" +
-    " Purposes:Birth Date}} for {{purpose:Research Purposes}}.</div>" +
     "<div>We need your {{pii:Test Purposes:Department}} , {{pii:Test" +
     " Purposes:Country}} . Optionally we need {{pii:Test Purposes:Last Name}} and {{pii:Test Purposes:Gender}} for" +
     " {{purpose:Test Purposes}}.</div></div>";
