52294c6d553755062ee61c8df2fd3fa489b4f294
tharindu-bandara <bandara14@live.com>
2019-01-23 22:30:32 +0530

    [configuration-store][refactor-datetime-metadata] gen files

--- components/configuration-mgt/org.wso2.carbon.identity.configuration.mgt.endpoint/src/gen/java/org/wso2/carbon/identity/configuration/mgt/endpoint/ResourceApi.java
+++ components/configuration-mgt/org.wso2.carbon.identity.configuration.mgt.endpoint/src/gen/java/org/wso2/carbon/identity/configuration/mgt/endpoint/ResourceApi.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.wso2.carbon.identity.configuration.mgt.endpoint;
@@ -20,13 +20,11 @@ import io.swagger.annotations.ApiParam;
 import org.wso2.carbon.identity.configuration.mgt.endpoint.dto.AttributeDTO;
 import org.wso2.carbon.identity.configuration.mgt.endpoint.dto.ResourceAddDTO;
 import org.wso2.carbon.identity.configuration.mgt.endpoint.dto.ResourceDTO;
-import org.wso2.carbon.identity.configuration.mgt.endpoint.dto.ResourcesDTO;
 import org.wso2.carbon.identity.configuration.mgt.endpoint.factories.ResourceApiServiceFactory;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -41,51 +39,6 @@ import javax.ws.rs.core.Response;
 public class ResourceApi {
 
     private final ResourceApiService delegate = ResourceApiServiceFactory.getResourceApi();
-
-    @GET
-
-    @Consumes({"application/json"})
-    @Produces({"application/json"})
-    @io.swagger.annotations.ApiOperation(value = "Retrieve resources.\n", notes = "This API is used to retrieve " +
-            "resources.\n", response = ResourcesDTO.class)
-    @io.swagger.annotations.ApiResponses(value = {
-            @io.swagger.annotations.ApiResponse(code = 200, message = "Ok"),
-
-            @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
-
-            @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
-
-            @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error")})
-
-    public Response resourceGet() {
-
-        return delegate.resourceGet();
-    }
-
-    @PATCH
-    @Path("/{resource-type}")
-    @Consumes({"application/json"})
-    @Produces({"application/json"})
-    @io.swagger.annotations.ApiOperation(value = "Update existing resource\n", notes = "This API is used to update an" +
-            " existing resource given by the user.\n", response = ResourceDTO.class)
-    @io.swagger.annotations.ApiResponses(value = {
-            @io.swagger.annotations.ApiResponse(code = 200, message = "Ok"),
-
-            @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
-
-            @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
-
-            @io.swagger.annotations.ApiResponse(code = 404, message = "Not Found"),
-
-            @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error")})
-
-    public Response resourceResourceTypePatch(@ApiParam(value = "This represents the type of the resource to be added" +
-            " and can either be the name or id.", required = true) @PathParam("resource-type") String resourceType,
-                                              @ApiParam(value = "This represents the resource that needs to be " +
-                                                      "updated.", required = true) ResourceAddDTO resource) {
-
-        return delegate.resourceResourceTypePatch(resourceType, resource);
-    }
 
     @POST
     @Path("/{resource-type}")
@@ -245,41 +198,11 @@ public class ResourceApi {
         return delegate.resourceResourceTypeResourceNameGet(resourceName, resourceType);
     }
 
-    @PATCH
-    @Path("/{resource-type}/{resource-name}")
-    @Consumes({"application/json"})
-    @Produces({"application/json"})
-    @io.swagger.annotations.ApiOperation(value = "Update existing attribute\n", notes = "This API is used to update " +
-            "an existing attribute given by the user.\n", response = AttributeDTO.class)
-    @io.swagger.annotations.ApiResponses(value = {
-            @io.swagger.annotations.ApiResponse(code = 200, message = "Ok"),
-
-            @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
-
-            @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
-
-            @io.swagger.annotations.ApiResponse(code = 404, message = "Not Found"),
-
-            @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error")})
-
-    public Response resourceResourceTypeResourceNamePatch(@ApiParam(value = "This represents the name of the " +
-            "attribute to be added or updated.", required = true) @PathParam("resource-name") String resourceName,
-                                                          @ApiParam(value = "This represents the type of the " +
-                                                                  "attribute to be added or updated and can either be" +
-                                                                  " the name or id.", required = true) @PathParam(
-                                                                          "resource-type") String resourceType,
-                                                          @ApiParam(value = "This represents the corresponding " +
-                                                                  "attribute value that needs to be added or updated" +
-                                                                  ".", required = true) AttributeDTO attribute) {
-
-        return delegate.resourceResourceTypeResourceNamePatch(resourceName, resourceType, attribute);
-    }
-
     @POST
     @Path("/{resource-type}/{resource-name}")
     @Consumes({"application/json"})
     @Produces({"application/json"})
-    @io.swagger.annotations.ApiOperation(value = "Create the attribute\n", notes = "This API is used to store the " +
+    @io.swagger.annotations.ApiOperation(value = "Create an attribute\n", notes = "This API is used to store an " +
             "attribute given by the user.\n", response = AttributeDTO.class)
     @io.swagger.annotations.ApiResponses(value = {
             @io.swagger.annotations.ApiResponse(code = 201, message = "Successful response"),
@@ -308,8 +231,8 @@ public class ResourceApi {
     @Path("/{resource-type}/{resource-name}")
     @Consumes({"application/json"})
     @Produces({"application/json"})
-    @io.swagger.annotations.ApiOperation(value = "Add or Replace the attribute\n", notes = "This API is used to store" +
-            " or replace the attribute given by the user.\n", response = AttributeDTO.class)
+    @io.swagger.annotations.ApiOperation(value = "Add or Replace an attribute\n", notes = "This API is used to store " +
+            "or replace an attribute given by the user.\n", response = AttributeDTO.class)
     @io.swagger.annotations.ApiResponses(value = {
             @io.swagger.annotations.ApiResponse(code = 200, message = "Ok"),
 
@@ -321,36 +244,10 @@ public class ResourceApi {
 
     public Response resourceResourceTypeResourceNamePut(@ApiParam(value = "This represents the name of the attribute " +
             "to be added or replaced.", required = true) @PathParam("resource-name") String resourceName,
-                                                        @ApiParam(value = "This represents the type of the attribute " +
-                                                                "to be added or replaced and can either be the name " +
-                                                                "or id.", required = true) @PathParam("resource-type") String resourceType,
-                                                        @ApiParam(value = "This represents the corresponding " +
-                                                                "attribute value that needs to be added or replaced."
-                                                                , required = true) AttributeDTO attribute) {
+                                                        @ApiParam(value = "This represents the type of the attribute to be added or replaced and can either be the name or id.", required = true) @PathParam("resource-type") String resourceType,
+                                                        @ApiParam(value = "This represents the corresponding attribute value that needs to be added or replaced.", required = true) AttributeDTO attribute) {
 
         return delegate.resourceResourceTypeResourceNamePut(resourceName, resourceType, attribute);
-    }
-
-    @GET
-    @Path("/resource-type/{resource-type-name}")
-    @Consumes({"application/json"})
-    @Produces({"application/json"})
-    @io.swagger.annotations.ApiOperation(value = "Retrieve all the resources for the given resource type.\n", notes =
-            "This API is used to retrieve all the resources for a given resource type.\n", response =
-            ResourcesDTO.class)
-    @io.swagger.annotations.ApiResponses(value = {
-            @io.swagger.annotations.ApiResponse(code = 200, message = "Ok"),
-
-            @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
-
-            @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
-
-            @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error")})
-
-    public Response resourceResourceTypeResourceTypeNameGet(@ApiParam(value = "This represents the name of the " +
-            "resource type to be retrieved.", required = true) @PathParam("resource-type-name") String resourceTypeName) {
-
-        return delegate.resourceResourceTypeResourceTypeNameGet(resourceTypeName);
     }
 }
 
--- components/configuration-mgt/org.wso2.carbon.identity.configuration.mgt.endpoint/src/gen/java/org/wso2/carbon/identity/configuration/mgt/endpoint/SearchApi.java
+++ components/configuration-mgt/org.wso2.carbon.identity.configuration.mgt.endpoint/src/gen/java/org/wso2/carbon/identity/configuration/mgt/endpoint/SearchApi.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.wso2.carbon.identity.configuration.mgt.endpoint;
@@ -28,31 +28,31 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 @Path("/search")
-@Consumes({"application/json"})
-@Produces({"application/json"})
+@Consumes({ "application/json" })
+@Produces({ "application/json" })
 @io.swagger.annotations.Api(value = "/search", description = "the search API")
-public class SearchApi {
+public class SearchApi  {
 
-    private final SearchApiService delegate = SearchApiServiceFactory.getSearchApi();
+   private final SearchApiService delegate = SearchApiServiceFactory.getSearchApi();
 
     @GET
 
-    @Consumes({"application/json"})
-    @Produces({"application/json"})
+    @Consumes({ "application/json" })
+    @Produces({ "application/json" })
     @io.swagger.annotations.ApiOperation(value = "Retrieve tenant resources based on search parameters.\n", notes =
-            "This API is used to search for tenant resources with search filters.\n", response = ResourcesDTO.class)
+            "This API is used to search resources across tenants with the given search query. Please refer the documentation for the usage.\n", response = ResourcesDTO.class)
     @io.swagger.annotations.ApiResponses(value = {
-            @io.swagger.annotations.ApiResponse(code = 200, message = "Ok"),
+        @io.swagger.annotations.ApiResponse(code = 200, message = "Ok"),
 
-            @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
+        @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request"),
 
-            @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
+        @io.swagger.annotations.ApiResponse(code = 401, message = "Unauthorized"),
 
-            @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error")})
+        @io.swagger.annotations.ApiResponse(code = 500, message = "Server Error") })
 
-    public Response searchGet(@Context SearchContext searchContext) {
-
-        return delegate.searchGet(searchContext);
+    public Response searchGet(@Context SearchContext searchContext)
+    {
+    return delegate.searchGet(searchContext);
     }
 }
 
