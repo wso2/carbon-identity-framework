b82f7e2738186bee07ef8b654cad1c3d47db46ff
deshankoswatte <dehami.deshan@gmail.com>
2019-10-14 10:45:58 +0530

    Moved SAML bootstrap method to a specific class to avoid duplication

--- components/identity-core/org.wso2.carbon.identity.core/src/test/java/org/wso2/carbon/identity/core/util/IdentityUtilTest.java
+++ components/identity-core/org.wso2.carbon.identity.core/src/test/java/org/wso2/carbon/identity/core/util/IdentityUtilTest.java
@@ -435,7 +435,7 @@ public class IdentityUtilTest {
 
     @Test
     public void testUnmarshall() throws Exception {
-        doBootstrap();
+        SAMLInitializer.doBootstrap();
         String xmlString = "<saml:Audience xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">https://sp.example.com/SAML2</saml:Audience>";
         XMLObject xmlObject = IdentityUtil.unmarshall(xmlString);
         assertEquals(xmlObject.getElementQName().getLocalPart(), "Audience", "Unmarshalled object doesn't match the " +
@@ -812,45 +812,4 @@ public class IdentityUtilTest {
         return new org.powermock.modules.testng.PowerMockObjectFactory();
     }
 
-    private static void doBootstrap() {
-        Thread thread = Thread.currentThread();
-        ClassLoader loader = thread.getContextClassLoader();
-        thread.setContextClassLoader(InitializationService.class.getClassLoader());
-
-        try {
-            InitializationService.initialize();
-
-            org.opensaml.saml.config.SAMLConfigurationInitializer initializer_1 = new org.opensaml.saml.config.SAMLConfigurationInitializer();
-            initializer_1.init();
-
-            org.opensaml.saml.config.XMLObjectProviderInitializer initializer_2 = new org.opensaml.saml.config.XMLObjectProviderInitializer();
-            initializer_2.init();
-
-            org.opensaml.core.xml.config.XMLObjectProviderInitializer initializer_3 = new org.opensaml.core.xml.config.XMLObjectProviderInitializer();
-            initializer_3.init();
-
-            org.opensaml.core.xml.config.GlobalParserPoolInitializer initializer_4 = new org.opensaml.core.xml.config.GlobalParserPoolInitializer();
-            initializer_4.init();
-
-            org.opensaml.xmlsec.config.JavaCryptoValidationInitializer initializer_5 = new org.opensaml.xmlsec.config.JavaCryptoValidationInitializer();
-            initializer_5.init();
-
-            org.opensaml.xmlsec.config.XMLObjectProviderInitializer initializer_6 = new org.opensaml.xmlsec.config.XMLObjectProviderInitializer();
-            initializer_6.init();
-
-            org.opensaml.xmlsec.config.ApacheXMLSecurityInitializer initializer_7 = new org.opensaml.xmlsec.config.ApacheXMLSecurityInitializer();
-            initializer_7.init();
-
-            org.opensaml.xmlsec.config.GlobalSecurityConfigurationInitializer initializer_8 = new org.opensaml.xmlsec.config.GlobalSecurityConfigurationInitializer();
-            initializer_8.init();
-
-            org.opensaml.xmlsec.config.GlobalAlgorithmRegistryInitializer initializer_9 = new org.opensaml.xmlsec.config.GlobalAlgorithmRegistryInitializer();
-            initializer_9.init();
-
-        } catch (org.opensaml.core.config.InitializationException e) {
-            e.printStackTrace();
-        } finally {
-            thread.setContextClassLoader(loader);
-        }
-    }
 }
