04dd0332f314359183d40ddf3620638110d253aa
nimashanishadi <nimasha@wso2.com>
2020-01-24 13:58:28 +0530

    fix Inbound Authentication Configuration get automatically filled in Admin Console

--- components/application-mgt/org.wso2.carbon.identity.application.mgt.ui/src/main/java/org/wso2/carbon/identity/application/mgt/ui/ApplicationBean.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt.ui/src/main/java/org/wso2/carbon/identity/application/mgt/ui/ApplicationBean.java
@@ -1278,7 +1278,7 @@ public class ApplicationBean {
         String passiveSTSRealm = request.getParameter("passiveSTSRealm");
         String passiveSTSWReply = request.getParameter("passiveSTSWReply");
 
-        if (passiveSTSRealm != null) {
+        if (StringUtils.isNotEmpty(passiveSTSRealm)) {
             InboundAuthenticationRequestConfig opicAuthenticationRequest = new InboundAuthenticationRequestConfig();
             opicAuthenticationRequest.setInboundAuthKey(passiveSTSRealm);
             opicAuthenticationRequest.setInboundAuthType("passivests");
@@ -1294,7 +1294,7 @@ public class ApplicationBean {
 
         String openidRealm = request.getParameter("openidRealm");
 
-        if (openidRealm != null) {
+        if (StringUtils.isNotEmpty(openidRealm)) {
             InboundAuthenticationRequestConfig opicAuthenticationRequest = new InboundAuthenticationRequestConfig();
             opicAuthenticationRequest.setInboundAuthKey(openidRealm);
             opicAuthenticationRequest.setInboundAuthType("openid");
