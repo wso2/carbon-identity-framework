275fe57370647c04c67a2249975c54e7d189633a
Thisara-Welmilla <thisara@wso2.com>
2021-05-19 17:28:50 +0530

    Refactored code

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/test/java/org/wso2/carbon/identity/application/authentication/framework/services/ConditionalAuthenticationMgtServiceTest.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/test/java/org/wso2/carbon/identity/application/authentication/framework/services/ConditionalAuthenticationMgtServiceTest.java
@@ -56,12 +56,13 @@ public class ConditionalAuthenticationMgtServiceTest {
 
         JsFunctionRegistry emptyRegistry = new JsFunctionRegistryImpl();
         JsFunctionRegistry registryWithCustomFunctions = new JsFunctionRegistryImpl();
-        registryWithCustomFunctions.register(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER, "func1", new Object());
+        registryWithCustomFunctions.register(JsFunctionRegistry.Subsystem.SEQUENCE_HANDLER,
+                                             "func1", new Object());
 
         return new Object[][]{
                 {emptyRegistry, new String[]{"executeStep", "selectAcrFrom", "sendError", "Log.info", "require"}},
-                {registryWithCustomFunctions, new String[]{"executeStep", "selectAcrFrom", "sendError", "Log.info", "require",
-                        "func1"}},
+                {registryWithCustomFunctions, new String[]{"executeStep", "selectAcrFrom", "sendError", "Log.info",
+                        "require", "func1"}},
         };
     }
 }
