7af1ea0a2477bdd67f64636d45714d5ea9569bd2
Wathsara Wishwantha Daluwatta <31571237+Wathsara@users.noreply.github.com>
2021-09-22 09:27:53 +0530

    Fix response issues when overlapping IDP API calls. (#3734)
    * Fix cache issues when overlapping calls.
    
    * Address comments
    
    * Fix type
    
    * Fix typo

--- components/idp-mgt/org.wso2.carbon.idp.mgt/src/main/java/org/wso2/carbon/idp/mgt/IdentityProviderManager.java
+++ components/idp-mgt/org.wso2.carbon.idp.mgt/src/main/java/org/wso2/carbon/idp/mgt/IdentityProviderManager.java
@@ -2372,7 +2372,7 @@ public class IdentityProviderManager implements IdpManager {
                 return null;
             }
         }
-        return dao.getIdPByResourceId(resourceId, tenantId, tenantDomain);
+        return dao.getUpdatedIdPByResourceId(resourceId, tenantId, tenantDomain);
     }
 
     private void updateIDP(IdentityProvider currentIdentityProvider, IdentityProvider newIdentityProvider, int tenantId,
