e96f0afbea8756da66eb08d0d8e474d8db83a20d
Wathsara Wishwantha Daluwatta <31571237+Wathsara@users.noreply.github.com>
2021-10-05 18:28:37 +0530

    update existing claims validation with ignore case (#3783)

--- components/claim-mgt/org.wso2.carbon.identity.claim.metadata.mgt/src/main/java/org/wso2/carbon/identity/claim/metadata/mgt/ClaimMetadataManagementServiceImpl.java
+++ components/claim-mgt/org.wso2.carbon.identity.claim.metadata.mgt/src/main/java/org/wso2/carbon/identity/claim/metadata/mgt/ClaimMetadataManagementServiceImpl.java
@@ -508,12 +508,12 @@ public class ClaimMetadataManagementServiceImpl implements ClaimMetadataManageme
             throws ClaimMetadataException {
 
         return this.externalClaimDAO.getExternalClaims(externalClaimDialectURI, tenantId).stream().filter(
-                claim -> claim.getClaimURI().equals(externalClaimURI)).findFirst().isPresent();
+                claim -> claim.getClaimURI().equalsIgnoreCase(externalClaimURI)).findFirst().isPresent();
     }
 
     private boolean isExistingLocalClaimURI(String localClaimURI, int tenantId) throws ClaimMetadataException {
 
         return this.localClaimDAO.getLocalClaims(tenantId).stream().filter(
-                claim -> claim.getClaimURI().equals(localClaimURI)).findFirst().isPresent();
+                claim -> claim.getClaimURI().equalsIgnoreCase(localClaimURI)).findFirst().isPresent();
     }
 }
