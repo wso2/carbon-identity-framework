45499c9648aa553fa9de4d904c21b6356469bda1
GANGANI <gcariyarathne.15@cse.mrt.ac.lk>
2021-10-11 12:50:46 +0530

    Resolve username to check whether the user can be provisioned

--- components/provisioning/org.wso2.carbon.identity.provisioning/src/main/java/org/wso2/carbon/identity/provisioning/OutboundProvisioningManager.java
+++ components/provisioning/org.wso2.carbon.identity.provisioning/src/main/java/org/wso2/carbon/identity/provisioning/OutboundProvisioningManager.java
@@ -22,6 +22,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.ListUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.CarbonException;
@@ -827,8 +828,9 @@ public class OutboundProvisioningManager {
             return true;
         }
 
-        if (provisioningEntity.getAttributes() != null) {
-            String userName = getUserName(provisioningEntity.getAttributes());
+        if (provisioningEntity.getAttributes() != null &&
+                StringUtils.isNotBlank(provisioningEntity.getEntityName())) {
+            String userName = provisioningEntity.getEntityName();
             List<String> provisioningRoleList = Arrays.asList(provisionByRoleList);
 
             List<String> roleListOfUser = getUserRoles(userName, tenantDomain);
