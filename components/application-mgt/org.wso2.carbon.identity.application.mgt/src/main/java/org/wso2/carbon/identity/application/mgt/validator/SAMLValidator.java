3ed26769a5835be986bc6a358ca6bb11d3d2c140
Nuwan-Harshakumara-Piyarathna <nuwan.harshamatrix@gmail.com>
2022-04-22 08:38:49 +0530

    saml validation

--- components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/validator/SAMLValidator.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/validator/SAMLValidator.java
@@ -2,15 +2,16 @@ package org.wso2.carbon.identity.application.mgt.validator;
 
 import org.wso2.carbon.identity.application.common.IdentityApplicationManagementException;
 import org.wso2.carbon.identity.application.common.model.InboundAuthenticationRequestConfig;
+import org.wso2.carbon.identity.application.common.model.Property;
 import org.wso2.carbon.identity.application.common.model.ServiceProvider;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SAMLValidator implements ApplicationValidator {
     @Override
     public int getOrderId() {
-        return 0;
+        return 1;
     }
 
     @Override
@@ -20,6 +21,7 @@ public class SAMLValidator implements ApplicationValidator {
                 serviceProvider.getInboundAuthenticationConfig().getInboundAuthenticationRequestConfigs()){
             if(inboundAuthenticationRequestConfig.getInboundAuthKey().equals("samlsso")){
                 validateSAMLProperties(validationErrors, inboundAuthenticationRequestConfig);
+                break;
             }
         }
         return validationErrors;
@@ -27,7 +29,9 @@ public class SAMLValidator implements ApplicationValidator {
 
     private void validateSAMLProperties(List<String> validationErrors, InboundAuthenticationRequestConfig inboundAuthenticationRequestConfig) {
         //TODO: add validations
+        Property[] properties = inboundAuthenticationRequestConfig.getProperties();
+        Map<String,List<String>> propertyMap = Arrays.stream(properties).collect(Collectors.groupingBy(Property::getName,Collectors.mapping(Property::getValue, Collectors.toList())));
     }
-
+    
 
 }
