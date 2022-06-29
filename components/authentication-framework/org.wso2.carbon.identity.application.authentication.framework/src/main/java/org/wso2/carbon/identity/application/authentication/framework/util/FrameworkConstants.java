838a3b5c2d03cc4a3e696cd5d0f2546404f5fe81
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2022-06-29 13:50:00 +0530

    add sessionDataKey to retry.do

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/util/FrameworkConstants.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/util/FrameworkConstants.java
@@ -109,6 +109,8 @@ public abstract class FrameworkConstants {
 
     public static final String REQUEST_PARAM_SP = "sp";
     public static final String REQUEST_PARAM_ERROR_KEY = "errorKey";
+    public static final String REQUEST_PARAM_SESSION_DATA_KEY = "sessionDataKey";
+
     public static final String MAPPED_ATTRIBUTES = "MappedAttributes";
     public static final String IDP_ID = "idpId";
     public static final String ASSOCIATED_ID = "associatedID";
