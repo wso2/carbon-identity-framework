20cc0473bdf496cf87dae38484fca737f0f43a9e
ChanikaRuchini <chanikaruchini.16@cse.mrt.ac.lk>
2022-06-30 22:42:25 +0530

    Add application-id as an event property

--- components/identity-event/org.wso2.carbon.identity.event/src/main/java/org/wso2/carbon/identity/event/IdentityEventConstants.java
+++ components/identity-event/org.wso2.carbon.identity.event/src/main/java/org/wso2/carbon/identity/event/IdentityEventConstants.java
@@ -320,6 +320,7 @@ public class IdentityEventConstants {
         public static final String UPDATED_DISABLED_STATUS = "updatedDisableStatus";
         public static final String CORRELATION_ID = "correlation-id";
         public static final String APPLICATION_NAME = "application-name";
+        public static final String APPLICATION_ID = "application-id";
         public static final String USER_AGENT = "user-agent";
         public static final String RESEND_CODE = "resend-code";
         public static final String GENERATED_OTP = "generated-otp";
