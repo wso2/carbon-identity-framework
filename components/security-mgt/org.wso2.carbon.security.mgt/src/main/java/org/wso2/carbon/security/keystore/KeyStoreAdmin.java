9583c256e40abad7f1c75c2264e1a78e3ab95125
Shalitha Pathiranage <shali23shri@gmail.com>
2022-07-03 18:24:35 +0530

    Add Method

--- components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/KeyStoreAdmin.java
+++ components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/KeyStoreAdmin.java
@@ -337,25 +337,9 @@ public class KeyStoreAdmin {
         }
     }
 
-    public void importCertToStore(String fileName, X509Certificate cert, KeyStore keyStore){
+    public void importCertToStore(String fileName, X509Certificate cert, String keyStoreName, KeyStore ks) throws SecurityConfigException {
 
-    }
-
-
-    public void importCertToStore(String fileName, String certData, String keyStoreName)
-            throws SecurityConfigException {
         try {
-            if (keyStoreName == null) {
-                throw new SecurityConfigException("Key Store name can't be null");
-            }
-
-
-
-            KeyStore ks = getKeyStore(keyStoreName);
-            X509Certificate cert = extractCertificate(certData);
-
-            importCertToStore();
-
             if(isCertificateExpired(cert)){
                 Exception e = new SecurityConfigException("Certificate is expired.");
                 log.error(e);
@@ -375,6 +359,27 @@ public class KeyStoreAdmin {
             if (isTrustStore(keyStoreName)) {
                 System.setProperty(IdentityUtil.PROP_TRUST_STORE_UPDATE_REQUIRED, "true");
             }
+        } catch (SecurityConfigException e) {
+            throw e;
+        } catch (Exception e) {
+            String msg = "Error when importing cert to the keyStore";
+            log.error(msg, e);
+            throw new SecurityConfigException(msg, e);
+        }
+
+    }
+
+    public void importCertToStore(String fileName, String certData, String keyStoreName)
+            throws SecurityConfigException {
+        try {
+            if (keyStoreName == null) {
+                throw new SecurityConfigException("Key Store name can't be null");
+            }
+
+            KeyStore ks = getKeyStore(keyStoreName);
+            X509Certificate cert = extractCertificate(certData);
+
+            importCertToStore(fileName,cert,keyStoreName,ks);
 
         } catch (SecurityConfigException e) {
             throw e;
