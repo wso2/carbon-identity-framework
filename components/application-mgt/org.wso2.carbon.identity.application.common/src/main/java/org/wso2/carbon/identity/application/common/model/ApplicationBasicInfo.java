b4e11e91fddc99c58de173e5d86c873d68b16e5d
Ravindu Ramesh Perera <ravinduramesh@gmail.com>
2022-07-28 11:27:06 +0530

    Added java doc comments and formatted the FilterData model code.

--- components/application-mgt/org.wso2.carbon.identity.application.common/src/main/java/org/wso2/carbon/identity/application/common/model/ApplicationBasicInfo.java
+++ components/application-mgt/org.wso2.carbon.identity.application.common/src/main/java/org/wso2/carbon/identity/application/common/model/ApplicationBasicInfo.java
@@ -58,7 +58,9 @@ public class ApplicationBasicInfo implements Serializable {
     }
 
     /**
-     * @return
+     * Get application name.
+     *
+     * @return application name
      */
     public String getApplicationName() {
 
@@ -66,7 +68,9 @@ public class ApplicationBasicInfo implements Serializable {
     }
 
     /**
-     * @param applicationName
+     * Set application name
+     *
+     * @param applicationName application name
      */
     public void setApplicationName(String applicationName) {
 
@@ -74,7 +78,9 @@ public class ApplicationBasicInfo implements Serializable {
     }
 
     /**
-     * @return
+     * Get application description
+     *
+     * @return description
      */
     public String getDescription() {
 
@@ -82,58 +88,110 @@ public class ApplicationBasicInfo implements Serializable {
     }
 
     /**
-     * @param description
+     * Set application description
+     *
+     * @param description description
      */
     public void setDescription(String description) {
 
         this.description = description;
     }
 
+    /**
+     * Get application resource id
+     *
+     * @return application resource id
+     */
     public String getApplicationResourceId() {
 
         return applicationResourceId;
     }
 
+    /**
+     * Set application resource id
+     *
+     * @param applicationResourceId application resource id
+     */
     public void setApplicationResourceId(String applicationResourceId) {
 
         this.applicationResourceId = applicationResourceId;
     }
 
+    /**
+     * Get image URL
+     *
+     * @return image URL
+     */
     public String getImageUrl() {
 
         return imageUrl;
     }
 
+    /**
+     * Set application image URL
+     *
+     * @param imageUrl application image URL
+     */
     public void setImageUrl(String imageUrl) {
 
         this.imageUrl = imageUrl;
     }
 
+    /**
+     * Get access URL
+     *
+     * @return access URL
+     */
     public String getAccessUrl() {
 
         return accessUrl;
     }
 
+    /**
+     * Set access URL
+     *
+     * @param accessUrl access URL
+     */
     public void setAccessUrl(String accessUrl) {
 
         this.accessUrl = accessUrl;
     }
 
+    /**
+     * Get app owner details
+     *
+     * @return app owner details
+     */
     public User getAppOwner() {
 
         return appOwner;
     }
 
+    /**
+     * Set application owner details
+     *
+     * @param appOwner application owner details
+     */
     public void setAppOwner(User appOwner) {
 
         this.appOwner = appOwner;
     }
 
+    /**
+     * Get inbound auth key
+     *
+     * @return inbound auth key
+     */
     public String getInboundKey() {
 
         return inboundKey;
     }
 
+    /**
+     * Set inbound auth key
+     *
+     * @param inboundKey inbound auth key
+     */
     public  void setInboundKey(String inboundKey) {
 
         this.inboundKey = inboundKey;
