c3523a2788e955ec53eb9252e5653cfdcd245076
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2022-08-17 14:40:52 +0530

    add not.authorized.for.authorization.grant.type tag

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.endpoint.util/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/Constants.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.endpoint.util/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/Constants.java
@@ -247,6 +247,8 @@ public class Constants {
                 "no.registered.device.found";
         public static final String INVALID_CLIENT_IN_TENANT = "invalid_client_no.valid.client.in.tenant";
         public static final String INVALID_CLIENT_IN_TENANT_I18N_KEY = "no.valid.client.in.tenant";
+        public static final String NOT_AUTHORIZED_FOR_AUTHORIZATION_GRANT_TYPE = "unauthorized_client_not.authorized.for.authorization.grant.type";
+        public static final String NOT_AUTHORIZED_FOR_AUTHORIZATION_GRANT_TYPE_I18N_KEY = "not.authorized.for.authorization.grant.type";
 
         private ErrorToi18nMappingConstants() {
 
