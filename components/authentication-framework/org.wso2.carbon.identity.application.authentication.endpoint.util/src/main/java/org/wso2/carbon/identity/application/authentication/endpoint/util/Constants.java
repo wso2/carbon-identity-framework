51a4b55fd33e4a7dbd5d1bf34083ac66c5813bca
Thamindu Aluthwala <thamindu.randil@gmail.com>
2022-09-01 10:26:25 +0530

    remove new line

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.endpoint.util/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/Constants.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.endpoint.util/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/Constants.java
@@ -36,7 +36,6 @@ public class Constants {
     public static final String USER_CLAIMS_CONSENT_ONLY = "userClaimsConsentOnly";
     public static final String CLAIM_SEPARATOR = ",";
     public static final String REQUEST_PARAM_SP = "sp";
-
     public static final String USERNAME = "username";
     public static final String PROMPT_FOR_ACCOUNT_LINKING = "prompt_for_account_linking";
     public static final String ATTEMPTED_USER = "attempted_user";
