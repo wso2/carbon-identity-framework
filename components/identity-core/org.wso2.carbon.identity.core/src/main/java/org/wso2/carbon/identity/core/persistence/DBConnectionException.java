94e7d9da61a2b3fe9e73167655eba497a6682efb
msm1992 <madusha.sachini@yahoo.com>
2023-01-23 19:59:02 +0530

    *Format code*

--- components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/persistence/DBConnectionException.java
+++ components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/persistence/DBConnectionException.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
- *
+ * <p>
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,33 +16,32 @@
  * under the License.
  */
 
- package org.wso2.carbon.identity.core.persistence;
+package org.wso2.carbon.identity.core.persistence;
 
- import org.wso2.carbon.identity.base.IdentityRuntimeException;
+import org.wso2.carbon.identity.base.IdentityRuntimeException;
 
- /**
-  * Custom exception used to handle DB connection unavailability issues
-  */
- public class DBConnectionException extends IdentityRuntimeException {
+/**
+ * Custom exception used to handle DB connection unavailability issues
+ */
+public class DBConnectionException extends IdentityRuntimeException {
 
-     public DBConnectionException(String message) {
+    public DBConnectionException(String message) {
 
-         super(message);
-     }
+        super(message);
+    }
 
-     public DBConnectionException(String errorCode, Throwable cause) {
+    public DBConnectionException(String errorCode, Throwable cause) {
 
-         super(errorCode, cause);
-     }
+        super(errorCode, cause);
+    }
 
-     public static DBConnectionException error(String message) {
+    public static DBConnectionException error(String message) {
 
-         return new DBConnectionException(message);
-     }
+        return new DBConnectionException(message);
+    }
 
-     public static DBConnectionException error(String errorDescription, Throwable cause) {
+    public static DBConnectionException error(String errorDescription, Throwable cause) {
 
-         return new DBConnectionException(errorDescription, cause);
-     }
- }
- 
+        return new DBConnectionException(errorDescription, cause);
+    }
+}
