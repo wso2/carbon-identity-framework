4c61c1e414edff8d953d4bec5b735b45e927db90
lasanthaS <lasn1987@gmail.com>
2023-03-16 13:57:36 +0530

    Add Enable PKCE checkbox in OIDC federated flow

--- components/application-mgt/org.wso2.carbon.identity.application.common/src/main/java/org/wso2/carbon/identity/application/common/util/IdentityApplicationConstants.java
+++ components/application-mgt/org.wso2.carbon.identity.application.common/src/main/java/org/wso2/carbon/identity/application/common/util/IdentityApplicationConstants.java
@@ -228,6 +228,7 @@ public class IdentityApplicationConstants {
             public static final String IS_USER_ID_IN_CLAIMS = "IsUserIdInClaims";
             public static final String IS_BASIC_AUTH_ENABLED = "IsBasicAuthEnabled";
             public static final String QUERY_PARAMS = "commonAuthQueryParams";
+            public static final String IS_PKCE_ENABLED = "IsPKCEEnabled";
         }
 
         /**
