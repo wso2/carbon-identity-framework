3a02dea11a154c08fba7a8a1914d4a9a81a41757
asha15 <165079T@uom.lk>
2023-03-22 07:22:08 +0530

    add java doc comment

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/exception/CookieValidationFailedException.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/exception/CookieValidationFailedException.java
@@ -1,14 +1,15 @@
 package org.wso2.carbon.identity.application.authentication.framework.exception;
 
-public class CookieValidationFailedException extends Exception{
+/**
+ * Session nonce Cookie Validation Failed Exception.
+ */
+public class CookieValidationFailedException extends Exception {
 
         public CookieValidationFailedException(String message) {
-
             super(message);
         }
 
         public CookieValidationFailedException(String message, Throwable cause) {
-
             super(message, cause);
         }
 }
