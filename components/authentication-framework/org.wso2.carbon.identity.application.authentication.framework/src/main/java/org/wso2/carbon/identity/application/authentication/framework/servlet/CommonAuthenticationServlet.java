736935fecd67336195ecc1a0bdd6a7329c86a400
Asha Sulaiman <165079T@uom.lk>
2023-04-18 11:12:17 +0530

    Remove the additional line

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/servlet/CommonAuthenticationServlet.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/servlet/CommonAuthenticationServlet.java
@@ -45,14 +45,12 @@ public class CommonAuthenticationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         commonAuthenticationHandler.doGet(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
         commonAuthenticationHandler.doGet(request, response);
     }
 
