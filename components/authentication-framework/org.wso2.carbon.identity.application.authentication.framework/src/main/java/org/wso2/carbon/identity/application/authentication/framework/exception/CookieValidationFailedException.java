cf03bc245768cb285ed7a6ef3ec515253f4909ae
Asha Sulaiman <165079T@uom.lk>
2023-04-18 12:09:31 +0530

    Add a new line

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/exception/CookieValidationFailedException.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/exception/CookieValidationFailedException.java
@@ -24,8 +24,8 @@ import java.io.IOException;
  * Session nonce Cookie Validation Failed Exception.
  */
 public class CookieValidationFailedException extends IOException {
+    
     public CookieValidationFailedException(String errorCode, String message) {
-
         super(errorCode + " : " + message);
     }
 }
