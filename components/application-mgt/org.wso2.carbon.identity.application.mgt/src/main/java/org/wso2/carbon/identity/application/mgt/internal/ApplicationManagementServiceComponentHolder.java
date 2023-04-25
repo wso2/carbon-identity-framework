1530d28da8b7b46036dc2d133b41e62926933882
sandushi <sandushidileka2@gmail.com>
2023-04-25 11:51:38 +0530

    Add consent url existence validation to application api

--- components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/internal/ApplicationManagementServiceComponentHolder.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/internal/ApplicationManagementServiceComponentHolder.java
@@ -21,6 +21,7 @@ import org.wso2.carbon.consent.mgt.core.ConsentManager;
 import org.wso2.carbon.identity.application.mgt.AbstractInboundAuthenticatorConfig;
 import org.wso2.carbon.identity.application.mgt.provider.ApplicationPermissionProvider;
 import org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataManagementService;
+import org.wso2.carbon.identity.consent.mgt.server.configs.services.ConsentManagementServerConfigsService;
 import org.wso2.carbon.identity.organization.management.service.OrganizationManagementInitialize;
 import org.wso2.carbon.identity.organization.management.service.OrganizationUserResidentResolverService;
 import org.wso2.carbon.registry.api.RegistryService;
@@ -59,6 +60,8 @@ public class ApplicationManagementServiceComponentHolder {
     private ApplicationPermissionProvider applicationPermissionProvider;
 
     private boolean isOrganizationManagementEnable = false;
+
+    private static ConsentManagementServerConfigsService consentManagementServerConfigsService;
 
     private ApplicationManagementServiceComponentHolder() {
 
@@ -248,5 +251,27 @@ public class ApplicationManagementServiceComponentHolder {
     public ApplicationPermissionProvider getApplicationPermissionProvider() {
 
         return applicationPermissionProvider;
+    }
+
+    /**
+     * Get Consent Management Server Configs Service.
+     *
+     * @return Consent Management Server Configs Service.
+     */
+    public static ConsentManagementServerConfigsService getConsentManagementServerConfigsService() {
+
+        return ApplicationManagementServiceComponentHolder.consentManagementServerConfigsService;
+    }
+
+    /**
+     * Set Consent Management Server Configs Service.
+     *
+     * @param consentManagementServerConfigsService Consent Management Server Configs Service.
+     */
+    public static void setConsentManagementServerConfigsService(ConsentManagementServerConfigsService
+                                                                        consentManagementServerConfigsService) {
+
+        ApplicationManagementServiceComponentHolder.consentManagementServerConfigsService =
+                consentManagementServerConfigsService;
     }
 }
