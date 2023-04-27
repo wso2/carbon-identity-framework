ae9e3f2a4fbe6171b1e508f873d6c19ceba817cd
sahandilshan <sahandilshan222@gmail.com>
2023-04-27 11:05:19 +0530

    Display message when authentication flow restarted
    Displaying a message to the user when the authentication flow got restarted due to the nonce validation failure

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/util/FrameworkConstants.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/util/FrameworkConstants.java
@@ -208,6 +208,8 @@ public abstract class FrameworkConstants {
     public static final String PRESERVE_LOCAL = "PRESERVE_LOCAL";
     public static final String RESTART_LOGIN_FLOW = "restartLoginFlow";
     public static final String INITIAL_CONTEXT = "initialContext";
+    public static final String RESTART_LOGIN_FLOW_QUERY_PARAMS =
+            "&authFailure=true&authFailureMsg=login.reinitiate.message";
 
     private FrameworkConstants() {
 
