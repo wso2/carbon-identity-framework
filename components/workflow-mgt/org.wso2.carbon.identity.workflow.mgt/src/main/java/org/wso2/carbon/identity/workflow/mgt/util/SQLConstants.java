b5a3ad9283bf6c49b2bda472067cc2bcdbed0552
malithiperera <malithiperera1998@gmail.com>
2023-04-28 18:29:00 +0530

    Addressed the comments

--- components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/main/java/org/wso2/carbon/identity/workflow/mgt/util/SQLConstants.java
+++ components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/main/java/org/wso2/carbon/identity/workflow/mgt/util/SQLConstants.java
@@ -56,14 +56,14 @@ public class SQLConstants {
     public static final String REQUEST_STATUS_COLUMN = "STATUS";
     public static final String REQUEST_ID_COLUMN = "REQUEST_ID";
     public static final String CREATED_BY_COLUMN = "CREATED_BY";
-
+    public static final String EXTERNAL_WORKFLOW_IDENTIFIER = "ExternalWorkflowIdentifier";
     public static final String ADD_WORKFLOW_REQUEST_QUERY = "INSERT INTO WF_REQUEST(UUID, CREATED_BY, OPERATION_TYPE," +
             " CREATED_AT, UPDATED_AT, REQUEST, STATUS, TENANT_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     public static final String GET_WORKFLOW_REQUEST_QUERY = "SELECT UUID, REQUEST, STATUS, CREATED_BY FROM WF_REQUEST" +
             " WHERE UUID = ?";
     public static final String GET_WORKFLOW_ID_QUERY = "SELECT PARAM_VALUE FROM WF_WORKFLOW_CONFIG_PARAM WHERE " +
-            "WORKFLOW_ID  = ? AND PARAM_NAME = 'ExternalWorkflowIdentifier'";
+            "WORKFLOW_ID  = ? AND PARAM_NAME = ? ";
     public static final String GET_TEMPLATE_ID_QUERY = "SELECT TEMPLATE_ID FROM WF_WORKFLOW WHERE ID = ? ";
     public static final String UPDATE_STATUS_OF_REQUEST = "UPDATE WF_REQUEST SET STATUS = ? , UPDATED_AT = ? WHERE " +
             "UUID = ?";
