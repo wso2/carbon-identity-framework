dbc5b7f238770aeaa0763b2fbe7b75259520a8f6
mpmadhavig <madhavi.17@cse.mrt.ac.lk>
2023-05-26 16:40:01 +0530

    Address PR comments

--- components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint.util/src/test/java/org/wso2/carbon/identity/mgt/endpoint/util/IdentityManagementServiceUtilTest.java
+++ components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint.util/src/test/java/org/wso2/carbon/identity/mgt/endpoint/util/IdentityManagementServiceUtilTest.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2023, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -43,11 +43,12 @@ public class IdentityManagementServiceUtilTest extends PowerMockTestCase {
     ServiceURL serviceURL;
     @Mock
     private ServiceURLBuilder serviceURLBuilder;
-    private IdentityManagementServiceUtil util = IdentityManagementServiceUtil.getInstance();
+    private final IdentityManagementServiceUtil util = IdentityManagementServiceUtil.getInstance();
     private static final String SERVICE_URL = "https://wso2.org:9443";
 
     /**
-     * This test tests IdentityManagementServiceUtil init method return a valid service URL.
+     * This test tests IdentityManagementServiceUtil init method return a valid service URL
+     * when the service URL is not configured from the property file.
      *
      * @throws Exception Exception.
      */
@@ -71,11 +72,10 @@ public class IdentityManagementServiceUtilTest extends PowerMockTestCase {
     }
 
     /**
-     * Prepare IdentityManagementServiceUtil.
+     * Prepare IdentityManagementServiceUtil by initializing properties.
      */
     private void prepareIdentityManagementServiceUtil() {
 
-        util = IdentityManagementServiceUtil.getInstance();
         util.init();
     }
 
