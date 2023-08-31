27d1b7d351d01714d50fa11aded56cce25f71815
mpmadhavig <madhavi.17@cse.mrt.ac.lk>
2023-08-31 13:54:23 +0530

    Remove keystore usages.

--- components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/SecurityServiceHolder.java
+++ components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/SecurityServiceHolder.java
@@ -19,46 +19,16 @@
 package org.wso2.carbon.security;
 
 import org.apache.axis2.context.ConfigurationContext;
-import org.wso2.carbon.registry.core.Registry;
-import org.wso2.carbon.registry.core.Resource;
-import org.wso2.carbon.registry.core.service.RegistryService;
-import org.wso2.carbon.registry.core.service.TenantRegistryLoader;
 import org.wso2.carbon.user.core.service.RealmService;
 import org.wso2.carbon.utils.ConfigurationContextService;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class SecurityServiceHolder {
-
-    private static RegistryService registryService;
 
     private static RealmService realmService;
 
     private static ConfigurationContextService ccService;
 
-    private static TenantRegistryLoader tenantRegistryLoader;
-
-    private static Map<String, Resource> policyResourceMap = new HashMap<>();
-
     private SecurityServiceHolder() {
-
-    }
-
-    public static RegistryService getRegistryService() {
-        return registryService;
-    }
-
-    public static void setRegistryService(RegistryService registryService) {
-        SecurityServiceHolder.registryService = registryService;
-    }
-
-    public static Registry getRegistry() throws Exception {
-        if (registryService == null) {
-            throw new SecurityConfigException("Registry Service is null");
-        }
-
-        return registryService.getConfigSystemRegistry();
 
     }
 
@@ -83,21 +53,4 @@ public class SecurityServiceHolder {
     public static void setConfigurationContextService(ConfigurationContextService ccService) {
         SecurityServiceHolder.ccService = ccService;
     }
-
-    public static void addPolicyResource(String location, Resource resource) {
-        policyResourceMap.put(location, resource);
-    }
-
-    public static TenantRegistryLoader getTenantRegistryLoader() {
-        return tenantRegistryLoader;
-    }
-
-    public static void setTenantRegistryLoader(TenantRegistryLoader tenantRegistryLoader) {
-        SecurityServiceHolder.tenantRegistryLoader = tenantRegistryLoader;
-    }
-
-    public static Map<String, Resource> getPolicyResourceMap() {
-        return policyResourceMap;
-    }
-
 }
