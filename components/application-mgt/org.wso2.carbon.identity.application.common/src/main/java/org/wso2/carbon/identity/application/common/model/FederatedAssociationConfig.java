1f29b72dda5c6c784342817d62910fc991a28c7f
Yasasr1 <yasasramanayaka@gmail.com>
2023-10-16 11:47:04 +0530

    add updated model classes

---
+++ components/application-mgt/org.wso2.carbon.identity.application.common/src/main/java/org/wso2/carbon/identity/application/common/model/FederatedAssociationConfig.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.application.common.model;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * This class represents the federated association configuration.
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "FederatedAssociationConfig")
+public class FederatedAssociationConfig implements Serializable {
+
+    private static final long serialVersionUID = 3689722862659524700L;
+    private static final String IS_ENABLED = "IsEnabled";
+    private static final String FEDERATED_ATTRIBUTE = "FederatedAttribute";
+    private static final String MAPPED_LOCAL_ATTRIBUTE = "MappedLocalAttribute";
+
+    @XmlElement(name = "IsEnabled")
+    private boolean isEnabled = false;
+
+    @XmlElement(name = "FederatedAttribute")
+    private String federatedAttribute;
+
+    @XmlElement(name = "MappedLocalAttribute")
+    private String mappedLocalAttribute;
+
+    public static FederatedAssociationConfig build(OMElement federatedAssociationConfigOM) {
+
+        FederatedAssociationConfig federatedAssociationConfig = new FederatedAssociationConfig();
+
+        if (federatedAssociationConfigOM == null) {
+            return federatedAssociationConfig;
+        }
+
+        Iterator<?> iter = federatedAssociationConfigOM.getChildElements();
+
+        while (iter.hasNext()) {
+            OMElement element = (OMElement) (iter.next());
+            String elementName = element.getLocalName();
+
+            if (IS_ENABLED.equals(elementName)) {
+                if (StringUtils.isNotBlank(element.getText())) {
+                    federatedAssociationConfig.setEnabled(Boolean.parseBoolean(element.getText()));
+                }
+            } else if (FEDERATED_ATTRIBUTE.equals(elementName)) {
+                federatedAssociationConfig.setFederatedAttribute(element.getText());
+            } else if (MAPPED_LOCAL_ATTRIBUTE.equals(elementName)) {
+                federatedAssociationConfig.setMappedLocalAttribute(element.getText());
+            }
+        }
+
+        return federatedAssociationConfig;
+    }
+
+    public void setEnabled(boolean enabled) {
+
+        isEnabled = enabled;
+    }
+
+    public String getFederatedAttribute() {
+
+        return federatedAttribute;
+    }
+
+    public boolean isEnabled() {
+
+        return isEnabled;
+    }
+
+    public void setFederatedAttribute(String federatedAttribute) {
+
+        this.federatedAttribute = federatedAttribute;
+    }
+
+    public String getMappedLocalAttribute() {
+
+        return mappedLocalAttribute;
+    }
+
+    public void setMappedLocalAttribute(String mappedLocalAttribute) {
+
+        this.mappedLocalAttribute = mappedLocalAttribute;
+    }
+
+}
