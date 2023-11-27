eecfa2800d7534907171fe0a637fb304bb21bb63
Sachin-Mamoru <sachinm@wso2.com>
2023-11-27 18:30:39 +0530

    updated arguments for string formatter

--- components/api-resource-mgt/org.wso2.carbon.identity.api.resource.mgt/src/main/java/org/wso2/carbon/identity/api/resource/mgt/dao/impl/APIResourceManagementDAOImpl.java
+++ components/api-resource-mgt/org.wso2.carbon.identity.api.resource.mgt/src/main/java/org/wso2/carbon/identity/api/resource/mgt/dao/impl/APIResourceManagementDAOImpl.java
@@ -739,8 +739,6 @@ public class APIResourceManagementDAOImpl implements APIResourceManagementDAO {
             return String.format(sqlStmtHead, limit) + filterQuery + String.format(sqlStmtTail, tenantId, sortOrder);
         } else if (databaseName.contains(SQLConstants.ORACLE)) {
             sqlStmtTail = SQLConstants.GET_API_RESOURCES_TAIL_ORACLE;
-
-            return String.format(sqlStmtHead, limit) + filterQuery + String.format(sqlStmtTail, tenantId, sortOrder);
         }
 
         return sqlStmtHead + filterQuery + String.format(sqlStmtTail, tenantId, sortOrder, limit);
