a3e05cd752354d63d40fea156dfe652487ecba22
Thisara-Welmilla <thisara@wso2.com>
2024-01-22 07:11:20 +0530

    Temp revert changes.

--- test-utils/org.wso2.carbon.identity.testutil/src/main/java/org/wso2/carbon/identity/testutil/ReadCertStoreSampleUtil.java
+++ test-utils/org.wso2.carbon.identity.testutil/src/main/java/org/wso2/carbon/identity/testutil/ReadCertStoreSampleUtil.java
@@ -41,9 +41,6 @@ import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.security.spec.PKCS8EncodedKeySpec;
 
-import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
-import org.wso2.carbon.utils.security.KeystoreUtils;
-
 public class ReadCertStoreSampleUtil {
 
     private static final String PRIVATE_KEY =
@@ -104,10 +101,8 @@ public class ReadCertStoreSampleUtil {
 
     public static KeyStore createKeyStore(Class clazz) throws Exception {
        clazz.getResource("");
-        File file = new File(clazz.getResource(KeystoreUtils.getKeyStoreFileLocation(
-                MultitenantConstants.SUPER_TENANT_DOMAIN_NAME)).getFile());
-        KeyStore keyStore = KeyStore.getInstance(KeystoreUtils.getKeyStoreFileExtension(
-                MultitenantConstants.SUPER_TENANT_DOMAIN_NAME));
+        File file = new File(clazz.getResource("/repository/resources/security/wso2carbon.jks").getFile());
+        KeyStore keyStore = KeyStore.getInstance("JKS");
         if (file.exists()) {
             // if exists, load
             keyStore.load(new FileInputStream(file), "wso2carbon".toCharArray());
@@ -122,8 +117,7 @@ public class ReadCertStoreSampleUtil {
     public static KeyPair getSampleKeyPair() throws CertificateException,
             NoSuchAlgorithmException, IOException, InvalidKeyException, KeyStoreException,
             NoSuchProviderException, SignatureException {
-        KeyStore keyStore = KeyStore.getInstance(KeystoreUtils.getKeyStoreFileExtension(
-                MultitenantConstants.SUPER_TENANT_DOMAIN_NAME));
+        KeyStore keyStore = KeyStore.getInstance("JKS");
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "SHA256WithRSA");
         SecureRandom random = SecureRandom.getInstance("RSA", "SHA256WithRSA");
         keyGen.initialize(1024, random);
