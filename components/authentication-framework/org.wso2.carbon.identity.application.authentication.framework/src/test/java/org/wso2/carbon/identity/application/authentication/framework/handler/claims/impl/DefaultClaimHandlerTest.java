5824443b798e75ba957dfcd1abc16920ddcac4db
SujanSanjula96 <sanjulasujan@gmail.com>
2024-01-30 16:44:55 +0530

    Update tests

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/test/java/org/wso2/carbon/identity/application/authentication/framework/handler/claims/impl/DefaultClaimHandlerTest.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/test/java/org/wso2/carbon/identity/application/authentication/framework/handler/claims/impl/DefaultClaimHandlerTest.java
@@ -22,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.ApplicationConfig;
@@ -38,6 +39,10 @@ import org.wso2.carbon.identity.application.common.model.ClaimMapping;
 import org.wso2.carbon.identity.application.common.model.IdPGroup;
 import org.wso2.carbon.identity.application.common.model.IdentityProvider;
 import org.wso2.carbon.identity.application.common.model.ServiceProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -63,7 +68,8 @@ public class DefaultClaimHandlerTest {
     private static final String testIdPGroupId = "testIdPGroupId";
     private static final String testIdPGroupName = "admin";
     private static final String applicationId = "testAppId";
-    private static final String[] mappedApplicationRoles = new String[]{"adminMapped", "hrMapped"};
+    private static final List<String> mappedApplicationRoles =
+            new ArrayList<>(Arrays.asList("adminMapped", "hrMapped"));
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -72,7 +78,7 @@ public class DefaultClaimHandlerTest {
     }
 
     @Test
-    public void testHandleApplicationRolesForFederatedUser() throws Exception {
+    public void testGetAppAssociatedRolesOfFederatedUser() throws Exception {
 
         DefaultClaimHandler defaultClaimHandler = new DefaultClaimHandler();
 
@@ -104,15 +110,44 @@ public class DefaultClaimHandlerTest {
         when(applicationConfig.getServiceProvider()).thenReturn(serviceProvider);
         when(authenticationContext.getExternalIdP()).thenReturn(externalIdPConfig);
 
-        mockStatic(FrameworkServiceDataHolder.class);
-        when(FrameworkServiceDataHolder.getInstance()).thenReturn(frameworkServiceDataHolder);
-        when(frameworkServiceDataHolder.getHighestPriorityApplicationRolesResolver()).thenReturn(
-                applicationRolesResolver);
+        mockStatic(FrameworkUtils.class);
+        when(FrameworkUtils.getMultiAttributeSeparator()).thenReturn(",");
+        when(FrameworkUtils.getIdpGroupClaimUri(eq(stepConfig), eq(authenticationContext))).thenReturn(
+                idpGroupClaimName);
+        when(FrameworkUtils.getAppAssociatedRolesOfFederatedUser(
+                eq(authenticatedUser), eq(applicationId), eq(idpGroupClaimName))).thenReturn(mappedApplicationRoles);
+
+        List<String> applicationRoles =
+                defaultClaimHandler.getAppAssociatedRolesOfFederatedUser(stepConfig, authenticationContext);
+
+        Assert.assertEquals(applicationRoles, mappedApplicationRoles);
+    }
+
+    @Test
+    public void testGetAppAssociatedRolesOfLocalUser() throws Exception {
+
+        DefaultClaimHandler defaultClaimHandler = new DefaultClaimHandler();
+
+        StepConfig stepConfig = new StepConfig();
+        stepConfig.setSubjectAttributeStep(true);
+        AuthenticatedUser authenticatedUser = new AuthenticatedUser();
+        stepConfig.setAuthenticatedUser(authenticatedUser);
+
+        ServiceProvider serviceProvider = new ServiceProvider();
+        serviceProvider.setApplicationResourceId(applicationId);
+
+        when(authenticationContext.getSequenceConfig()).thenReturn(sequenceConfig);
+        when(sequenceConfig.getApplicationConfig()).thenReturn(applicationConfig);
+        when(applicationConfig.getServiceProvider()).thenReturn(serviceProvider);
 
         mockStatic(FrameworkUtils.class);
         when(FrameworkUtils.getMultiAttributeSeparator()).thenReturn(",");
-
-        when(applicationRolesResolver.getRoles(eq(authenticatedUser), eq(applicationId))).thenReturn(
+        when(FrameworkUtils.getAppAssociatedRolesOfLocalUser(eq(authenticatedUser), eq(applicationId))).thenReturn(
                 mappedApplicationRoles);
+
+        List<String> applicationRoles =
+                defaultClaimHandler.getAppAssociatedRolesOfLocalUser(stepConfig, authenticationContext);
+
+        Assert.assertEquals(applicationRoles, mappedApplicationRoles);
     }
 }
