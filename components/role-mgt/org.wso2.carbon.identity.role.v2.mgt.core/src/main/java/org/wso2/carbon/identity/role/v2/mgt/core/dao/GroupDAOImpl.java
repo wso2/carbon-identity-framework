bca0da93068254a8c2eb88f3a6864e883b6b4955
somindagamage <sominda@wso2.com>
2024-01-31 16:50:10 -0500

    Invoke user core APIs when resolving groups

--- components/role-mgt/org.wso2.carbon.identity.role.v2.mgt.core/src/main/java/org/wso2/carbon/identity/role/v2/mgt/core/dao/GroupDAOImpl.java
+++ components/role-mgt/org.wso2.carbon.identity.role.v2.mgt.core/src/main/java/org/wso2/carbon/identity/role/v2/mgt/core/dao/GroupDAOImpl.java
@@ -18,25 +18,24 @@
 
 package org.wso2.carbon.identity.role.v2.mgt.core.dao;
 
-import org.wso2.carbon.database.utils.jdbc.NamedPreparedStatement;
-import org.wso2.carbon.identity.core.util.IdentityDatabaseUtil;
-import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
-import org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants;
 import org.wso2.carbon.identity.role.v2.mgt.core.exception.IdentityRoleManagementClientException;
 import org.wso2.carbon.identity.role.v2.mgt.core.exception.IdentityRoleManagementException;
 import org.wso2.carbon.identity.role.v2.mgt.core.exception.IdentityRoleManagementServerException;
+import org.wso2.carbon.identity.role.v2.mgt.core.internal.RoleManagementServiceComponentHolder;
+import org.wso2.carbon.user.api.UserStoreManager;
+import org.wso2.carbon.user.core.UserStoreClientException;
+import org.wso2.carbon.user.core.UserStoreException;
+import org.wso2.carbon.user.core.common.AbstractUserStoreManager;
+import org.wso2.carbon.user.core.service.RealmService;
+import org.wso2.carbon.user.core.util.UserCoreUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.Error.INVALID_REQUEST;
 import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.Error.UNEXPECTED_SERVER_ERROR;
-import static org.wso2.carbon.identity.role.v2.mgt.core.dao.SQLQueries.GET_GROUP_ID_BY_NAME_SQL;
-import static org.wso2.carbon.identity.role.v2.mgt.core.dao.SQLQueries.GET_GROUP_NAME_BY_ID_SQL;
+import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.Error.UNSUPPORTED_USER_STORE_MANAGER;
 
 /**
  * RoleDAO Implementation.
@@ -46,160 +45,111 @@ public class GroupDAOImpl implements GroupDAO {
     @Override
     public String getGroupNameByID(String id, String tenantDomain) throws IdentityRoleManagementException {
 
-        int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
-        String groupName = null;
-        try (Connection connection = IdentityDatabaseUtil.getDBConnection(false)) {
-            try (NamedPreparedStatement statement = new NamedPreparedStatement(connection, GET_GROUP_NAME_BY_ID_SQL)) {
-                statement.setInt(RoleConstants.RoleTableColumns.TENANT_ID, tenantId);
-                statement.setString(RoleConstants.RoleTableColumns.ATTR_NAME, RoleConstants.ID_URI);
-                statement.setString(RoleConstants.RoleTableColumns.ATTR_VALUE, id);
-                int count = 0;
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    while (resultSet.next()) {
-                        // Handle multiple matching groups.
-                        count++;
-                        if (count > 1) {
-                            String errorMessage =
-                                    "Invalid scenario. Multiple groups found for the given group ID: " + id + " and "
-                                            + "tenantDomain: " + tenantDomain;
-                            throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage);
-                        }
-                        groupName = resultSet.getString(1);
-                    }
-                }
+        try {
+            AbstractUserStoreManager userStoreManager = getUserStoreManager(tenantDomain);
+            return userStoreManager.getGroupNameByGroupId(id);
+        } catch (UserStoreException e) {
+            String errorMessage = String.format(
+                    "Error while resolving the group name for the given group ID: %s " + "and tenantDomain: %s", id,
+                    tenantDomain);
+            if (e instanceof UserStoreClientException) {
+                throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage, e);
             }
-        } catch (SQLException e) {
-            String errorMessage =
-                    "Error while resolving the group name for the given group ID: " + id + " and tenantDomain: "
-                            + tenantDomain;
             throw new IdentityRoleManagementServerException(UNEXPECTED_SERVER_ERROR.getCode(), errorMessage, e);
         }
-        return groupName;
     }
 
     @Override
     public Map<String, String> getGroupNamesByIDs(List<String> ids, String tenantDomain)
             throws IdentityRoleManagementException {
 
-        Map<String, String> groupIdsToNames;
-        try (Connection connection = IdentityDatabaseUtil.getDBConnection(false)) {
-            groupIdsToNames = batchProcessGroupIDs(ids, tenantDomain, connection);
-        } catch (SQLException e) {
+        try {
+            Map<String, String> groupIdsToNames = new HashMap<>();
+            AbstractUserStoreManager userStoreManager = getUserStoreManager(tenantDomain);
+            for (String id : ids) {
+                groupIdsToNames.put(id, userStoreManager.getGroupNameByGroupId(id));
+            }
+            return groupIdsToNames;
+        } catch (UserStoreException e) {
             String errorMessage =
                     "Error while resolving the group name for the given group Ids in the tenantDomain: " + tenantDomain;
+            if (e instanceof UserStoreClientException) {
+                throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage, e);
+            }
             throw new IdentityRoleManagementServerException(UNEXPECTED_SERVER_ERROR.getCode(), errorMessage, e);
         }
-        return groupIdsToNames;
-    }
-
-    private Map<String, String> batchProcessGroupIDs(List<String> ids, String tenantDomain, Connection connection)
-            throws SQLException, IdentityRoleManagementException {
-
-        Map<String, String> groupIdsToNames = new HashMap<>();
-        int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
-        String groupName;
-        for (String id : ids) {
-            try (NamedPreparedStatement statement = new NamedPreparedStatement(connection, GET_GROUP_NAME_BY_ID_SQL)) {
-                statement.setInt(RoleConstants.RoleTableColumns.TENANT_ID, tenantId);
-                statement.setString(RoleConstants.RoleTableColumns.ATTR_NAME, RoleConstants.ID_URI);
-                statement.setString(RoleConstants.RoleTableColumns.ATTR_VALUE, id);
-                int count = 0;
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    while (resultSet.next()) {
-                        // Handle multiple matching groups.
-                        count++;
-                        if (count > 1) {
-                            String errorMessage =
-                                    "Invalid scenario. Multiple groups found for the given group ID: " + id + " and "
-                                            + "tenantDomain: " + tenantDomain;
-                            throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage);
-                        }
-                        groupName = resultSet.getString(1);
-                        groupIdsToNames.put(id, groupName);
-                    }
-                }
-            }
-        }
-        return groupIdsToNames;
     }
 
     @Override
     public String getGroupIDByName(String name, String tenantDomain) throws IdentityRoleManagementException {
 
-        int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
-        String groupID = null;
-        try (Connection connection = IdentityDatabaseUtil.getDBConnection(false)) {
-            try (NamedPreparedStatement statement = new NamedPreparedStatement(connection, GET_GROUP_ID_BY_NAME_SQL)) {
-                statement.setInt(RoleConstants.RoleTableColumns.TENANT_ID, tenantId);
-                statement.setString(RoleConstants.RoleTableColumns.ROLE_NAME, name);
-                statement.setString(RoleConstants.RoleTableColumns.ATTR_NAME, RoleConstants.ID_URI);
-                int count = 0;
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    while (resultSet.next()) {
-                        // Handle multiple matching groups.
-                        count++;
-                        if (count > 1) {
-                            String errorMessage =
-                                    "Invalid scenario. Multiple groups found for the given group name: " + name + " "
-                                            + "and tenantDomain: " + tenantDomain;
-                            throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage);
-                        }
-                        groupID = resultSet.getString(1);
-                    }
-                }
+        String domainAwareName = UserCoreUtil.addDomainToName(name, tenantDomain);
+        try {
+            AbstractUserStoreManager userStoreManager = getUserStoreManager(tenantDomain);
+            return userStoreManager.getGroupIdByGroupName(UserCoreUtil.addDomainToName(name, domainAwareName));
+        } catch (UserStoreException e) {
+            String errorMessage = String.format(
+                    "Error while resolving the group id for the given group: %s " + "and tenantDomain: %s",
+                    domainAwareName, tenantDomain);
+            if (e instanceof UserStoreClientException) {
+                throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage, e);
             }
-        } catch (SQLException e) {
-            String errorMessage =
-                    "Error while resolving the group ID for the given group name: " + name + " and tenantDomain: "
-                            + tenantDomain;
             throw new IdentityRoleManagementServerException(UNEXPECTED_SERVER_ERROR.getCode(), errorMessage, e);
         }
-        return groupID;
     }
 
     @Override
     public Map<String, String> getGroupIDsByNames(List<String> names, String tenantDomain)
             throws IdentityRoleManagementException {
 
-        Map<String, String> groupNamesToIDs;
-        try (Connection connection = IdentityDatabaseUtil.getDBConnection(false)) {
-            groupNamesToIDs = batchProcessGroupNames(names, tenantDomain, connection);
-        } catch (SQLException e) {
+        try {
+            Map<String, String> groupNamesToIDs = new HashMap<>();
+            AbstractUserStoreManager userStoreManager = getUserStoreManager(tenantDomain);
+            for (String name : names) {
+                String domainAwareName = UserCoreUtil.addDomainToName(name, tenantDomain);
+                groupNamesToIDs.put(name,
+                        userStoreManager.getGroupIdByGroupName(UserCoreUtil.addDomainToName(name, domainAwareName)));
+            }
+            return groupNamesToIDs;
+        } catch (UserStoreException e) {
             String errorMessage =
                     "Error while resolving the group ID for the given group names in the tenantDomain: " + tenantDomain;
+            if (e instanceof UserStoreClientException) {
+                throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage, e);
+            }
             throw new IdentityRoleManagementServerException(UNEXPECTED_SERVER_ERROR.getCode(), errorMessage, e);
         }
-        return groupNamesToIDs;
     }
 
-    private Map<String, String> batchProcessGroupNames(List<String> names, String tenantDomain, Connection connection)
-            throws SQLException, IdentityRoleManagementException {
+    /**
+     * Get the AbstractUserStoreManager for the given tenant domain.
+     *
+     * @param tenantDomain Tenant domain.
+     * @return UserStoreManager.
+     * @throws IdentityRoleManagementServerException If an error occurred while getting the AbstractUserStoreManager
+     *                                               instance of the tenant.
+     */
+    private AbstractUserStoreManager getUserStoreManager(String tenantDomain)
+            throws IdentityRoleManagementServerException {
 
-        Map<String, String> groupNamesToIDs = new HashMap<>();
-        int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
-        String groupID;
-        for (String name : names) {
-            try (NamedPreparedStatement statement = new NamedPreparedStatement(connection, GET_GROUP_ID_BY_NAME_SQL)) {
-                statement.setInt(RoleConstants.RoleTableColumns.TENANT_ID, tenantId);
-                statement.setString(RoleConstants.RoleTableColumns.ROLE_NAME, name);
-                statement.setString(RoleConstants.RoleTableColumns.ATTR_NAME, RoleConstants.ID_URI);
-                int count = 0;
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    while (resultSet.next()) {
-                        // Handle multiple matching groups.
-                        count++;
-                        if (count > 1) {
-                            String errorMessage =
-                                    "Invalid scenario. Multiple groups found for the given group name: " + name + " "
-                                            + "and tenantDomain: " + tenantDomain;
-                            throw new IdentityRoleManagementClientException(INVALID_REQUEST.getCode(), errorMessage);
-                        }
-                        groupID = resultSet.getString(1);
-                        groupNamesToIDs.put(name, groupID);
-                    }
-                }
-            }
+        RealmService realmService = RoleManagementServiceComponentHolder.getInstance().getRealmService();
+        UserStoreManager userStoreManager;
+        try {
+            userStoreManager =
+                    realmService.getTenantUserRealm(realmService.getTenantManager().getTenantId(tenantDomain))
+                            .getUserStoreManager();
+        } catch (org.wso2.carbon.user.api.UserStoreException e) {
+            throw new IdentityRoleManagementServerException(UNEXPECTED_SERVER_ERROR.getCode(),
+                    "Error occurred while getting the userStoreManager for tenant: " + tenantDomain, e);
         }
-        return groupNamesToIDs;
+        if (userStoreManager == null) {
+            throw new IdentityRoleManagementServerException(UNEXPECTED_SERVER_ERROR.getCode(),
+                    "Error occurred while getting the userStoreManager for tenant: " + tenantDomain);
+        }
+        if (!(userStoreManager instanceof AbstractUserStoreManager)) {
+            throw new IdentityRoleManagementServerException(UNSUPPORTED_USER_STORE_MANAGER.getCode(),
+                    "Underlying userStoreManager does not support getGroupNameByID operation " + tenantDomain);
+        }
+        return (AbstractUserStoreManager) userStoreManager;
     }
 }
