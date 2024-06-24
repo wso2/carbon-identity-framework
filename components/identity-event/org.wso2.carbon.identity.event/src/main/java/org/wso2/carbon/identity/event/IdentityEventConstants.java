181bfceeb28375d05af5c54d7ec09f0060149d18
Sachith Manchanayaka <42811248+Manchiz@users.noreply.github.com>
2024-06-24 12:47:53 +0530

    Introduce SERVICE_PROVIDER_UUID EventProperty (#5758)

--- components/identity-event/org.wso2.carbon.identity.event/src/main/java/org/wso2/carbon/identity/event/IdentityEventConstants.java
+++ components/identity-event/org.wso2.carbon.identity.event/src/main/java/org/wso2/carbon/identity/event/IdentityEventConstants.java
@@ -321,6 +321,7 @@ public class IdentityEventConstants {
         public static final String CORRELATION_ID = "correlation-id";
         public static final String APPLICATION_NAME = "application-name";
         public static final String APPLICATION_ID = "application-id";
+        public static final String SERVICE_PROVIDER_UUID = "serviceProviderUUID";
         public static final String USER_AGENT = "user-agent";
         public static final String RESEND_CODE = "resend-code";
         public static final String GENERATED_OTP = "generated-otp";
