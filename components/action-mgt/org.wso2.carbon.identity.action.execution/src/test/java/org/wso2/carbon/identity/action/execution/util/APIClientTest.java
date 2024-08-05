c78a7c1f2394ef7bdc1d1ea59fcc9b61f1f1bbaf
malithie <malithim@wso2.com>
2024-08-05 06:30:43 +0530

    Add unit tests with 0.57 coverage.

---
+++ components/action-mgt/org.wso2.carbon.identity.action.execution/src/test/java/org/wso2/carbon/identity/action/execution/util/APIClientTest.java
@@ -0,0 +1,301 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.action.execution.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.wso2.carbon.identity.action.execution.model.ActionInvocationErrorResponse;
+import org.wso2.carbon.identity.action.execution.model.ActionInvocationResponse;
+import org.wso2.carbon.identity.action.execution.model.ActionInvocationSuccessResponse;
+import org.wso2.carbon.identity.action.execution.model.Operation;
+
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Field;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
+public class APIClientTest {
+
+    @Mock
+    private CloseableHttpClient httpClient;
+
+    @Mock
+    private CloseableHttpResponse httpResponse;
+
+    @Mock
+    private StatusLine statusLine;
+
+    @InjectMocks
+    private APIClient apiClient;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.openMocks(this);
+        setField(apiClient, "httpClient", httpClient);
+    }
+
+    @Test
+    public void testCallAPIUnacceptableContentTypeForSuccessResponse()
+            throws Exception {
+
+        when(httpClient.execute(any(HttpPost.class))).thenReturn(httpResponse);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn(HttpStatus.SC_OK);
+        InputStreamEntity entity =
+                new InputStreamEntity(new ByteArrayInputStream("{}".getBytes(StandardCharsets.UTF_8)));
+        entity.setContentType(ContentType.DEFAULT_TEXT.getMimeType());
+        when(httpResponse.getEntity()).thenReturn(entity);
+
+        ActionInvocationResponse apiResponse = apiClient.callAPI("http://example.com", null, "{}");
+        assertNotNull(apiResponse);
+        assertNull(apiResponse.getResponse());
+        assertFalse(apiResponse.isRetry());
+        assertNotNull(apiResponse.getErrorLog());
+        assertEquals(apiResponse.getErrorLog(),
+                "Unexpected response for status code: 200. The response content type is not application/json.");
+    }
+
+    @DataProvider(name = "unacceptableSuccessResponsePayloads")
+    public String[] unacceptableSuccessResponsePayloads() {
+
+        return new String[]{"{}", "", "success", "{\"actionStatus\":\"SUCCESS\"}"};
+    }
+
+    @Test(dataProvider = "unacceptableSuccessResponsePayloads")
+    public void testCallAPIUnacceptablePayloadForSuccessResponse(String payload)
+            throws Exception {
+
+        when(httpClient.execute(any(HttpPost.class))).thenReturn(httpResponse);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn(HttpStatus.SC_OK);
+        InputStreamEntity entity =
+                new InputStreamEntity(new ByteArrayInputStream(payload.getBytes(StandardCharsets.UTF_8)));
+        entity.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+        when(httpResponse.getEntity()).thenReturn(entity);
+
+        ActionInvocationResponse apiResponse = apiClient.callAPI("http://example.com", null, "{}");
+        assertNotNull(apiResponse);
+        assertTrue(apiResponse.isError());
+        assertFalse(apiResponse.isRetry());
+        assertNotNull(apiResponse.getErrorLog());
+        assertEquals(apiResponse.getErrorLog(),
+                "Unexpected response for status code: 200. Parsing JSON response failed.");
+    }
+
+    @Test
+    public void testCallAPIAcceptablePayloadForSuccessResponse() throws Exception {
+
+        String successResponse =
+                "{\"actionStatus\":\"SUCCESS\",\"operations\":[" +
+                        "{\"op\":\"add\",\"path\":\"/accessToken/claims/-\",\"" +
+                        "value\":{\"name\":\"customSID\",\"value\":\"12345\"}}]}";
+
+        when(httpClient.execute(any(HttpPost.class))).thenReturn(httpResponse);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn(HttpStatus.SC_OK);
+
+        InputStreamEntity entity = new InputStreamEntity(new ByteArrayInputStream(successResponse.getBytes(
+                StandardCharsets.UTF_8)));
+        entity.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+        when(httpResponse.getEntity()).thenReturn(entity);
+
+        ActionInvocationResponse apiResponse = apiClient.callAPI("http://example.com", null, "{}");
+
+        assertNotNull(apiResponse);
+        assertNotNull(apiResponse.getResponse());
+        assertEquals(apiResponse.getResponse().getActionStatus(), ActionInvocationResponse.Status.SUCCESS);
+        assertTrue(apiResponse.getResponse() instanceof ActionInvocationSuccessResponse);
+        ((ActionInvocationSuccessResponse) apiResponse.getResponse()).getOperations().forEach(operation -> {
+            assertEquals(operation.getOp(), Operation.ADD);
+            assertEquals(operation.getPath(), "/accessToken/claims/-");
+            assertTrue(operation.getValue() instanceof HashMap);
+            ((HashMap<String, String>) operation.getValue()).forEach((key, value) -> {
+                if ("name".equals(key)) {
+                    assertEquals(value, "customSID");
+                } else if ("value".equals(key)) {
+                    assertEquals(value, "12345");
+                }
+            });
+        });
+        assertFalse(apiResponse.isRetry());
+        assertNull(apiResponse.getErrorLog());
+    }
+
+    @DataProvider(name = "unexpectedErrorResponses")
+    public Object[][] unexpectedErrorResponses() {
+
+        return new Object[][]{
+                {HttpStatus.SC_BAD_REQUEST, ContentType.DEFAULT_TEXT.getMimeType(),
+                        "", "Failed to execute the action request. Received: 400"},
+                {HttpStatus.SC_UNAUTHORIZED, ContentType.APPLICATION_JSON.getMimeType(),
+                        "{}", "Failed to execute the action request. Received: 401"},
+                {HttpStatus.SC_BAD_REQUEST, ContentType.APPLICATION_JSON.getMimeType(),
+                        "{\"actionStatus\":\"ERROR\"}", "Failed to execute the action request. Received: 400"},
+                {HttpStatus.SC_INTERNAL_SERVER_ERROR, ContentType.APPLICATION_JSON.getMimeType(),
+                        "server_error", "Failed to execute the action request. Received: 500"},
+                {HttpStatus.SC_BAD_GATEWAY, ContentType.DEFAULT_TEXT.getMimeType(),
+                        "", "Failed to execute the action request. Received: 502"},
+                {HttpStatus.SC_CONFLICT, ContentType.DEFAULT_TEXT.getMimeType(),
+                        "", "Unexpected response status code: 409"}
+        };
+    }
+
+    @Test(dataProvider = "unexpectedErrorResponses")
+    public void testCallAPIUnexpectedErrorResponse(Object statusCode, Object contentType, Object payload,
+                                                   Object expectedErrorLog)
+            throws Exception {
+
+        when(httpClient.execute(any(HttpPost.class))).thenReturn(httpResponse);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn((int) statusCode);
+        InputStreamEntity entity =
+                new InputStreamEntity(new ByteArrayInputStream(payload.toString().getBytes(StandardCharsets.UTF_8)));
+        entity.setContentType(contentType.toString());
+        when(httpResponse.getEntity()).thenReturn(entity);
+
+        ActionInvocationResponse apiResponse = apiClient.callAPI("http://example.com", null, "{}");
+        assertNotNull(apiResponse);
+        assertTrue(apiResponse.isError());
+        if ((int) statusCode == 500 || (int) statusCode == 502) { // This is a retry
+            assertTrue(apiResponse.isRetry());
+            verify(httpClient, times(2)).execute(any(HttpPost.class));
+        } else {
+            assertFalse(apiResponse.isRetry());
+        }
+        assertNotNull(apiResponse.getErrorLog());
+        assertEquals(apiResponse.getErrorLog(), expectedErrorLog.toString());
+    }
+
+    @DataProvider(name = "acceptableErrorResponsePayloads")
+    public Object[][] acceptableErrorResponses() {
+
+        return new Object[][]{
+                {HttpStatus.SC_BAD_REQUEST,
+                        "{\"actionStatus\":\"ERROR\",\"error\":\"invalid_request\"," +
+                                "\"errorDescription\":\"client_id is missing\"}"},
+                {HttpStatus.SC_INTERNAL_SERVER_ERROR,
+                        "{\"actionStatus\":\"ERROR\",\"error\":\"server_error\"," +
+                                "\"errorDescription\":\"internal server error\"}"},
+                {HttpStatus.SC_UNAUTHORIZED,
+                        "{\"actionStatus\":\"ERROR\",\"error\":\"access_denied\"," +
+                                "\"errorDescription\":\"scope validation failed\"}"}
+        };
+    }
+
+    @Test(dataProvider = "acceptableErrorResponsePayloads")
+    public void testCallAPIAcceptablePayloadForErrorResponse(Object statusCode, Object payload) throws Exception {
+
+        when(httpClient.execute(any(HttpPost.class))).thenReturn(httpResponse);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn((int) statusCode);
+
+        InputStreamEntity entity = new InputStreamEntity(new ByteArrayInputStream(payload.toString().getBytes(
+                StandardCharsets.UTF_8)));
+        entity.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+        when(httpResponse.getEntity()).thenReturn(entity);
+
+        ActionInvocationResponse apiResponse = apiClient.callAPI("http://example.com", null, "{}");
+
+        assertNotNull(apiResponse);
+        assertNotNull(apiResponse.getResponse());
+        assertEquals(apiResponse.getResponse().getActionStatus(), ActionInvocationResponse.Status.ERROR);
+        assertTrue(apiResponse.getResponse() instanceof ActionInvocationErrorResponse);
+        ActionInvocationErrorResponse errorResponseObject =
+                ((ActionInvocationErrorResponse) apiResponse.getResponse());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(payload.toString());
+        String error = rootNode.path("error").asText();
+        String errorDescription = rootNode.path("errorDescription").asText();
+        assertEquals(errorResponseObject.getError(), error);
+        assertEquals(errorResponseObject.getErrorDescription(), errorDescription);
+        assertNull(apiResponse.getErrorLog());
+    }
+
+    @Test
+    public void testCallAPIRetryOnTimeoutAndReceiveSuccessResponse() throws Exception {
+
+        when(httpClient.execute(any(HttpPost.class))).thenThrow(new ConnectTimeoutException("Timeout"))
+                .thenReturn(httpResponse);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn(200);
+
+        String successResponse =
+                "{\"actionStatus\":\"SUCCESS\",\"operations\":[" +
+                        "{\"op\":\"add\",\"path\":\"/accessToken/claims/-\",\"" +
+                        "value\":{\"name\":\"customSID\",\"value\":\"12345\"}}]}";
+        InputStreamEntity entity = new InputStreamEntity(new ByteArrayInputStream(successResponse.getBytes(
+                StandardCharsets.UTF_8)));
+        entity.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+        when(httpResponse.getEntity()).thenReturn(entity);
+
+        ActionInvocationResponse response = apiClient.callAPI("http://example.com", null, "{}");
+
+        assertNotNull(response);
+        assertTrue(response.isSuccess());
+        verify(httpClient, times(2)).execute(any(HttpPost.class));
+    }
+
+    @Test
+    public void testCallAPIRetryOnTimeoutAndReachMaxRetryAttempts() throws Exception {
+
+        when(httpClient.execute(any(HttpPost.class))).thenThrow(new ConnectTimeoutException("Connection Timeout"))
+                .thenThrow(new SocketTimeoutException("Read Timeout"));
+
+        ActionInvocationResponse response = apiClient.callAPI("http://example.com", null, "{}");
+
+        assertNotNull(response);
+        assertTrue(response.isError());
+        assertEquals(response.getErrorLog(), "Failed to execute the action request or maximum retry attempts reached.");
+        verify(httpClient, times(2)).execute(any(HttpPost.class));
+    }
+
+    private void setField(Object target, String fieldName, Object value) throws Exception {
+
+        Field field = target.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(target, value);
+    }
+}
