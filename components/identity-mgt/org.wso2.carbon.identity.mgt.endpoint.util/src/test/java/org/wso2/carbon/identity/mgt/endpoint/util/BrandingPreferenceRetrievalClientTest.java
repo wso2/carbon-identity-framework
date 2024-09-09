ed07ac8981da3b56388b054d072143de8ae2f149
sadilchamishka <sandilchamishka@gmail.com>
2024-09-09 14:21:37 +0530

    Add unit tests for preference retrieval clients

---
+++ components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint.util/src/test/java/org/wso2/carbon/identity/mgt/endpoint/util/BrandingPreferenceRetrievalClientTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.mgt.endpoint.util;
+
+import org.mockito.MockedStatic;
+import org.testng.annotations.Test;
+import org.wso2.carbon.identity.mgt.endpoint.util.client.BrandingPreferenceRetrievalClient;
+import org.wso2.carbon.identity.mgt.endpoint.util.client.BrandingPreferenceRetrievalClientException;
+import org.wso2.carbon.utils.HTTPClientUtils;
+
+import static org.mockito.Mockito.mockStatic;
+
+public class BrandingPreferenceRetrievalClientTest extends RetrievalClientBaseTest {
+
+    private final BrandingPreferenceRetrievalClient preferenceRetrievalClient =
+            new BrandingPreferenceRetrievalClient();
+
+    @Test
+    public void testGetPreference() throws BrandingPreferenceRetrievalClientException {
+
+        try (MockedStatic<IdentityManagementServiceUtil> identityMgtServiceUtil = mockStatic(IdentityManagementServiceUtil.class);
+             MockedStatic<HTTPClientUtils> httpclientUtil = mockStatic(HTTPClientUtils.class)) {
+            identityMgtServiceUtil.when(IdentityManagementServiceUtil::getInstance)
+                    .thenReturn(identityManagementServiceUtil);
+            httpclientUtil.when(HTTPClientUtils::createClientWithCustomVerifier).thenReturn(httpClientBuilder);
+            preferenceRetrievalClient.getPreference(SUPER_TENANT_DOMAIN, "", "", "");
+        }
+    }
+
+    @Test
+    public void testGetCustomTextPreference() throws BrandingPreferenceRetrievalClientException {
+
+        try (MockedStatic<IdentityManagementServiceUtil> identityMgtServiceUtil = mockStatic(IdentityManagementServiceUtil.class);
+             MockedStatic<HTTPClientUtils> httpclientUtil = mockStatic(HTTPClientUtils.class)) {
+            identityMgtServiceUtil.when(IdentityManagementServiceUtil::getInstance)
+                    .thenReturn(identityManagementServiceUtil);
+            httpclientUtil.when(HTTPClientUtils::createClientWithCustomVerifier).thenReturn(httpClientBuilder);
+            preferenceRetrievalClient.getCustomTextPreference(SUPER_TENANT_DOMAIN, "", "", "", "");
+        }
+    }
+}
---
+++ components/identity-mgt/org.wso2.carbon.identity.mgt.endpoint.util/src/test/java/org/wso2/carbon/identity/mgt/endpoint/util/RetrievalClientBaseTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.mgt.endpoint.util;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.mockito.Mock;
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.wso2.carbon.base.CarbonBaseConstants;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.wso2.carbon.utils.multitenancy.MultitenantConstants.SUPER_TENANT_DOMAIN_NAME;
+import static org.wso2.carbon.utils.multitenancy.MultitenantConstants.SUPER_TENANT_ID;
+
+@Listeners(MockitoTestNGListener.class)
+public class RetrievalClientBaseTest {
+
+    @Mock
+    IdentityManagementServiceUtil identityManagementServiceUtil;
+    @Mock
+    HttpClientBuilder httpClientBuilder;
+    @Mock
+    CloseableHttpClient httpClient;
+    @Mock
+    StatusLine statusLine;
+    @Mock
+    HttpEntity httpEntity;
+    @Mock
+    CloseableHttpResponse httpResponse;
+
+    private String mockJsonResponse = "{}";
+    public static final String SUPER_TENANT_DOMAIN = "carbon.super";
+    private static final String SERVICE_URL = "https://wso2.org:9443";
+    private static final String APP_NAME = "admin";
+    private static final String APP_PASSWORD = "p@ssw0rd";
+
+    public String getMockJsonResponse() {
+
+        return mockJsonResponse;
+    }
+
+    public void setMockJsonResponse(String mockJsonResponse) {
+
+        this.mockJsonResponse = mockJsonResponse;
+    }
+
+    @BeforeMethod
+    public void setup() throws IOException {
+
+        setupConfiguration();
+        when(httpClientBuilder.build()).thenReturn(httpClient);
+        when(httpClient.execute(any())).thenReturn(httpResponse);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn(200);
+        when(httpResponse.getEntity()).thenReturn(httpEntity);
+        InputStream inputStream = new ByteArrayInputStream(mockJsonResponse.getBytes());
+        when(httpEntity.getContent()).thenReturn(inputStream);
+
+        when(identityManagementServiceUtil.getContextURLFromFile()).thenReturn(SERVICE_URL);
+        when(identityManagementServiceUtil.getAppName()).thenReturn(APP_NAME);
+        when(identityManagementServiceUtil.getAppPassword()).thenReturn(APP_PASSWORD.toCharArray());
+    }
+
+    public void setupConfiguration() {
+
+        String carbonHome = Paths.get(System.getProperty("user.dir"), "target", "test-classes", "repository").
+                toString();
+        System.setProperty(CarbonBaseConstants.CARBON_HOME, carbonHome);
+        System.setProperty(CarbonBaseConstants.CARBON_CONFIG_DIR_PATH, Paths.get(carbonHome, "conf").toString());
+
+        PrivilegedCarbonContext.startTenantFlow();
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(SUPER_TENANT_DOMAIN_NAME);
+        PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(SUPER_TENANT_ID);
+        PrivilegedCarbonContext.getThreadLocalCarbonContext();
+    }
+}
