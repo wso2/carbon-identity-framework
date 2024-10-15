32d667ae806f73cc904ae86e06ee387bc5c2be28
sadilchamishka <sandilchamishka@gmail.com>
2024-10-15 11:42:45 +0530

    Add unit tests

--- components/provisioning/org.wso2.carbon.identity.provisioning/src/test/java/org/wso2/carbon/identity/provisioning/listener/ProvisioningRoleMgtListenerTest.java
+++ components/provisioning/org.wso2.carbon.identity.provisioning/src/test/java/org/wso2/carbon/identity/provisioning/listener/ProvisioningRoleMgtListenerTest.java
@@ -18,45 +18,68 @@
 
 package org.wso2.carbon.identity.provisioning.listener;
 
+import org.mockito.Mock;
 import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
+import org.mockito.testng.MockitoTestNGListener;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.identity.application.common.IdentityApplicationManagementException;
+import org.wso2.carbon.identity.application.common.model.ServiceProvider;
+import org.wso2.carbon.identity.application.mgt.ApplicationManagementService;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.provisioning.internal.ProvisioningServiceDataHolder;
 import org.wso2.carbon.identity.role.v2.mgt.core.RoleManagementService;
 import org.wso2.carbon.identity.role.v2.mgt.core.exception.IdentityRoleManagementException;
 import org.wso2.carbon.identity.role.v2.mgt.core.model.RoleBasicInfo;
+import org.wso2.carbon.user.api.RealmConfiguration;
 import org.wso2.carbon.user.api.UserRealm;
 import org.wso2.carbon.user.api.UserStoreException;
-import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.common.AbstractUserStoreManager;
 import org.wso2.carbon.user.core.service.RealmService;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
+@Listeners(MockitoTestNGListener.class)
 public class ProvisioningRoleMgtListenerTest {
 
-    RealmService realmService = mock(RealmService.class);
-    RoleManagementService roleManagementService = mock(RoleManagementService.class);
-    DefaultInboundUserProvisioningListener defaultInboundUserProvisioningListener =
-            mock(DefaultInboundUserProvisioningListener.class);
-    UserRealm userRealm = mock(UserRealm.class);
-    AbstractUserStoreManager userStoreManager = mock(AbstractUserStoreManager.class);
-    private MockedStatic<IdentityTenantUtil> identityTenantUtil;
+    @Mock
+    RealmService realmService;
+    @Mock
+    RealmConfiguration realmConfiguration;
+    @Mock
+    UserRealm userRealm;
+    @Mock
+    AbstractUserStoreManager userStoreManager;
+    @Mock
+    RoleManagementService roleManagementService;
+    @Mock
+    ApplicationManagementService applicationManagementService;
 
+    DefaultInboundUserProvisioningListener defaultInboundUserProvisioningListener;
+
+    private MockedStatic<IdentityTenantUtil> identityTenantUtil;
+    private MockedStatic<ApplicationManagementService> applicationManagementServiceMockedStatic;
 
     @BeforeTest
-    public void setup() throws UserStoreException {
+    public void setup() throws UserStoreException, IdentityApplicationManagementException {
+
+        // Initialize mocks
+        MockitoAnnotations.initMocks(this);
+        defaultInboundUserProvisioningListener = new DefaultInboundUserProvisioningListener();
 
         ProvisioningServiceDataHolder.getInstance().setRealmService(realmService);
-        ProvisioningServiceDataHolder.getInstance().setRoleManagementService(roleManagementService);
         ProvisioningServiceDataHolder.getInstance()
                 .setDefaultInboundUserProvisioningListener(defaultInboundUserProvisioningListener);
+
 
         when(realmService.getTenantUserRealm(-1234)).thenReturn(userRealm);
         when(userRealm.getUserStoreManager()).thenReturn(userStoreManager);
@@ -64,16 +87,35 @@ public class ProvisioningRoleMgtListenerTest {
         identityTenantUtil = mockStatic(IdentityTenantUtil.class);
         identityTenantUtil.when(()->IdentityTenantUtil.getTenantId("carbon.super")).thenReturn(-1234);
         when(userStoreManager.getUserNameFromUserID(anyString())).thenReturn("");
+
+        when(userStoreManager.getRealmConfiguration()).thenReturn(realmConfiguration);
+        when(realmConfiguration.getUserStoreProperty("DomainName")).thenReturn("PRIMARY");
+
+        applicationManagementServiceMockedStatic = mockStatic(ApplicationManagementService.class);
+        applicationManagementServiceMockedStatic.when(ApplicationManagementService::getInstance)
+                .thenReturn(applicationManagementService);
+        ServiceProvider serviceProvider = new ServiceProvider();
+        serviceProvider.setApplicationName("sample-app");
+        when(applicationManagementService.getServiceProvider(anyString(), anyString())).thenReturn(serviceProvider);
     }
 
     @Test
     public void abc() throws IdentityRoleManagementException {
 
         ProvisioningRoleMgtListener postUpdateUserListOfRole = new ProvisioningRoleMgtListener();
-        RoleBasicInfo roleBasicInfo = new RoleBasicInfo("", "");
-        roleBasicInfo.setAudienceName("organization");
-        when(roleManagementService.getRoleBasicInfoById(anyString(), anyString())).thenReturn(roleBasicInfo);
-        postUpdateUserListOfRole.postUpdateUserListOfRole("", new ArrayList<>(), new ArrayList<>(), "carbon.super");
+        RoleBasicInfo roleBasicInfo = new RoleBasicInfo("", "developer");
+        roleBasicInfo.setAudience("organization");
+        ProvisioningServiceDataHolder.getInstance().setRoleManagementService(roleManagementService);
+        when(roleManagementService.getRoleBasicInfoById(anyString(), eq("carbon.super"))).thenReturn(roleBasicInfo);
+        List<String> newUserIdList = new ArrayList<>();
+        newUserIdList.add("12345");
+        try {
+            PrivilegedCarbonContext.startTenantFlow();
+            PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain("carbon.super");
+            postUpdateUserListOfRole.postUpdateUserListOfRole("", newUserIdList, new ArrayList<>(), "carbon.super");
+        } finally {
+            PrivilegedCarbonContext.endTenantFlow();
+        }
     }
 
 }
