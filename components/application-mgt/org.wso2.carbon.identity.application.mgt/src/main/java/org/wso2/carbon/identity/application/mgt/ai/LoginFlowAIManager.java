b4df626bc907703464dbcceb8e2197665ec99a80
sahandilshan <sahandilshan222@gmail.com>
2024-10-18 12:26:15 +0530

    Add loginflow AI capability

--- components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/ai/LoginFlowAIManager.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/ai/LoginFlowAIManager.java
@@ -2,6 +2,10 @@ package org.wso2.carbon.identity.application.mgt.ai;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.wso2.carbon.ai.service.mgt.exceptions.AIClientException;
+import org.wso2.carbon.ai.service.mgt.exceptions.AIServerException;
+
+import java.util.Map;
 
 /**
  * AI Manager interface for the LoginFlowAI module.
@@ -9,11 +13,11 @@ import org.json.JSONObject;
 public interface LoginFlowAIManager {
 
     String generateAuthenticationSequence(String userQuery, JSONArray userClaims, JSONObject availableAuthenticators)
-            throws LoginFlowAIServerException, LoginFlowAIClientException;
+            throws AIServerException, AIClientException;
 
-    Object getAuthenticationSequenceGenerationStatus(String operationId) throws LoginFlowAIServerException,
-            LoginFlowAIClientException;
+    Map<String, Object> getAuthenticationSequenceGenerationStatus(String operationId) throws AIServerException,
+            AIClientException;
 
-    Object getAuthenticationSequenceGenerationResult(String operationId) throws LoginFlowAIServerException,
-            LoginFlowAIClientException;
+    Map<String, Object> getAuthenticationSequenceGenerationResult(String operationId) throws AIServerException,
+            AIClientException;
 }
--- components/application-mgt/org.wso2.carbon.identity.application.mgt/src/test/java/org/wso2/carbon/identity/application/mgt/ai/LoginFlowAIManagerTest.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt/src/test/java/org/wso2/carbon/identity/application/mgt/ai/LoginFlowAIManagerTest.java
@@ -20,8 +20,6 @@ package org.wso2.carbon.identity.application.mgt.ai;
 
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.mockito.InjectMocks;
@@ -30,54 +28,58 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.wso2.carbon.ai.service.mgt.exceptions.AIClientException;
+import org.wso2.carbon.ai.service.mgt.exceptions.AIServerException;
+import org.wso2.carbon.ai.service.mgt.util.AIHttpClientUtil;
 import org.wso2.carbon.base.CarbonBaseConstants;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.identity.common.testng.realm.InMemoryRealmService;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.user.core.UserStoreException;
 
-import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.MockitoAnnotations.openMocks;
 import static org.wso2.carbon.base.MultitenantConstants.SUPER_TENANT_DOMAIN_NAME;
 import static org.wso2.carbon.base.MultitenantConstants.SUPER_TENANT_ID;
 
 public class LoginFlowAIManagerTest {
 
-    private MockedStatic<LoginFlowAITokenService> loginFlowTokenServiceMock;
-    private MockedStatic<LoginFlowAIManagerImpl.HttpClientHelper> httpClientHelperMockedStatic;
+    private MockedStatic<AIHttpClientUtil> aiHttpClientUtilMockedStatic;
 
     @InjectMocks
     private LoginFlowAIManagerImpl loginFlowAIManager;
 
     @BeforeMethod
     public void setUp() throws UserStoreException {
-        initMocks(this);
+
+        openMocks(this);
         setCarbonHome();
         setCarbonContextForTenant(SUPER_TENANT_DOMAIN_NAME, SUPER_TENANT_ID);
-        loginFlowTokenServiceMock = mockStatic(LoginFlowAITokenService.class);
-        httpClientHelperMockedStatic = mockStatic(LoginFlowAIManagerImpl.HttpClientHelper.class);
+        aiHttpClientUtilMockedStatic = mockStatic(AIHttpClientUtil.class);
     }
 
     @Test
     public void testGenerateAuthenticationSequence_Success() throws Exception {
-        mockSuccessfulResponse("{\"operation_id\": \"12345\"}", HttpPost.class);
-        String result = loginFlowAIManager.generateAuthenticationSequence("Need username and password as" +
-                " the first step", new JSONArray(), new JSONObject());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("operation_id", "12345");
+        mockSuccessfulResponse(response, HttpPost.class);
+        String result = loginFlowAIManager.generateAuthenticationSequence("Need username and password as " +
+                "the first step", new JSONArray(), new JSONObject());
         Assert.assertEquals(result, "12345");
     }
 
     @Test
     public void testGetAuthenticationSequenceGenerationStatus_Success() throws Exception {
-        mockSuccessfulResponse("{\"status\":\"COMPLETED\"}", HttpGet.class);
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "COMPLETED");
+        mockSuccessfulResponse(response, HttpGet.class);
         Object result = loginFlowAIManager.getAuthenticationSequenceGenerationStatus("operation123");
 
         Assert.assertTrue(result instanceof Map);
@@ -87,7 +89,10 @@ public class LoginFlowAIManagerTest {
 
     @Test
     public void testGetAuthenticationSequenceGenerationResult_Success() throws Exception {
-        mockSuccessfulResponse("{\"result\":\"SUCCESS\"}", HttpGet.class);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("result", "SUCCESS");
+        mockSuccessfulResponse(response, HttpGet.class);
         Object result = loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
 
         Assert.assertTrue(result instanceof Map);
@@ -95,118 +100,31 @@ public class LoginFlowAIManagerTest {
         Assert.assertEquals("SUCCESS", resultMap.get("result"));
     }
 
-    @Test
-    public void testTokenRenewalVerification() throws Exception {
-        LoginFlowAITokenService mockTokenService = mock(LoginFlowAITokenService.class);
-        loginFlowTokenServiceMock.when(LoginFlowAITokenService::getInstance).thenReturn(mockTokenService);
-        when(mockTokenService.getAccessToken(false)).thenReturn("initialMockToken");
-        when(mockTokenService.getAccessToken(true)).thenReturn("renewedMockToken");
-        when(mockTokenService.getClientId()).thenReturn("mockOrgName");
+    private void mockSuccessfulResponse(Map<String, Object> responseBody, Class<?> requestClass) throws Exception {
 
-        LoginFlowAIManagerImpl.HttpResponseWrapper unauthorizedResponse =
-                new LoginFlowAIManagerImpl.HttpResponseWrapper(401, "Unauthorized");
-        LoginFlowAIManagerImpl.HttpResponseWrapper successResponse =
-                new LoginFlowAIManagerImpl.HttpResponseWrapper(200, "{\"result\":\"SUCCESS\"}");
-
-        httpClientHelperMockedStatic.when(() -> LoginFlowAIManagerImpl.HttpClientHelper.executeRequest(
-                any(CloseableHttpAsyncClient.class), any(HttpUriRequest.class)))
-                .thenReturn(unauthorizedResponse)
-                .thenReturn(successResponse);
-
-
-        Object result = loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
-        Assert.assertTrue(result instanceof Map);
-        Map<String, Object> resultMap = (Map<String, Object>) result;
-        Assert.assertEquals(resultMap.get("result"), "SUCCESS");
-        verify(mockTokenService).getAccessToken(true);
-    }
-
-    @Test(expectedExceptions = LoginFlowAIServerException.class)
-    public void testTokenRenewalFailure() throws Exception {
-        LoginFlowAITokenService mockTokenService = mock(LoginFlowAITokenService.class);
-        loginFlowTokenServiceMock.when(LoginFlowAITokenService::getInstance).thenReturn(mockTokenService);
-        when(mockTokenService.getAccessToken(false)).thenReturn("initialMockToken");
-        when(mockTokenService.getAccessToken(true)).thenReturn(null);
-        when(mockTokenService.getClientId()).thenReturn("mockClientId");
-
-        LoginFlowAIManagerImpl.HttpResponseWrapper unauthorizedResponse =
-                new LoginFlowAIManagerImpl.HttpResponseWrapper(401, "Unauthorized");
-        httpClientHelperMockedStatic.when(() -> LoginFlowAIManagerImpl.HttpClientHelper.executeRequest(
-                any(CloseableHttpAsyncClient.class), any(HttpUriRequest.class))).thenReturn(unauthorizedResponse);
-
-        loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
-    }
-
-    @Test(expectedExceptions = LoginFlowAIClientException.class)
-    public void testClientError() throws Exception {
-        mockErrorResponse(400, "Client Error");
-        loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
-    }
-
-    @Test(expectedExceptions = LoginFlowAIServerException.class)
-    public void testServerError() throws Exception {
-        mockErrorResponse(500, "Server Error");
-        loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
-    }
-
-    @Test(expectedExceptions = LoginFlowAIServerException.class)
-    public void testParsingError() throws Exception {
-        mockSuccessfulResponse("{invalid_json}", HttpGet.class);
-        loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
-    }
-
-    @Test(expectedExceptions = LoginFlowAIServerException.class)
-    public void testIOException() throws Exception {
-        mockExceptionDuringRequest(new IOException("Simulated IOException"));
-        loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
-    }
-
-    @Test(expectedExceptions = LoginFlowAIServerException.class)
-    public void testInterruptedException() throws Exception {
-        mockExceptionDuringRequest(new InterruptedException("Simulated InterruptedException"));
-        loginFlowAIManager.getAuthenticationSequenceGenerationResult("operation123");
-    }
-
-    private void mockSuccessfulResponse(String responseBody, Class<? extends HttpUriRequest> requestClass)
-            throws Exception {
-        LoginFlowAITokenService mockTokenService = mock(LoginFlowAITokenService.class);
-        loginFlowTokenServiceMock.when(LoginFlowAITokenService::getInstance).thenReturn(mockTokenService);
-        when(mockTokenService.getAccessToken(anyBoolean())).thenReturn("mockAccessToken");
-
-        LoginFlowAIManagerImpl.HttpResponseWrapper mockResponse = new LoginFlowAIManagerImpl
-                .HttpResponseWrapper(200, responseBody);
-        httpClientHelperMockedStatic.when(() -> LoginFlowAIManagerImpl.HttpClientHelper.executeRequest(
-                any(CloseableHttpAsyncClient.class), any(requestClass))).thenReturn(mockResponse);
+        aiHttpClientUtilMockedStatic.when(() -> AIHttpClientUtil.executeRequest(
+                any(), any(), any(), any()
+        )).thenReturn(responseBody);
     }
 
     private void mockErrorResponse(int statusCode, String responseBody) throws Exception {
-        LoginFlowAITokenService mockTokenService = mock(LoginFlowAITokenService.class);
-        loginFlowTokenServiceMock.when(LoginFlowAITokenService::getInstance).thenReturn(mockTokenService);
-        when(mockTokenService.getAccessToken(false)).thenReturn("mockAccessToken");
 
-        LoginFlowAIManagerImpl.HttpResponseWrapper errorResponse =
-                new LoginFlowAIManagerImpl.HttpResponseWrapper(statusCode, responseBody);
-        httpClientHelperMockedStatic.when(() -> LoginFlowAIManagerImpl.HttpClientHelper.executeRequest(
-                any(CloseableHttpAsyncClient.class), any(HttpUriRequest.class))).thenReturn(errorResponse);
-    }
-
-    private void mockExceptionDuringRequest(Exception exception) throws Exception {
-        LoginFlowAITokenService mockTokenService = mock(LoginFlowAITokenService.class);
-        loginFlowTokenServiceMock.when(LoginFlowAITokenService::getInstance).thenReturn(mockTokenService);
-        when(mockTokenService.getAccessToken(false)).thenReturn("mockAccessToken");
-
-        httpClientHelperMockedStatic.when(() -> LoginFlowAIManagerImpl.HttpClientHelper.executeRequest(
-                any(CloseableHttpAsyncClient.class), any(HttpUriRequest.class))).thenThrow(exception);
+        aiHttpClientUtilMockedStatic.when(() -> AIHttpClientUtil.executeRequest(
+                anyString(), anyString(), any(), any()
+        )).thenThrow(statusCode >= 500 ? new AIServerException(responseBody, "ERROR_CODE") : new AIClientException(
+                responseBody, "ERROR_CODE"));
     }
 
     private void setCarbonHome() {
-        String carbonHome = Paths.get(System.getProperty("user.dir"), "target", "test-classes", "repository")
-                .toString();
+
+        String carbonHome = Paths.get(System.getProperty("user.dir"), "target", "test-classes",
+                "repository").toString();
         System.setProperty(CarbonBaseConstants.CARBON_HOME, carbonHome);
         System.setProperty(CarbonBaseConstants.CARBON_CONFIG_DIR_PATH, Paths.get(carbonHome, "conf").toString());
     }
 
     private void setCarbonContextForTenant(String tenantDomain, int tenantId) throws UserStoreException {
+
         PrivilegedCarbonContext.startTenantFlow();
         PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(tenantDomain);
         PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(tenantId);
@@ -216,7 +134,8 @@ public class LoginFlowAIManagerTest {
 
     @AfterMethod
     public void tearDown() {
-        loginFlowTokenServiceMock.close();
-        httpClientHelperMockedStatic.close();
+
+        aiHttpClientUtilMockedStatic.close();
+        PrivilegedCarbonContext.endTenantFlow();
     }
 }
