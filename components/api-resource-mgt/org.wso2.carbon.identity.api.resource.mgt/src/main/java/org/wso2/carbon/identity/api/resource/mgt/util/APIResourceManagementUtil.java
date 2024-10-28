0c4b70e07685430e059273ada01df07dff2372ca
vimukthiRajapaksha <reachvimukthi@gmail.com>
2024-10-28 09:54:29 +0530

    Add missing method comments

--- components/api-resource-mgt/org.wso2.carbon.identity.api.resource.mgt/src/main/java/org/wso2/carbon/identity/api/resource/mgt/util/APIResourceManagementUtil.java
+++ components/api-resource-mgt/org.wso2.carbon.identity.api.resource.mgt/src/main/java/org/wso2/carbon/identity/api/resource/mgt/util/APIResourceManagementUtil.java
@@ -47,8 +47,8 @@ import java.util.stream.Collectors;
 public class APIResourceManagementUtil {
 
     private static final Log LOG = LogFactory.getLog(APIResourceManagementUtil.class);
-    private static final Type SCHEMA_TYPE = new TypeToken<HashMap<String, Object>>() {
-    }.getType();
+    private static final Gson GSON = new Gson();
+    private static final Type SCHEMA_TYPE = new TypeToken<Map<String, Object>>() { }.getType();
 
     /**
      * Handle API Resource Management client exceptions.
@@ -182,8 +182,8 @@ public class APIResourceManagementUtil {
                 APIResourceManagementConstants.NON_BUSINESS_API_FILTER, APIResourceManagementConstants.ASC,
                 tenantDomain).getTotalCount();
         return new ArrayList<>(APIResourceManagerImpl.getInstance().getAPIResources(null, null, systemAPICount,
-                        APIResourceManagementConstants.NON_BUSINESS_API_FILTER, APIResourceManagementConstants.ASC,
-                        tenantDomain).getAPIResources());
+                APIResourceManagementConstants.NON_BUSINESS_API_FILTER, APIResourceManagementConstants.ASC,
+                tenantDomain).getAPIResources());
     }
 
     /**
@@ -221,13 +221,25 @@ public class APIResourceManagementUtil {
         return IdentityTenantUtil.getTenantId(tenantDomain);
     }
 
+    /**
+     * Parses a JSON schema represented as a string and converts it into a map structure.
+     *
+     * @param schema the JSON schema string to be parsed. It must be a valid JSON string.
+     * @return a {@code Map<String, Object>} representing the parsed JSON schema.
+     */
     public static Map<String, Object> parseSchema(final String schema) {
 
-        return new Gson().fromJson(schema, SCHEMA_TYPE);
+        return GSON.fromJson(schema, SCHEMA_TYPE);
     }
 
+    /**
+     * Converts a map representing a schema into its JSON string representation.
+     *
+     * @param schema the schema represented as a {@code Map<String, Object>} to be converted into a JSON string.
+     * @return a JSON string representation of the provided schema map.
+     */
     public static String toJsonString(final Map<String, Object> schema) {
 
-        return new Gson().toJson(schema, SCHEMA_TYPE);
+        return GSON.toJson(schema, SCHEMA_TYPE);
     }
 }
