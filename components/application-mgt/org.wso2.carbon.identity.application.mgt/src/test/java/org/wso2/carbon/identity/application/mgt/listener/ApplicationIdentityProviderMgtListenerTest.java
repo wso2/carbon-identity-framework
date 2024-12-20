9243de7c9ee1dd3da030c60117e40dd10c1bae39
DilshanSenarath <74205483+DilshanSenarath@users.noreply.github.com>
2024-12-20 14:39:12 +0530

    add unit tests

--- components/application-mgt/org.wso2.carbon.identity.application.mgt/src/test/java/org/wso2/carbon/identity/application/mgt/listener/ApplicationIdentityProviderMgtListenerTest.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt/src/test/java/org/wso2/carbon/identity/application/mgt/listener/ApplicationIdentityProviderMgtListenerTest.java
@@ -21,11 +21,11 @@ package org.wso2.carbon.identity.application.mgt.listener;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.carbon.CarbonConstants;
 import org.wso2.carbon.base.CarbonBaseConstants;
-import org.wso2.carbon.context.CarbonContext;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.context.RegistryType;
 import org.wso2.carbon.context.internal.OSGiDataHolder;
@@ -34,7 +34,9 @@ import org.wso2.carbon.identity.application.common.ApplicationAuthenticatorServi
 import org.wso2.carbon.identity.application.common.IdentityApplicationManagementException;
 import org.wso2.carbon.identity.application.common.model.FederatedAuthenticatorConfig;
 import org.wso2.carbon.identity.application.common.model.IdentityProvider;
+import org.wso2.carbon.identity.application.common.model.OutboundProvisioningConfig;
 import org.wso2.carbon.identity.application.common.model.Property;
+import org.wso2.carbon.identity.application.common.model.ProvisioningConnectorConfig;
 import org.wso2.carbon.identity.application.common.model.ServiceProvider;
 import org.wso2.carbon.identity.application.mgt.ApplicationManagementService;
 import org.wso2.carbon.identity.application.mgt.ApplicationManagementServiceImpl;
@@ -53,6 +55,7 @@ import org.wso2.carbon.identity.core.internal.IdentityCoreServiceDataHolder;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.idp.mgt.IdentityProviderManagementException;
 import org.wso2.carbon.idp.mgt.IdentityProviderManager;
+import org.wso2.carbon.idp.mgt.cache.IdPCacheByName;
 import org.wso2.carbon.idp.mgt.internal.IdpMgtServiceComponentHolder;
 import org.wso2.carbon.idp.mgt.util.MetadataConverter;
 import org.wso2.carbon.registry.core.Collection;
@@ -90,9 +93,7 @@ import static org.wso2.carbon.utils.multitenancy.MultitenantConstants.SUPER_TENA
 public class ApplicationIdentityProviderMgtListenerTest {
 
     private ApplicationManagementService applicationManagementService;
-    private IdentityProviderManager identityProviderManager;
     private String appResourceId1;
-    private String appResourceId2;
     private IdentityProvider identityProvider1;
     private IdentityProvider identityProvider2;
     private String username;
@@ -109,7 +110,7 @@ public class ApplicationIdentityProviderMgtListenerTest {
         setupConfiguration();
 
         applicationManagementService = ApplicationManagementServiceImpl.getInstance();
-        identityProviderManager = IdentityProviderManager.getInstance();
+        IdentityProviderManager identityProviderManager = IdentityProviderManager.getInstance();
         registerSystemAuthenticators();
         IdpMgtServiceComponentHolder.getInstance().addMetadataConverter(mock(MetadataConverter.class));
         applicationIdentityProviderMgtListener = new ApplicationIdentityProviderMgtListener();
@@ -117,14 +118,21 @@ public class ApplicationIdentityProviderMgtListenerTest {
         IdentityProvider idp1 = getTestIdentityProvider("test-idp-1.xml");
         identityProvider1 = identityProviderManager.addIdPWithResourceId(idp1, SUPER_TENANT_DOMAIN_NAME);
         ServiceProvider serviceProvider1 = getTestServiceProvider("test-sp-1.xml");
+        OutboundProvisioningConfig outboundProvisioningConfig = serviceProvider1.getOutboundProvisioningConfig();
+        outboundProvisioningConfig.setProvisioningIdentityProviders(new IdentityProvider[]{ identityProvider1 });
         appResourceId1 =
                 applicationManagementService.createApplication(serviceProvider1, SUPER_TENANT_DOMAIN_NAME, username);
 
         IdentityProvider idp2 = getTestIdentityProvider("test-idp-2.xml");
         identityProvider2 = identityProviderManager.addIdPWithResourceId(idp2, SUPER_TENANT_DOMAIN_NAME);
         ServiceProvider serviceProvider2 = getTestServiceProvider("test-sp-2.xml");
-        appResourceId2 =
-                applicationManagementService.createApplication(serviceProvider2, SUPER_TENANT_DOMAIN_NAME, username);
+        applicationManagementService.createApplication(serviceProvider2, SUPER_TENANT_DOMAIN_NAME, username);
+    }
+
+    @AfterMethod
+    public void after() {
+
+        IdPCacheByName.getInstance().clear(SUPER_TENANT_DOMAIN_NAME);
     }
 
     @Test(description = "Test the general workflow of the pre-update IDP listener during a normal IDP update.")
@@ -135,6 +143,120 @@ public class ApplicationIdentityProviderMgtListenerTest {
                 applicationIdentityProviderMgtListener.doPreUpdateIdP(identityProvider1.getIdentityProviderName(),
                         identityProvider1, SUPER_TENANT_DOMAIN_NAME);
         Assert.assertTrue(result);
+    }
+
+    @Test(description = "Test the pre-update IDP listener functionality for an IDP in a disabled state.")
+    public void testDoPreUpdateIDPWithDisabledIDP() {
+
+        identityProvider1.setEnable(false);
+        String exceptionMessage = null;
+        try {
+            applicationIdentityProviderMgtListener.doPreUpdateIdP(identityProvider1.getIdentityProviderName(),
+                    identityProvider1, SUPER_TENANT_DOMAIN_NAME);
+        } catch (IdentityProviderManagementException ex) {
+            exceptionMessage = ex.getMessage();
+        }
+        Assert.assertEquals(exceptionMessage,
+                "Error in disabling identity provider as it is referred by service providers.");
+        identityProvider1.setEnable(true);
+    }
+
+    @Test(description = "Test the pre-update IDP listener functionality for an IDP with disabled default"
+            + " authenticator.")
+    public void testDoPreUpdateIDPWithDisabledDefaultAuthenticator() {
+
+        FederatedAuthenticatorConfig[] federatedAuthenticatorConfig =
+                identityProvider1.getFederatedAuthenticatorConfigs();
+        federatedAuthenticatorConfig[0].setEnabled(false);
+        String exceptionMessage = null;
+        try {
+            applicationIdentityProviderMgtListener.doPreUpdateIdP(identityProvider1.getIdentityProviderName(),
+                    identityProvider1, SUPER_TENANT_DOMAIN_NAME);
+        } catch (IdentityProviderManagementException ex) {
+            exceptionMessage = ex.getMessage();
+        }
+        Assert.assertEquals(exceptionMessage,
+                "Error in disabling default federated authenticator as it is referred by service providers.");
+        federatedAuthenticatorConfig[0].setEnabled(true);
+    }
+
+    @Test(description = "Test the pre-update IDP listener functionality for an IDP with disabled authenticator.")
+    public void testDoPreUpdateIDPWithDisabledAuthenticator() {
+
+        FederatedAuthenticatorConfig[] federatedAuthenticatorConfig =
+                identityProvider2.getFederatedAuthenticatorConfigs();
+        federatedAuthenticatorConfig[1].setEnabled(false);
+        String exceptionMessage = null;
+        try {
+            applicationIdentityProviderMgtListener.doPreUpdateIdP(identityProvider2.getIdentityProviderName(),
+                    identityProvider2, SUPER_TENANT_DOMAIN_NAME);
+        } catch (IdentityProviderManagementException ex) {
+            exceptionMessage = ex.getMessage();
+        }
+        Assert.assertEquals(exceptionMessage,
+                "CustomAuthenticator is referred by service providers.");
+        federatedAuthenticatorConfig[1].setEnabled(true);
+    }
+
+    @Test(description = "Test the pre-update IDP listener functionality for an IDP with disabled provisioning"
+            + " connector.")
+    public void testDoPreUpdateIDPWithDisabledProvisioningConnector() {
+
+        ProvisioningConnectorConfig[] provisioningConnectorConfigs =
+                identityProvider1.getProvisioningConnectorConfigs();
+        provisioningConnectorConfigs[0].setEnabled(false);
+        String exceptionMessage = null;
+        try {
+            applicationIdentityProviderMgtListener.doPreUpdateIdP(identityProvider1.getIdentityProviderName(),
+                    identityProvider1, SUPER_TENANT_DOMAIN_NAME);
+        } catch (IdentityProviderManagementException ex) {
+            exceptionMessage = ex.getMessage();
+        }
+        Assert.assertEquals(exceptionMessage,
+                "ProvisioningConnector outbound provisioning connector is referred by service providers.");
+        provisioningConnectorConfigs[0].setEnabled(true);
+    }
+
+    @Test(description = "Test the pre-update IDP listener functionality for the IDP name change"
+            + " connector.", dependsOnMethods = {"testDoPreUpdateIDPWithDisabledProvisioningConnector",
+            "testDoPreUpdateIDPWithDisabledAuthenticator", "testDoPreUpdateIDPWithDisabledDefaultAuthenticator",
+            "testDoPreUpdateIDPWithDisabledIDP", "testDoPreUpdateIDP"})
+    public void testDoPreUpdateIDPWithIDPNameChange() throws IdentityProviderManagementException {
+
+        identityProvider2.setIdentityProviderName("identityProviderIDP_3_IS");
+        boolean result =
+                applicationIdentityProviderMgtListener.doPreUpdateIdP("identityProviderIDP_2_IS",
+                        identityProvider2, SUPER_TENANT_DOMAIN_NAME);
+        Assert.assertTrue(result);
+    }
+
+    @Test(description = "Test the pre-update IDP listener functionality for the IDP without resource ID.",
+            dependsOnMethods = { "testDoPreUpdateIDPWithIDPNameChange"})
+    public void testDoPreUpdateIDPWithoutUUID() throws IdentityProviderManagementException {
+
+        identityProvider1.setResourceId(null);
+        boolean result =
+                applicationIdentityProviderMgtListener.doPreUpdateIdP(identityProvider1.getIdentityProviderName(),
+                        identityProvider1, SUPER_TENANT_DOMAIN_NAME);
+        Assert.assertTrue(result);
+    }
+
+    @Test(description = "Test updating the default authenticator of an identity provider configured as a federated"
+            + " authentication type in the service provider.", dependsOnMethods = {
+            "testDoPreUpdateIDPWithoutUUID"})
+    public void testDoPreUpdateIDPWhenChangingDefaultAuthenticator()
+            throws IdentityProviderManagementException, IdentityApplicationManagementException {
+
+        FederatedAuthenticatorConfig federatedAuthenticatorConfig =
+                identityProvider1.getFederatedAuthenticatorConfigs()[1];
+        identityProvider1.setDefaultAuthenticatorConfig(federatedAuthenticatorConfig);
+        applicationIdentityProviderMgtListener.doPreUpdateIdP(identityProvider1.getIdentityProviderName(),
+                        identityProvider1, SUPER_TENANT_DOMAIN_NAME);
+        ServiceProvider serviceProvider =
+                applicationManagementService.getApplicationByResourceId(appResourceId1, SUPER_TENANT_DOMAIN_NAME);
+        Assert.assertEquals(serviceProvider.getLocalAndOutBoundAuthenticationConfig()
+                .getAuthenticationSteps()[0].getFederatedIdentityProviders()[0].getDefaultAuthenticatorConfig()
+                .getName(), "CustomAuthenticator");
     }
 
     private void registerSystemAuthenticators() {
@@ -227,7 +349,9 @@ public class ApplicationIdentityProviderMgtListenerTest {
         ApplicationManagementServiceComponentHolder holder = ApplicationManagementServiceComponentHolder.getInstance();
         setInstanceValue(testSessionRealmService, RealmService.class, ApplicationManagementServiceComponentHolder.class,
                 holder);
-        setInstanceValue(new RegistryBasedApplicationPermissionProvider(), ApplicationPermissionProvider.class,
+        RegistryBasedApplicationPermissionProvider registryBasedApplicationPermissionProvider =
+                mock(RegistryBasedApplicationPermissionProvider.class);
+        setInstanceValue(registryBasedApplicationPermissionProvider, ApplicationPermissionProvider.class,
                 ApplicationManagementServiceComponentHolder.class, holder);
 
         // Configure Registry Service.
@@ -237,13 +361,10 @@ public class ApplicationIdentityProviderMgtListenerTest {
         OSGiDataHolder.getInstance().setRegistryService(mockRegistryService);
         CarbonCoreDataHolder.getInstance().setRegistryService(mockRegistryService);
         PrivilegedCarbonContext.getThreadLocalCarbonContext()
-                .setRegistry(RegistryType.USER_GOVERNANCE, mockRegistryService.getRegistry());
-        when(mockRegistry.resourceExists(anyString())).thenReturn(FALSE);
+                .setRegistry(RegistryType.USER_GOVERNANCE, mockRegistry);
         Collection mockPermissionNode = mock(Collection.class);
         when(mockRegistry.newCollection()).thenReturn(mockPermissionNode);
         when(mockRegistry.get(anyString())).thenReturn(mockPermissionNode);
-        when(CarbonContext.getThreadLocalCarbonContext().getRegistry(
-                RegistryType.USER_GOVERNANCE)).thenReturn(mockRegistry);
         when(mockRegistry.resourceExists(anyString())).thenReturn(FALSE);
     }
 }
