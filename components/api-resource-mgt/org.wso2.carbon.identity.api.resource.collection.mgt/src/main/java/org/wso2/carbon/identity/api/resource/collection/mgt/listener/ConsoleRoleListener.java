77574f25ac8e402345b945b562c418f9b48e39f1
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2025-01-24 13:40:48 +0530

    fix update and add role

--- components/api-resource-mgt/org.wso2.carbon.identity.api.resource.collection.mgt/src/main/java/org/wso2/carbon/identity/api/resource/collection/mgt/listener/ConsoleRoleListener.java
+++ components/api-resource-mgt/org.wso2.carbon.identity.api.resource.collection.mgt/src/main/java/org/wso2/carbon/identity/api/resource/collection/mgt/listener/ConsoleRoleListener.java
@@ -23,6 +23,7 @@ import java.util.stream.Collectors;
 
 import static org.wso2.carbon.identity.api.resource.collection.mgt.constant.APIResourceCollectionManagementConstants.APIResourceCollectionConfigBuilderConstants.EDIT_ACTION;
 import static org.wso2.carbon.identity.api.resource.collection.mgt.constant.APIResourceCollectionManagementConstants.APIResourceCollectionConfigBuilderConstants.VIEW_ACTION;
+import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.APPLICATION;
 import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.CONSOLE_APP_AUDIENCE_NAME;
 import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.CONSOLE_ORG_SCOPE_PREFIX;
 import static org.wso2.carbon.identity.role.v2.mgt.core.RoleConstants.CONSOLE_SCOPE_PREFIX;
@@ -45,96 +46,156 @@ public class ConsoleRoleListener extends AbstractRoleManagementListener {
     }
 
     @Override
+    public void preAddRole(String roleName, List<String> userList, List<String> groupList, List<Permission> permissions,
+                           String audience, String audienceId, String tenantDomain)
+            throws IdentityRoleManagementException {
+
+        if (APPLICATION.equals(audience)) {
+            List<Permission> consoleFeaturePermissions = getConsoleFeaturePermissions(permissions);
+            if (ArrayUtils.isNotEmpty(consoleFeaturePermissions.toArray())) {
+                // If console features are added to the role, then we need to we only need to persist the console
+                // permissions.
+                permissions.retainAll(consoleFeaturePermissions);
+            }
+        }
+    }
+
+    @Override
     public void postGetRole(Role role, String roleId, String tenantDomain) throws IdentityRoleManagementException {
 
-        // Add new read and write scopes from API resource collection to the console role's permission list.
+
         if (isConsoleRole(roleId, tenantDomain)) {
-            // Fetch all system scopes to resolve permission details from permission name.
-            List<Scope> systemScopes;
-            try {
-                systemScopes = APIResourceCollectionMgtServiceDataHolder.getInstance()
-                        .getAPIResourceManagementService().getSystemAPIScopes(tenantDomain);
-            } catch (APIResourceMgtException e) {
-                throw new IdentityRoleManagementException("Error while retrieving internal scopes for tenant " +
-                        "domain : " + tenantDomain, e);
+            // Get updated console role permissions with newly added read and write scopes from API resource collection.
+            List<Permission> rolePermissions = getUpdatedPermissions(role.getPermissions(), tenantDomain);
+            role.setPermissions(rolePermissions);
+        }
+    }
+
+    @Override
+    public void postGetPermissionListOfRole(List<Permission> permissionListOfRole, String roleId, String tenantDomain)
+            throws IdentityRoleManagementException {
+
+        if (isConsoleRole(roleId, tenantDomain)) {
+            List<Permission> rolePermissions = getUpdatedPermissions(permissionListOfRole, tenantDomain);
+            permissionListOfRole.retainAll(rolePermissions);
+        }
+    }
+
+    @Override
+    public void postGetPermissionListOfRoles(List<String> permissions, List<String> roleIds, String tenantDomain)
+            throws IdentityRoleManagementException {
+
+        boolean isConsoleRole = false;
+        for (String roleId : roleIds) {
+            if (isConsoleRole(roleId, tenantDomain)) {
+                isConsoleRole = true;
+                break;
             }
-            List<Permission> systemPermissions = systemScopes.stream().map(scope -> new Permission(scope.getName(),
-                    scope.getDisplayName(), scope.getApiID())).collect(Collectors.toList());
-            // Get all the permissions of the role.
-            List<Permission> rolePermissions = role.getPermissions();
-            List<APIResourceCollection> apiResourceCollections = getAPIResourceCollections(tenantDomain);
-            List<Permission> consoleFeaturePermissions = getConsoleFeaturePermissions(rolePermissions);
-            if (!consoleFeaturePermissions.isEmpty()) {
-                // This is where we handle the new console roles (console roles created after 7.0.0) permissions.
-                // We check whether the role has the view feature scope and edit feature scope. If the role has the
-                // view feature scope, then we add all the read scopes. If the role has the edit feature scope, then we
-                // add all the write scopes.
-                consoleFeaturePermissions.forEach(permission -> {
-                    apiResourceCollections.forEach(apiResourceCollection -> {
-                        if (apiResourceCollection.getViewFeatureScope() != null &&
-                                apiResourceCollection.getViewFeatureScope().equals(permission.getName())) {
-                            apiResourceCollection.getReadScopes().forEach(newReadScope -> {
-                                Optional<Permission> newPermission = systemPermissions.stream()
-                                        .filter(permission1 -> permission1.getName().equals(newReadScope))
-                                        .findFirst();
-                                newPermission.ifPresent(rolePermissions::add);
-                            });
-                        }
-                        // If the role has the edit feature scope, then we add all the write scopes.
-                        if (apiResourceCollection.getEditFeatureScope() != null &&
-                                apiResourceCollection.getEditFeatureScope().equals(permission.getName())) {
-                            apiResourceCollection.getWriteScopes().forEach(newReadScope -> {
-                                Optional<Permission> newPermission = systemPermissions.stream()
-                                        .filter(permission1 -> permission1.getName().equals(newReadScope))
-                                        .findFirst();
-                                newPermission.ifPresent(rolePermissions::add);
-                            });
-                        }
-                    });
+        }
+        if (isConsoleRole) {
+            List<Permission> resolvedRolePermissions = new ArrayList<>();
+            List<Permission> systemPermissions = getSystemPermission(tenantDomain);
+            permissions.forEach(permission -> {Optional<Permission> newPermission = systemPermissions.stream()
+                        .filter(permission1 -> permission1.getName().equals(permission)).findFirst();
+                newPermission.ifPresent(resolvedRolePermissions::add);
+            });
+            List<Permission> rolePermissions = getUpdatedPermissions(resolvedRolePermissions, tenantDomain);
+            permissions.retainAll(rolePermissions.stream().map(Permission::getName).collect(Collectors.toList()));
+        }
+    }
+
+    @Override
+    public void preUpdatePermissionsForRole(String roleId, List<Permission> addedPermissions,
+                                            List<Permission> deletedPermissions, String audience, String audienceId,
+                                            String tenantDomain) throws IdentityRoleManagementException {
+
+        if (APPLICATION.equals(audience)) {
+            List<Permission> consoleFeaturePermissions = getConsoleFeaturePermissions(addedPermissions);
+            if (ArrayUtils.isNotEmpty(consoleFeaturePermissions.toArray())) {
+                // If console features are added to the role, then we need to we only need to persist the console
+                // permissions.
+                addedPermissions.retainAll(consoleFeaturePermissions);
+            }
+        }
+    }
+
+    private List<Permission> getUpdatedPermissions(List<Permission> rolePermissions, String tenantDomain)
+            throws IdentityRoleManagementException {
+
+        // Fetch all system scopes to resolve permission details from permission name.
+        List<Permission> systemPermissions = getSystemPermission(tenantDomain);
+        List<APIResourceCollection> apiResourceCollections = getAPIResourceCollections(tenantDomain);
+        List<Permission> consoleFeaturePermissions = getConsoleFeaturePermissions(rolePermissions);
+        if (!consoleFeaturePermissions.isEmpty()) {
+            // This is where we handle the new console roles (console roles created after 7.0.0) permissions.
+            // We check whether the role has the view feature scope or edit feature scope. If the role has the
+            // view feature scope, then we add all the read scopes. If the role has the edit feature scope, then we
+            // add all the write scopes.
+            consoleFeaturePermissions.forEach(permission -> {
+                apiResourceCollections.forEach(apiResourceCollection -> {
+                    if (apiResourceCollection.getViewFeatureScope() != null &&
+                            apiResourceCollection.getViewFeatureScope().equals(permission.getName())) {
+                        apiResourceCollection.getReadScopes().forEach(newReadScope -> {
+                            Optional<Permission> newPermission = systemPermissions.stream()
+                                    .filter(permission1 -> permission1.getName().equals(newReadScope))
+                                    .findFirst();
+                            newPermission.ifPresent(rolePermissions::add);
+                        });
+                    }
+                    // If the role has the edit feature scope, then we add all the write scopes.
+                    if (apiResourceCollection.getEditFeatureScope() != null &&
+                            apiResourceCollection.getEditFeatureScope().equals(permission.getName())) {
+                        apiResourceCollection.getWriteScopes().forEach(newReadScope -> {
+                            Optional<Permission> newPermission = systemPermissions.stream()
+                                    .filter(permission1 -> permission1.getName().equals(newReadScope))
+                                    .findFirst();
+                            newPermission.ifPresent(rolePermissions::add);
+                        });
+                    }
                 });
-            } else {
-                // This is where we handle the initial console roles (console roles created in 7.0.0) permissions.
-                List<Permission> consolePermissions = getConsolePermissions(rolePermissions);
-                consolePermissions.forEach(permission -> {
-                    apiResourceCollections.forEach(apiResourceCollection -> {
-                        // Match the permission with the collection.
-                        if (apiResourceCollection.getReadScopes().contains(permission.getName())) {
-                            List<String> newReadScopes = apiResourceCollection.getNewReadScopes().get("v1");
-                            if (ArrayUtils.isNotEmpty(newReadScopes.toArray())) {
-                                // Add new read scopes since we have the feature scope.
-                                newReadScopes.forEach(newReadScope -> {
+            });
+        } else {
+            // This is where we handle the initial console roles (console roles created in 7.0.0) permissions.
+            List<Permission> consolePermissions = getConsolePermissions(rolePermissions);
+            consolePermissions.forEach(permission -> {
+                apiResourceCollections.forEach(apiResourceCollection -> {
+                    // Match the permission with the collection.
+                    if (apiResourceCollection.getReadScopes().contains(permission.getName())) {
+                        List<String> newReadScopes = apiResourceCollection.getNewReadScopes().get("v1");
+                        if (ArrayUtils.isNotEmpty(newReadScopes.toArray())) {
+                            // Add new read scopes since we have the feature scope.
+                            newReadScopes.forEach(newReadScope -> {
+                                Optional<Permission> newPermission = systemPermissions.stream()
+                                        .filter(permission1 -> permission1.getName().equals(newReadScope))
+                                        .findFirst();
+                                newPermission.ifPresent(rolePermissions::add);
+                            });
+
+                        }
+                        List<String> writeScopes = apiResourceCollection.getWriteScopes();
+                        List<String> newWriteScopes = apiResourceCollection.getNewWriteScopes().get("v1");
+                        if (ArrayUtils.isNotEmpty(newWriteScopes.toArray())) {
+                            // Create a copy of the original writeScopes list
+                            List<String> writeScopesCopy = new ArrayList<>(writeScopes);
+                            // Remove the new write scopes from the write scopes since we need to cross-check with
+                            // the role's permission list.
+                            writeScopesCopy.removeAll(newWriteScopes);
+                            // if all the writeScopes are in the role's permission list, then add new write scopes.
+                            if (rolePermissions.stream().anyMatch(rolePermission ->
+                                    writeScopesCopy.contains(rolePermission.getName()))) {
+                                newWriteScopes.forEach(newWriteScope -> {
                                     Optional<Permission> newPermission = systemPermissions.stream()
-                                            .filter(permission1 -> permission1.getName().equals(newReadScope))
+                                            .filter(permission1 -> permission1.getName().equals(newWriteScope))
                                             .findFirst();
                                     newPermission.ifPresent(rolePermissions::add);
                                 });
-
-                            }
-                            List<String> writeScopes = apiResourceCollection.getWriteScopes();
-                            List<String> newWriteScopes = apiResourceCollection.getNewWriteScopes().get("v1");
-                            if (ArrayUtils.isNotEmpty(newWriteScopes.toArray())) {
-                                // Create a copy of the original writeScopes list
-                                List<String> writeScopesCopy = new ArrayList<>(writeScopes);
-                                // Remove the new write scopes from the write scopes since we need to cross-check with
-                                // the role's permission list.
-                                writeScopesCopy.removeAll(newWriteScopes);
-                                // if all the writeScopes are in the role's permission list, then add new write scopes.
-                                if (rolePermissions.stream().anyMatch(rolePermission ->
-                                        writeScopesCopy.contains(rolePermission.getName()))) {
-                                    newWriteScopes.forEach(newWriteScope -> {
-                                        Optional<Permission> newPermission = systemPermissions.stream()
-                                                .filter(permission1 -> permission1.getName().equals(newWriteScope))
-                                                .findFirst();
-                                        newPermission.ifPresent(rolePermissions::add);
-                                    });
-                                }
                             }
                         }
-                    });
+                    }
                 });
-            }
-            role.setPermissions(rolePermissions);
+            });
         }
+        return rolePermissions;
     }
 
     private boolean isConsoleRole(String roleId, String tenantDomain) throws IdentityRoleManagementException {
@@ -178,5 +239,18 @@ public class ConsoleRoleListener extends AbstractRoleManagementListener {
                         || permission.getName().startsWith(CONSOLE_ORG_SCOPE_PREFIX)) &&
                         !(permission.getName().endsWith(VIEW_ACTION) || permission.getName().endsWith(EDIT_ACTION)))
                 .collect(Collectors.toList());
+    }
+
+    private List<Permission> getSystemPermission(String tenantDomain) throws IdentityRoleManagementException {
+        List<Scope> systemScopes;
+        try {
+            systemScopes = APIResourceCollectionMgtServiceDataHolder.getInstance()
+                    .getAPIResourceManagementService().getSystemAPIScopes(tenantDomain);
+        } catch (APIResourceMgtException e) {
+            throw new IdentityRoleManagementException("Error while retrieving internal scopes for tenant " +
+                    "domain : " + tenantDomain, e);
+        }
+        return  systemScopes.stream().map(scope -> new Permission(scope.getName(), scope.getDisplayName(),
+                scope.getApiID())).collect(Collectors.toList());
     }
 }
