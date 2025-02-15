01438162ab49f3496e9664ef3bcb1555d1b76992
roshanemoraes <moraes.git@gmail.com>
2025-02-15 19:57:42 +0530

    Create AsyncStatusMgt files

---
+++ components/async-status-mgt/org.wso2.carbon.identity.framework.async.status.mgt/src/main/java/org/wso2/carbon/identity/framework/async/status/mgt/AsyncStatusMgtService.java
@@ -0,0 +1,5 @@
+package org.wso2.carbon.identity.framework.async.status.mgt;
+
+public interface AsyncStatusMgtService {
+    void processEvent(String event);
+}
---
+++ components/async-status-mgt/org.wso2.carbon.identity.framework.async.status.mgt/src/main/java/org/wso2/carbon/identity/framework/async/status/mgt/AsyncStatusMgtServiceImpl.java
@@ -0,0 +1,19 @@
+package org.wso2.carbon.identity.framework.async.status.mgt;
+
+import org.osgi.service.component.annotations.Component;
+
+import java.util.logging.Logger;
+
+@Component(
+        service = AsyncStatusMgtService.class,
+        immediate = true
+)
+public class AsyncStatusMgtServiceImpl implements AsyncStatusMgtService {
+    private static final Logger LOGGER =
+            Logger.getLogger(AsyncStatusMgtServiceImpl.class.getName());
+
+    @Override
+    public void processEvent(String event) {
+        LOGGER.info("Processing Event: " + event);
+    }
+}
