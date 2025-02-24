c792f2f029af6e8e90aa2b7333d5a0f1f603cd5a
Pamoda Wimalasiri <pamodaaw@gmail.com>
2025-02-24 14:18:48 +0530

    Introduce DataDTO to the StepDTO

---
+++ components/user-self-registration/org.wso2.carbon.identity.user.registration.mgt/src/main/java/org/wso2/carbon/identity/user/registration/mgt/model/DataDTO.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.user.registration.mgt.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private ActionDTO action;
+    private String url;
+    private List<ComponentDTO> components = new ArrayList<>();
+
+    public DataDTO() {
+
+    }
+
+    private DataDTO(Builder builder) {
+
+        this.components = builder.components;
+        this.action = builder.action;
+        this.url = builder.url;
+    }
+
+    public List<ComponentDTO> getComponents() {
+
+        return components;
+    }
+
+    public void addComponent(ComponentDTO component) {
+
+        this.components.add(component);
+    }
+
+    public ActionDTO getAction() {
+
+        return action;
+    }
+
+    public void setAction(ActionDTO action) {
+
+        this.action = action;
+    }
+
+    public String getUrl() {
+
+        return url;
+    }
+
+    public void setUrl(String url) {
+
+        this.url = url;
+    }
+
+    public static class Builder {
+
+        private final List<ComponentDTO> components = new ArrayList<>();
+        private ActionDTO action;
+        private String url;
+
+        public Builder components(List<ComponentDTO> components) {
+
+            if (components != null && !components.isEmpty()) {
+                this.components.addAll(components);
+            }
+            return this;
+        }
+
+        public Builder action(ActionDTO action) {
+
+            this.action = action;
+            return this;
+        }
+
+        public Builder url(String url) {
+
+            this.url = url;
+            return this;
+        }
+
+        public DataDTO build() {
+
+            return new DataDTO(this);
+        }
+    }
+}
