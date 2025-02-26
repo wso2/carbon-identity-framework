15b4ef56ea714d98b86ff78b714ef397d306af65
pamoda <pamodaaw@gmail.com>
2025-02-26 23:07:46 +0530

    Add audit logger

--- components/user-self-registration/org.wso2.carbon.identity.user.registration.mgt/src/main/java/org/wso2/carbon/identity/user/registration/mgt/utils/RegistrationMgtUtils.java
+++ components/user-self-registration/org.wso2.carbon.identity.user.registration.mgt/src/main/java/org/wso2/carbon/identity/user/registration/mgt/utils/RegistrationMgtUtils.java
@@ -18,10 +18,18 @@
 
 package org.wso2.carbon.identity.user.registration.mgt.utils;
 
+import static org.wso2.carbon.identity.central.log.mgt.utils.LoggerUtils.ENABLE_V2_AUDIT_LOGS;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.wso2.carbon.CarbonConstants;
+import org.wso2.carbon.context.CarbonContext;
+import org.wso2.carbon.identity.central.log.mgt.utils.LoggerUtils;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.user.registration.mgt.Constants.ErrorMessages;
 import org.wso2.carbon.identity.user.registration.mgt.exception.RegistrationClientException;
 import org.wso2.carbon.identity.user.registration.mgt.exception.RegistrationServerException;
+import org.wso2.carbon.user.core.util.UserCoreUtil;
+import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
 public class RegistrationMgtUtils {
 
@@ -93,5 +101,55 @@ public class RegistrationMgtUtils {
             description = String.format(description, data);
         }
         return new RegistrationClientException(error.getCode(), error.getMessage(), description);
+    }
+
+    /**
+     * Check whether the v2 audit logs are enabled.
+     *
+     * @return true if v2 audit logs are enabled.
+     */
+    public static boolean isEnableV2AuditLogs() {
+
+        return Boolean.parseBoolean(System.getProperty(ENABLE_V2_AUDIT_LOGS));
+    }
+
+    /**
+     * Get the initiator for audit logs.
+     *
+     * @return Initiator id despite masking.
+     */
+    public static String getInitiatorId() {
+
+        String initiator = null;
+        String username = MultitenantUtils.getTenantAwareUsername(getUser());
+        String tenantDomain = MultitenantUtils.getTenantDomain(getUser());
+        if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(tenantDomain)) {
+            initiator = IdentityUtil.getInitiatorId(username, tenantDomain);
+        }
+        if (StringUtils.isBlank(initiator)) {
+            if (username.equals(CarbonConstants.REGISTRY_SYSTEM_USERNAME)) {
+                // If the initiator is wso2.system, we need not to mask the username.
+                return LoggerUtils.Initiator.System.name();
+            }
+            initiator = LoggerUtils.getMaskedContent(getUser());
+        }
+        return initiator;
+    }
+
+    /**
+     * To get the current user, who is doing the current task.
+     *
+     * @return current logged-in user.
+     */
+    private static String getUser() {
+
+        String user = CarbonContext.getThreadLocalCarbonContext().getUsername();
+        if (StringUtils.isNotEmpty(user)) {
+            user = UserCoreUtil
+                    .addTenantDomainToEntry(user, CarbonContext.getThreadLocalCarbonContext().getTenantDomain());
+        } else {
+            user = CarbonConstants.REGISTRY_SYSTEM_USERNAME;
+        }
+        return user;
     }
 }
