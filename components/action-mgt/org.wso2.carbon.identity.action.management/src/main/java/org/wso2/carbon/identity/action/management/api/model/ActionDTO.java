16d36a823a4d9dd6d3380cd22b9a735261ef206d
osandamaleesha <osandamaleesha@gmail.com>
2025-03-11 11:42:22 +0530

    Modify DAO layer to handle different action property types

--- components/action-mgt/org.wso2.carbon.identity.action.management/src/main/java/org/wso2/carbon/identity/action/management/api/model/ActionDTO.java
+++ components/action-mgt/org.wso2.carbon.identity.action.management/src/main/java/org/wso2/carbon/identity/action/management/api/model/ActionDTO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2024-2025, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -19,6 +19,7 @@
 package org.wso2.carbon.identity.action.management.api.model;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Action Data Transfer Object.
@@ -35,6 +36,30 @@ public class ActionDTO {
     private final Map<String, Object> properties;
 
     public ActionDTO(Builder builder) {
+
+        this.id = builder.id;
+        this.type = builder.type;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.status = builder.status;
+        this.endpoint = builder.endpoint;
+        this.rule = builder.rule;
+        this.properties = builder.properties;
+    }
+
+    public ActionDTO(BuilderForService builder) {
+
+        this.id = builder.id;
+        this.type = builder.type;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.status = builder.status;
+        this.endpoint = builder.endpoint;
+        this.rule = builder.rule;
+        this.properties = builder.properties;
+    }
+
+    public ActionDTO(BuilderForData builder) {
 
         this.id = builder.id;
         this.type = builder.type;
@@ -135,6 +160,95 @@ public class ActionDTO {
         public Builder properties(Map<String, Object> properties) {
 
             this.properties = properties;
+            return this;
+        }
+
+        public ActionDTO build() {
+
+            return new ActionDTO(this);
+        }
+    }
+
+    /**
+     * Builder for ActionDTO model used in Action Converter.
+     */
+    public static class BuilderForService {
+
+        private final String id;
+        private final Action.ActionTypes type;
+        private final String name;
+        private final String description;
+        private final Action.Status status;
+        private final EndpointConfig endpoint;
+        private final ActionRule rule;
+        private Map<String, Object> properties;
+
+        public BuilderForService(ActionDTO actionDTO) {
+
+            this.id = actionDTO.getId();
+            this.type = actionDTO.getType();
+            this.name = actionDTO.getName();
+            this.description = actionDTO.getDescription();
+            this.status = actionDTO.getStatus();
+            this.endpoint = actionDTO.getEndpoint();
+            this.rule = actionDTO.getActionRule();
+            this.properties = actionDTO.getProperties();
+        }
+
+        public BuilderForService(Action action) {
+
+            this.id = action.getId();
+            this.type = action.getType();
+            this.name = action.getName();
+            this.description = action.getDescription();
+            this.status = action.getStatus();
+            this.endpoint = action.getEndpoint();
+            this.rule = action.getActionRule();
+        }
+
+        public BuilderForService properties(Map<String, ActionPropertyForService> properties) {
+
+            this.properties = properties.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,
+                    Map.Entry::getValue));
+            return this;
+        }
+
+        public ActionDTO build() {
+
+            return new ActionDTO(this);
+        }
+    }
+
+    /**
+     * Builder for ActionDTO model used in DAO service impl.
+     */
+    public static class BuilderForData {
+
+        private final String id;
+        private final Action.ActionTypes type;
+        private final String name;
+        private final String description;
+        private final Action.Status status;
+        private final EndpointConfig endpoint;
+        private final ActionRule rule;
+        private Map<String, Object> properties;
+
+        public BuilderForData(ActionDTO actionDTO) {
+
+            this.id = actionDTO.getId();
+            this.type = actionDTO.getType();
+            this.name = actionDTO.getName();
+            this.description = actionDTO.getDescription();
+            this.status = actionDTO.getStatus();
+            this.endpoint = actionDTO.getEndpoint();
+            this.rule = actionDTO.getActionRule();
+            this.properties = actionDTO.getProperties();
+        }
+
+        public BuilderForData properties(Map<String, ActionPropertyForDAO> properties) {
+
+            this.properties = properties.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,
+                    Map.Entry::getValue));
             return this;
         }
 
