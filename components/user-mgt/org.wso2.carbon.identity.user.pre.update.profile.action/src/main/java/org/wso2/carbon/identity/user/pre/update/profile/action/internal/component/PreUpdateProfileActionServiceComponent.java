05d13bd95461ed45280c790f92eed1fa825a067b
malithie <malithim@wso2.com>
2025-03-16 11:40:27 +0530

    Add request builder and response processor.

--- components/user-mgt/org.wso2.carbon.identity.user.pre.update.profile.action/src/main/java/org/wso2/carbon/identity/user/pre/update/profile/action/internal/component/PreUpdateProfileActionServiceComponent.java
+++ components/user-mgt/org.wso2.carbon.identity.user.pre.update.profile.action/src/main/java/org/wso2/carbon/identity/user/pre/update/profile/action/internal/component/PreUpdateProfileActionServiceComponent.java
@@ -25,10 +25,20 @@ import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.identity.action.execution.api.service.ActionExecutionRequestBuilder;
+import org.wso2.carbon.identity.action.execution.api.service.ActionExecutionResponseProcessor;
+import org.wso2.carbon.identity.action.execution.api.service.ActionExecutorService;
 import org.wso2.carbon.identity.action.management.api.service.ActionConverter;
 import org.wso2.carbon.identity.action.management.api.service.ActionDTOModelResolver;
+import org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataManagementService;
+import org.wso2.carbon.identity.user.pre.update.profile.action.internal.execution.PreUpdateProfileRequestBuilder;
+import org.wso2.carbon.identity.user.pre.update.profile.action.internal.execution.PreUpdateProfileResponseProcessor;
 import org.wso2.carbon.identity.user.pre.update.profile.action.internal.management.PreUpdateProfileActionConverter;
 import org.wso2.carbon.identity.user.pre.update.profile.action.internal.management.PreUpdateProfileActionDTOModelResolver;
+import org.wso2.carbon.user.core.service.RealmService;
 
 /**
  * OSGI Service component for the Pre Update Profile Action.
@@ -53,6 +63,13 @@ public class PreUpdateProfileActionServiceComponent {
             bundleCtx.registerService(ActionDTOModelResolver.class, new PreUpdateProfileActionDTOModelResolver(), null);
             LOG.debug("Pre Update Profile Action DTO Model Resolver is enabled");
 
+            bundleCtx.registerService(ActionExecutionRequestBuilder.class, new PreUpdateProfileRequestBuilder(), null);
+            LOG.debug("Pre Update Profile Action Request Builder is registered");
+
+            bundleCtx.registerService(
+                    ActionExecutionResponseProcessor.class, new PreUpdateProfileResponseProcessor(), null);
+            LOG.debug("Pre Update Profile Action Response Processor is registered");
+
             LOG.debug("Pre Update Profile Action bundle is activated");
         } catch (Throwable e) {
             LOG.error("Error while initializing Pre Update Profile Action service component.", e);
@@ -63,5 +80,60 @@ public class PreUpdateProfileActionServiceComponent {
     protected void deactivate(ComponentContext context) {
 
         LOG.debug("Pre Update Profile Action bundle is deactivated");
+    }
+
+    @Reference(
+            name = "action.execution.service",
+            service = ActionExecutorService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetActionExecutorService"
+    )
+    protected void setActionExecutionService(ActionExecutorService actionExecutorService) {
+
+        PreUpdateProfileActionServiceComponentHolder.getInstance().setActionExecutorService(actionExecutorService);
+        LOG.debug("ActionExecutorService set in PreUpdateProfileActionServiceComponentHolder bundle.");
+    }
+
+    protected void unsetActionExecutorService(ActionExecutorService actionExecutorService) {
+
+        PreUpdateProfileActionServiceComponentHolder.getInstance().setActionExecutorService(null);
+        LOG.debug("ActionExecutorService unset in PreUpdateProfileActionServiceComponentHolder bundle.");
+    }
+
+    @Reference(
+            name = "realm.service",
+            service = org.wso2.carbon.user.core.service.RealmService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetRealmService")
+    protected void setRealmService(RealmService realmService) {
+
+        PreUpdateProfileActionServiceComponentHolder.getInstance().setRealmService(realmService);
+        LOG.debug("RealmService set in PreUpdateProfileActionServiceComponentHolder bundle.");
+    }
+
+    protected void unsetRealmService(RealmService realmService) {
+
+        PreUpdateProfileActionServiceComponentHolder.getInstance().setRealmService(null);
+        LOG.debug("RealmService unset in PreUpdateProfileActionServiceComponentHolder bundle.");
+    }
+
+    @Reference(
+            name = "claim.metadata.management.service",
+            service = org.wso2.carbon.identity.claim.metadata.mgt.ClaimMetadataManagementService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetClaimMetadataManagementService")
+    protected void setClaimMetadataManagementService(ClaimMetadataManagementService claimManagementService) {
+
+        PreUpdateProfileActionServiceComponentHolder.getInstance().setClaimManagementService(claimManagementService);
+        LOG.debug("ClaimMetadataManagementService set in PreUpdateProfileActionServiceComponentHolder bundle.");
+    }
+
+    protected void unsetClaimMetadataManagementService(ClaimMetadataManagementService claimManagementService) {
+
+        PreUpdateProfileActionServiceComponentHolder.getInstance().setClaimManagementService(null);
+        LOG.debug("ClaimMetadataManagementService unset in PreUpdateProfileActionServiceComponentHolder bundle.");
     }
 }
