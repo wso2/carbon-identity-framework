2f1e87c38e513028b81618b215cf3dd4230de567
roshanemoraes <moraes.git@gmail.com>
2025-03-17 11:59:00 +0530

    Add unit tests for AsyncStatusMgtDAO.

--- components/async-status-mgt/org.wso2.carbon.identity.framework.async.status.mgt/src/test/java/org/wso2/carbon/identity/framework/async/status/mgt/constants/TestAsyncOperationConstants.java
+++ components/async-status-mgt/org.wso2.carbon.identity.framework.async.status.mgt/src/test/java/org/wso2/carbon/identity/framework/async/status/mgt/constants/TestAsyncOperationConstants.java
@@ -5,4 +5,42 @@ package org.wso2.carbon.identity.framework.async.status.mgt.constants;
  */
 public class TestAsyncOperationConstants {
 
+    //Organizations.
+    public static final String IDN_RESIDENT_ORG_ID_1 = "10084a8d-113f-4211-a0d5-efe36b082211";
+    public static final String IDN_RESIDENT_ORG_ID_2 = "c524c30a-cbd4-4169-ac9d-1ee3edf1bf16";
+    public static final String IDN_RESIDENT_ORG_ID_3 = "cd5a1dcb-fff2-4c14-a073-c07b3caf1757";
+    public static final String IDN_RESIDENT_ORG_ID_4 = "7cb4ab7e-9a25-44bd-a9e0-cf4e07d804dc";
+    public static final String IDN_RESIDENT_ORG_ID_5 = "440ad5b6-6a41-4da7-aadd-272995d0e5db";
+
+    //Operation Subjects.
+    public static final String IDN_OPERATION_SUBJECT_ID_1 = "448e57e7-ff6b-4c31-a1eb-2a0e2d635b2a";
+    public static final String IDN_OPERATION_SUBJECT_ID_2 = "558e57e7-ff6b-4c31-a1eb-2a0e2d635b2a";
+    public static final String IDN_OPERATION_SUBJECT_ID_3 = "668e57e7-ff6b-4c31-a1eb-2a0e2d635b2b";
+    public static final String IDN_OPERATION_SUBJECT_ID_4 = "778e57e7-ff6b-4c31-a1eb-2a0e2d635b2c";
+    public static final String IDN_OPERATION_SUBJECT_ID_5 = "888e57e7-ff6b-4c31-a1eb-2a0e2d635b2c";
+
+    //Operation Initiators.
+    public static final String IDN_OPERATION_INITIATOR_ID_1 = "443k57a1-gl6b-4c31-a1eb-2a0e2d635b2a";
+    public static final String IDN_OPERATION_INITIATOR_ID_2 = "553k57a1-gl6b-4c31-a1eb-2a0e2d635b2a";
+    public static final String IDN_OPERATION_INITIATOR_ID_3 = "663k57a1-gl6b-4c31-a1eb-2a0e2d635b2b";
+    public static final String IDN_OPERATION_INITIATOR_ID_4 = "773k57a1-gl6b-4c31-a1eb-2a0e2d635b2c";
+    public static final String IDN_OPERATION_INITIATOR_ID_5 = "883k57a1-gl6b-4c31-a1eb-2a0e2d635b2c";
+
+    //Operation Types.
+    public static final String IDN_OPERATION_TYPE_1 = "USER";
+    public static final String IDN_OPERATION_TYPE_2 = "APPLICATION";
+
+    //Operation Subject Types.
+    public static final String IDN_OPERATION_SUBJECT_TYPE_1 = "B2B_USER_SHARE";
+    public static final String IDN_OPERATION_SUBJECT_TYPE_2 = "B2B_APPLICATION_SHARE";
+
+    //Operation Status.
+    public static final String IDN_OPERATION_STATUS_1 = "SUCCESS";
+    public static final String IDN_OPERATION_STATUS_2 = "FAIL";
+    public static final String IDN_OPERATION_STATUS_3 = "PARTIAL SUCCESS";
+
+    //Operation Policies.
+    public static final String IDN_OPERATION_POLICY_1 = "SHARE_WITH_ALL";
+    public static final String IDN_OPERATION_POLICY_2 = "SELECTIVE_SHARE";
+    public static final String IDN_OPERATION_POLICY_3 = "DO_NOT_SHARE";
 }
--- components/async-status-mgt/org.wso2.carbon.identity.framework.async.status.mgt/src/test/java/org/wso2/carbon/identity/framework/async/status/mgt/util/TestUtils.java
+++ components/async-status-mgt/org.wso2.carbon.identity.framework.async.status.mgt/src/test/java/org/wso2/carbon/identity/framework/async/status/mgt/util/TestUtils.java
@@ -29,7 +29,7 @@ public class TestUtils {
     public static String getFilePath(String fileName) {
 
         if (StringUtils.isNotBlank(fileName)) {
-            return Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "dbscripts",
+            return Paths.get(System.getProperty("user.dir"), "src", "test", "resources", "dbScripts",
                     fileName).toString();
         }
         throw new IllegalArgumentException("DB Script file name cannot be empty.");
