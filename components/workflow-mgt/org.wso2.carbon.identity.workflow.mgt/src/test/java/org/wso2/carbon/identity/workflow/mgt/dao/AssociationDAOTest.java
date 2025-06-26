ade6922c637e7fee574370347a60d025780f9942
sadilchamishka <sandilchamishka@gmail.com>
2025-06-26 13:51:09 +0530

    Add unit test for workflow association update

--- components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/test/java/org/wso2/carbon/identity/workflow/mgt/dao/AssociationDAOTest.java
+++ components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/test/java/org/wso2/carbon/identity/workflow/mgt/dao/AssociationDAOTest.java
@@ -21,18 +21,23 @@ package org.wso2.carbon.identity.workflow.mgt.dao;
 import org.mockito.MockedStatic;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.core.util.IdentityDatabaseUtil;
+import org.wso2.carbon.identity.workflow.mgt.dto.Association;
 import org.wso2.carbon.identity.workflow.mgt.exception.WorkflowClientException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertThrows;
 
 public class AssociationDAOTest {
 
     @Test
-    public void testGetAssociationNumberFormatException() {
+    public void testGetAssociationByInvalidAssociationId() {
 
         try (MockedStatic<IdentityDatabaseUtil> identityDatabaseUtil = mockStatic(IdentityDatabaseUtil.class)) {
             Connection mockConnection = mock(Connection.class);
@@ -41,6 +46,27 @@ public class AssociationDAOTest {
             AssociationDAO dao = new AssociationDAO();
             String invalidAssociationId = "invalid_id";
             assertThrows(WorkflowClientException.class, () -> dao.getAssociation(invalidAssociationId));
+        }
+    }
+
+    @Test
+    public void testUpdateAssociationByInvalidAssociationId() throws SQLException {
+
+        try (MockedStatic<IdentityDatabaseUtil> identityDatabaseUtil = mockStatic(IdentityDatabaseUtil.class)) {
+            Connection mockConnection = mock(Connection.class);
+            PreparedStatement mockPreparedStatement = mock(PreparedStatement.class);
+            identityDatabaseUtil.when(IdentityDatabaseUtil::getDBConnection).thenReturn(mockConnection);
+
+            when(mockConnection.prepareStatement(anyString())).thenReturn(mockPreparedStatement);
+
+            AssociationDAO dao = new AssociationDAO();
+            Association associationDTO = new Association();
+            associationDTO.setEventId("ADD_USER");
+            associationDTO.setAssociationName("User Addition Association");
+            associationDTO.setCondition("true");
+            associationDTO.setWorkflowId("12345");
+            associationDTO.setAssociationId("invalid_id");
+            assertThrows(WorkflowClientException.class, () -> dao.updateAssociation(associationDTO));
         }
     }
 }
