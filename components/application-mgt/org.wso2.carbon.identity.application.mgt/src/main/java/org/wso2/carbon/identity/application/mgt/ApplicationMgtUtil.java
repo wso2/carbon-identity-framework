e5d3b1a503f51eab974cf88485f5257a415e3eda
JeethJJ <jihanjeed49@gmail.com>
2025-07-28 13:27:12 +0530

    Add support for shared apps in resolveOriginUrlFromPlaceholders.

--- components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/ApplicationMgtUtil.java
+++ components/application-mgt/org.wso2.carbon.identity.application.mgt/src/main/java/org/wso2/carbon/identity/application/mgt/ApplicationMgtUtil.java
@@ -1185,6 +1185,21 @@ public class ApplicationMgtUtil {
     public static String resolveOriginUrlFromPlaceholders(String absoluteUrl, String appName)
             throws URLBuilderException {
 
+        return resolveOriginUrlFromPlaceholders(absoluteUrl, appName, false);
+    }
+
+    /**
+     * This method is used to replace placeholders with the hostname and port of URLs for the portal apps.
+     *
+     * @param absoluteUrl                       The URL which need to resolve from placeholders.
+     * @param appName                           Application name.
+     * @param subOrgAppWithBaseURLPlaceholder   If provided app is a shared app containing baseURL placeholder.
+     * @return The resolved URL from placeholders.
+     * @throws URLBuilderException If any error occurs when building absolute public url without path.
+     */
+    public static String resolveOriginUrlFromPlaceholders(String absoluteUrl, String appName,
+                                                          boolean subOrgAppWithBaseURLPlaceholder) throws URLBuilderException {
+
         if (StringUtils.isEmpty(appName)) {
             return resolveOriginUrlFromPlaceholders(absoluteUrl);
         }
@@ -1195,16 +1210,14 @@ public class ApplicationMgtUtil {
             basePath = IdentityUtil.getProperty(MYACCOUNT_ACCESS_ORIGIN);
         }
 
+        if (subOrgAppWithBaseURLPlaceholder) {
+            basePath = ServiceURLBuilder.create().build().getAbsolutePublicUrlWithoutPath();
+        }
+
         if (StringUtils.isEmpty(basePath)) {
             return resolveOriginUrlFromPlaceholders(absoluteUrl);
         }
-        absoluteUrl = StringUtils.replace(absoluteUrl, BASE_URL_PLACEHOLDER, basePath);
-
-        if (ApplicationConstants.MY_ACCOUNT_APPLICATION_NAME.equals(appName)) {
-            String consoleBasePath = IdentityUtil.getProperty(CONSOLE_ACCESS_ORIGIN);
-            absoluteUrl = StringUtils.replace(absoluteUrl, BASE_URL_CUSTOM_PLACEHOLDER, consoleBasePath);
-        }
-        return absoluteUrl;
+        return StringUtils.replace(absoluteUrl, BASE_URL_PLACEHOLDER, basePath);
     }
 
     /**
