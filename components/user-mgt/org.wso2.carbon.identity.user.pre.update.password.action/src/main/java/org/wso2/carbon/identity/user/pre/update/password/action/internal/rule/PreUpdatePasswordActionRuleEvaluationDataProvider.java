01dacea47962b279808c8a00f63113f422b0f569
Lashen1227 <lashenmartino@gmail.com>
2025-08-07 00:23:46 +0530

    feat: add application initiated registration with password flow

--- components/user-mgt/org.wso2.carbon.identity.user.pre.update.password.action/src/main/java/org/wso2/carbon/identity/user/pre/update/password/action/internal/rule/PreUpdatePasswordActionRuleEvaluationDataProvider.java
+++ components/user-mgt/org.wso2.carbon.identity.user.pre.update.password.action/src/main/java/org/wso2/carbon/identity/user/pre/update/password/action/internal/rule/PreUpdatePasswordActionRuleEvaluationDataProvider.java
@@ -51,8 +51,9 @@ public class PreUpdatePasswordActionRuleEvaluationDataProvider implements RuleEv
         APPLICATION_INITIATED_PASSWORD_UPDATE("applicationInitiatedPasswordUpdate"),
         USER_INITIATED_PASSWORD_UPDATE("userInitiatedPasswordUpdate"),
         USER_INITIATED_PASSWORD_RESET("userInitiatedPasswordReset"),
-        ADMIN_REGISTER_USER_WITH_PASSWORD("adminRegisterUserWithPassword"),
-        USER_REGISTER_WITH_PASSWORD("userRegisterWithPassword");
+        ADMIN_INITIATED_REGISTRATION_WITH_PASSWORD("adminInitiatedRegistrationWithPassword"),
+        APPLICATION_INITIATED_REGISTRATION_WITH_PASSWORD("applicationInitiatedRegistrationWithPassword"),
+        USER_INITIATED_REGISTRATION_WITH_PASSWORD("userInitiatedRegistrationWithPassword");
 
         final String flowName;
 
@@ -129,12 +130,17 @@ public class PreUpdatePasswordActionRuleEvaluationDataProvider implements RuleEv
 
         if (flow.getName() == Flow.Name.USER_REGISTRATION &&
                 flow.getInitiatingPersona() == Flow.InitiatingPersona.ADMIN) {
-            return PasswordUpdateFlowType.ADMIN_REGISTER_USER_WITH_PASSWORD.getFlowName();
+            return PasswordUpdateFlowType.ADMIN_INITIATED_REGISTRATION_WITH_PASSWORD.getFlowName();
+        }
+
+        if (flow.getName() == Flow.Name.USER_REGISTRATION &&
+                flow.getInitiatingPersona() == Flow.InitiatingPersona.APPLICATION) {
+            return PasswordUpdateFlowType.APPLICATION_INITIATED_REGISTRATION_WITH_PASSWORD.getFlowName();
         }
 
         if (flow.getName() == Flow.Name.USER_REGISTRATION &&
                 flow.getInitiatingPersona() == Flow.InitiatingPersona.USER) {
-            return PasswordUpdateFlowType.USER_REGISTER_WITH_PASSWORD.getFlowName();
+            return PasswordUpdateFlowType.USER_INITIATED_REGISTRATION_WITH_PASSWORD.getFlowName();
         }
 
         throw new RuleEvaluationDataProviderException("Unsupported flow type: " + flow.getName() +
