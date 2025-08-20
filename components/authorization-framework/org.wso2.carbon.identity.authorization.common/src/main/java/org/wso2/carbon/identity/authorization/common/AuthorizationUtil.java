f3626f974f7658282628ad6e78b26da19b04b249
Madhavi Gayathri <madhavi.17@cse.mrt.ac.lk>
2025-08-20 15:08:55 +0530

    Introduce AuthorizationUtil component.

---
+++ components/authorization-framework/org.wso2.carbon.identity.authorization.common/src/main/java/org/wso2/carbon/identity/authorization/common/AuthorizationUtil.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.authorization.common;
+
+import org.wso2.carbon.context.OperationScopeValidationContext;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.identity.authorization.common.exception.ForbiddenException;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Utility class for authorization operations.
+ */
+public class AuthorizationUtil {
+
+    /**
+     * Validates the operation scopes for the given operation name.
+     *
+     * @param operationName Name of the operation to validate.
+     * @throws ForbiddenException If the operation is not permitted.
+     */
+    public static void validateOperationScopes(String operationName) throws ForbiddenException {
+
+        OperationScopeValidationContext operationScopeValidationContext =
+                PrivilegedCarbonContext.getThreadLocalCarbonContext().getOperationScopeValidationContext();
+
+        if (operationScopeValidationContext.isValidationRequired()) {
+            List<String> allowedScopes = operationScopeValidationContext.getValidatedScopes();
+            Map<String, String> operationScopeMap = operationScopeValidationContext.getOperationScopeMap();
+            String operationScope = operationScopeMap.get(operationName);
+
+            if (!allowedScopes.contains(operationScope)) {
+                throw new ForbiddenException("Operation is not permitted. You do not have permissions to make " +
+                        "this request.");
+            }
+        }
+    }
+}
