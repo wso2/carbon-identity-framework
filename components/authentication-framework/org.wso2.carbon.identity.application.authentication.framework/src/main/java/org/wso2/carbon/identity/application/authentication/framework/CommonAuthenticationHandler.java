c86e9194fb9c5261e8b929d14c7fa1d9ad9b3c53
ImalshaD <plid475@gmail.com>
2025-08-26 13:47:14 +0530

    Improve cookie validation error handling and enhance exception details

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/CommonAuthenticationHandler.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/CommonAuthenticationHandler.java
@@ -22,6 +22,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.config.ConfigurationFacade;
 import org.wso2.carbon.identity.application.authentication.framework.exception.CookieValidationFailedException;
+import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkErrorConstants;
 import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkUtils;
 
 import java.io.IOException;
@@ -57,8 +58,11 @@ public class CommonAuthenticationHandler {
             FrameworkUtils.getRequestCoordinator().handle(request, response);
         } catch (CookieValidationFailedException e) {
 
-            log.warn("Session nonce cookie validation has failed for the sessionDataKey: "
+            if (!FrameworkErrorConstants.ErrorMessages.ERROR_MISMATCHING_TENANT_DOMAIN.getCode()
+                    .equals(e.getErrorCode())) {
+                log.warn("Session nonce cookie validation has failed for the sessionDataKey: "
                         + request.getParameter("sessionDataKey") + ". Hence, restarting the login flow.");
+            }
             FrameworkUtils.getRequestCoordinator().handle(request, response);
         }
     }
