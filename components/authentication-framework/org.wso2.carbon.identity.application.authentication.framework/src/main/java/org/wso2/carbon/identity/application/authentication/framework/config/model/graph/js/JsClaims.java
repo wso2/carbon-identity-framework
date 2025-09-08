1328fc927452fe71652e67f5f6494b275880b273
Ashan Thamara Palihakkara <75057725+ashanthamara@users.noreply.github.com>
2025-09-08 10:24:49 +0530

    Remove unused import

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/config/model/graph/js/JsClaims.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/config/model/graph/js/JsClaims.java
@@ -52,7 +52,6 @@ import org.wso2.carbon.user.core.service.RealmService;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 /**
