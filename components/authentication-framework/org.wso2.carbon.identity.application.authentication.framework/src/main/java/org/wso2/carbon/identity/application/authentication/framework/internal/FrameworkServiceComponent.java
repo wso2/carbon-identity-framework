15c04824300c2e1a71af35740f374304afebd6b6
sahandilshan <sahandilshan222@gmail.com>
2025-09-22 17:04:32 +0530

    Validate boolean strings

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/internal/FrameworkServiceComponent.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/internal/FrameworkServiceComponent.java
@@ -19,6 +19,7 @@
 package org.wso2.carbon.identity.application.authentication.framework.internal;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -128,6 +129,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import javax.servlet.Servlet;
 
@@ -403,7 +405,13 @@ public class FrameworkServiceComponent {
         boolean timeOutEnabled = FrameworkConstants.AdaptiveAuthentication
                 .DEFAULT_EXECUTION_SUPERVISOR_TIMEOUT_ENABLE;
         if (StringUtils.isNotBlank(timeOutEnabledString)) {
-            timeOutEnabled = Boolean.parseBoolean(timeOutEnabledString);
+            try {
+                timeOutEnabled = BooleanUtils.toBoolean(timeOutEnabledString.toLowerCase(Locale.ROOT),
+                        Boolean.TRUE.toString(), Boolean.FALSE.toString());
+            } catch (IllegalArgumentException e) {
+                log.error("Error while parsing adaptive authentication execution supervisor timeout enable config: "
+                        + timeOutEnabledString + ", setting timeout enable to default value: " + timeOutEnabled, e);
+            }
         }
         String timeoutString = IdentityUtil.getProperty(
                 FrameworkConstants.AdaptiveAuthentication.CONF_EXECUTION_SUPERVISOR_TIMEOUT);
