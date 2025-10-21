bb59d3fefd9046ff7cf6e74d93d49619061a760d
thisarawelmilla <thisara@wso2.com>
2025-10-21 13:38:51 +0530

    Improve x-wso2-api-version header resolving

--- components/action-mgt/org.wso2.carbon.identity.action.execution/src/main/java/org/wso2/carbon/identity/action/execution/internal/util/APIClient.java
+++ components/action-mgt/org.wso2.carbon.identity.action.execution/src/main/java/org/wso2/carbon/identity/action/execution/internal/util/APIClient.java
@@ -49,6 +49,8 @@ import org.wso2.carbon.identity.action.execution.internal.service.impl.ResponseD
 import java.io.IOException;
 import java.net.SocketTimeoutException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class is responsible for making API calls to the external services.
@@ -80,17 +82,27 @@ public class APIClient {
                 .build();
     }
 
+    /**
+     * Makes a POST API call to the given URL with the provided payload and headers.
+     *
+     * @param actionType  Action type.
+     * @param url         URL of the API endpoint.
+     * @param authMethod  Authentication method to be used.
+     * @param headers     Headers to be included in the request.
+     * @param payload     Payload to be sent in the request body.
+     * @return ActionInvocationResponse containing the response or error details.
+     */
     public ActionInvocationResponse callAPI(ActionType actionType, String url, AuthMethods.AuthMethod authMethod,
-                                            String payload) {
+                                            Map<String, String> headers, String payload) {
 
         HttpPost httpPost = new HttpPost(url);
-        setRequestEntity(httpPost, payload, authMethod, actionType);
+        setRequestEntity(httpPost, payload, authMethod, headers);
 
         return executeRequest(actionType, httpPost);
     }
 
     private void setRequestEntity(HttpPost httpPost, String jsonRequest, AuthMethods.AuthMethod authMethod,
-                                  ActionType actionType) {
+                                  Map<String, String> headers) {
 
         StringEntity entity = new StringEntity(jsonRequest, StandardCharsets.UTF_8);
         if (authMethod != null) {
@@ -99,8 +111,9 @@ public class APIClient {
         httpPost.setEntity(entity);
         httpPost.setHeader("Accept", "application/json");
         httpPost.setHeader("Content-type", "application/json");
-        httpPost.setHeader(ActionAPIVersionResolver.API_VERSION_HEADER,
-                ActionAPIVersionResolver.resolveAPIVersion(actionType));
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            httpPost.setHeader(header.getKey(), header.getValue());
+        }
     }
 
     private ActionInvocationResponse executeRequest(ActionType actionType, HttpPost request) {
