2cbb514c08e7134e779c0823a115cce17ce42135
thisarawelmilla <thisara@wso2.com>
2025-11-11 10:48:59 +0530

    Introduce new component to token retrieval.

---
+++ components/external-api-framework/org.wso2.carbon.identity.external.api.token.handler/src/main/java/org/wso2/carbon/identity/external/api/token/handler/api/service/TokenAcquirerService.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.external.api.token.handler.api.service;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.external.api.client.api.exception.APIClientException;
+import org.wso2.carbon.identity.external.api.client.api.model.APIClientConfig;
+import org.wso2.carbon.identity.external.api.client.api.model.APIInvocationConfig;
+import org.wso2.carbon.identity.external.api.client.api.model.APIRequestContext;
+import org.wso2.carbon.identity.external.api.client.api.model.APIResponse;
+import org.wso2.carbon.identity.external.api.client.api.service.AbstractAPIClientManager;
+import org.wso2.carbon.identity.external.api.token.handler.api.exception.TokenHandlerException;
+import org.wso2.carbon.identity.external.api.token.handler.api.model.TokenRequestContext;
+import org.wso2.carbon.identity.external.api.token.handler.api.model.TokenResponse;
+import org.wso2.carbon.identity.external.api.token.handler.internal.util.TokenRequestBuilderUtils;
+
+/**
+ * Service class for acquiring tokens using different grant types.
+ */
+public class TokenAcquirerService extends AbstractAPIClientManager {
+
+    private static final Log LOG = LogFactory.getLog(TokenAcquirerService.class);
+
+    private TokenRequestContext tokenRequestContext;
+    private APIInvocationConfig apiInvocationConfig = new APIInvocationConfig();
+
+    /**
+     * Constructor to initialize API client configuration.
+     *
+     * @param apiClientConfig API client configuration.
+     */
+    public TokenAcquirerService(APIClientConfig apiClientConfig) {
+
+        super(apiClientConfig);
+    }
+
+    /**
+     * Set API invocation configuration.
+     *
+     * @param apiInvocationConfig API invocation configuration.
+     */
+    public void setApiInvocationConfig(APIInvocationConfig apiInvocationConfig) {
+
+        this.apiInvocationConfig = apiInvocationConfig;
+    }
+
+    /**
+     * Set token request context.
+     *
+     * @param tokenRequestContext Token request context.
+     */
+    public void setTokenRequestContext(TokenRequestContext tokenRequestContext) {
+
+        this.tokenRequestContext = tokenRequestContext;
+    }
+
+    /**
+     * Get a new access token using the token request context.
+     *
+     * @return Token response.
+     * @throws TokenHandlerException TokenHandlerException.
+     */
+    public TokenResponse getNewAccessToken() throws TokenHandlerException {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Attempting to acquire new access token.");
+        }
+
+        if (tokenRequestContext == null) {
+            throw new TokenHandlerException("Token request context is not initialized.");
+        }
+
+        try {
+            APIRequestContext apiRequestContext = TokenRequestBuilderUtils.buildAPIRequestContext(tokenRequestContext);
+            APIResponse apiResponse = callAPI(apiRequestContext, apiInvocationConfig);
+            TokenResponse tokenResponse = handleResponse(apiResponse);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Successfully acquired new access token.");
+            }
+            return tokenResponse;
+        } catch (APIClientException e) {
+            throw new TokenHandlerException("Error occurred while acquiring access token", e);
+        }
+    }
+
+    /**
+     * Get a new access token using the refresh token grant.
+     *
+     * @param refreshToken Refresh token.
+     * @return Token response.
+     * @throws TokenHandlerException TokenHandlerException.
+     */
+    public TokenResponse getNewAccessTokenFromRefreshGrant(String refreshToken) throws TokenHandlerException {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Attempting to acquire new access token using refresh token grant.");
+        }
+
+        if (tokenRequestContext == null) {
+            throw new TokenHandlerException("Token request context is not initialized.");
+        }
+        if (StringUtils.isBlank(refreshToken)) {
+            throw new TokenHandlerException("Refresh token cannot be null or empty.");
+        }
+
+        try {
+            APIRequestContext apiRequestContext = TokenRequestBuilderUtils.buildAPIRequestContextForRefreshGrant(
+                    tokenRequestContext, refreshToken);
+            APIResponse apiResponse = callAPI(apiRequestContext, apiInvocationConfig);
+            TokenResponse tokenResponse = handleResponse(apiResponse);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Successfully acquired new access token from refresh token grant.");
+            }
+            return tokenResponse;
+        } catch (APIClientException e) {
+            throw new TokenHandlerException("Error occurred while acquiring access token from refresh grant", e);
+        }
+    }
+
+    private TokenResponse handleResponse(APIResponse response) throws TokenHandlerException {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Handling token response from the token endpoint.");
+        }
+
+        if (response == null) {
+            throw new TokenHandlerException("Error occured. The API Response the token endpoint is null.");
+        }
+        if (response.getStatusCode() != 200) {
+            throw new TokenHandlerException(
+                    "Error response received from the token endpoint. Status Code: " + response.getStatusCode());
+        }
+
+        TokenResponse tokenResponse = new TokenResponse.Builder(response).build();
+        return tokenResponse;
+    }
+}
---
+++ components/external-api-framework/org.wso2.carbon.identity.external.api.token.handler/src/main/java/org/wso2/carbon/identity/external/api/token/handler/internal/util/TokenRequestBuilderUtils.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.external.api.token.handler.internal.util;
+
+import com.google.gson.JsonObject;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.external.api.client.api.exception.APIClientRequestException;
+import org.wso2.carbon.identity.external.api.client.api.model.APIAuthentication;
+import org.wso2.carbon.identity.external.api.client.api.model.APIRequestContext;
+import org.wso2.carbon.identity.external.api.token.handler.api.exception.TokenRequestException;
+import org.wso2.carbon.identity.external.api.token.handler.api.model.GrantContext;
+import org.wso2.carbon.identity.external.api.token.handler.api.model.GrantContext.Property;
+import org.wso2.carbon.identity.external.api.token.handler.api.model.TokenRequestContext;
+
+import java.util.Map;
+
+/**
+ * Utility class for building token request related objects.
+ */
+public class TokenRequestBuilderUtils {
+
+    private static final Log LOG = LogFactory.getLog(TokenRequestBuilderUtils.class);
+
+    /**
+     * Builds an APIRequestContext for acquiring an access token using the grant type specified in the provided
+     * TokenRequestContext. This method prepares the request context with the appropriate payload and authentication
+     * details required for the token endpoint.
+     *
+     * @param requestContext The context containing grant type and related properties for the token request.
+     * @return An APIRequestContext configured for the token request.
+     * @throws TokenRequestException If an error occurs while building the request context.
+     */
+    public static APIRequestContext buildAPIRequestContext(TokenRequestContext requestContext)
+            throws TokenRequestException {
+
+        return buildBasicAPIRequestContext(requestContext, buildTokenRequestPayload(requestContext));
+    }
+
+    /**
+     * Builds an APIRequestContext for acquiring an access token using the refresh token grant type.
+     *
+     * @param requestContext The context containing details of the token request.
+     * @param refreshToken   The refresh token to be used for obtaining a new access token.
+     * @return An APIRequestContext configured for the refresh token grant.
+     * @throws TokenRequestException If an error occurs while building the APIRequestContext.
+     */
+    public static APIRequestContext buildAPIRequestContextForRefreshGrant(
+            TokenRequestContext requestContext, String refreshToken) throws TokenRequestException {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Building APIRequestContext for refresh token grant.");
+        }
+        JsonObject json = new JsonObject();
+        json.addProperty("grant_type", "refresh_token");
+        json.addProperty("refresh_token", refreshToken);
+        String payload = json.toString();
+        return buildBasicAPIRequestContext(requestContext, payload);
+    }
+
+    private static APIRequestContext buildBasicAPIRequestContext(TokenRequestContext requestContext, String payload)
+            throws TokenRequestException {
+
+        try {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Building basic APIRequestContext for endpoint: " +
+                        requestContext.getTokenEndpointUrl());
+            }
+            APIAuthentication authentication = TokenRequestBuilderUtils
+                    .buildTokenRequestAPIAuthentication(requestContext);
+            APIRequestContext.Builder requestContextBuilder = new APIRequestContext.Builder()
+                    .httpMethod(APIRequestContext.HttpMethod.POST)
+                    .headers(requestContext.getHeaders())
+                    .endpointUrl(requestContext.getTokenEndpointUrl())
+                    .apiAuthentication(authentication)
+                    .payload(payload);
+            APIRequestContext context = requestContextBuilder.build();
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Successfully built APIRequestContext for token request.");
+            }
+            return context;
+        } catch (APIClientRequestException e) {
+            throw new TokenRequestException("Error building API Request Context for token request.", e);
+        }
+    }
+
+    private static APIAuthentication buildTokenRequestAPIAuthentication(TokenRequestContext requestContext)
+            throws TokenRequestException {
+
+        GrantContext grantContext = requestContext.getGrantContext();
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Building API authentication for grant type: " + grantContext.getGrantType());
+        }
+        try {
+            switch (grantContext.getGrantType()) {
+                case CLIENT_CREDENTIAL:
+                    APIAuthentication authentication = new APIAuthentication.Builder()
+                            .authType(APIAuthentication.AuthType.BASIC)
+                            .properties(Map.of(
+                                    APIAuthentication.Property.USERNAME.getName(),
+                                    grantContext.getProperty(Property.CLIENT_ID.getName()),
+                                    APIAuthentication.Property.PASSWORD.getName(),
+                                    grantContext.getProperty(Property.CLIENT_SECRET.getName())
+                            ))
+                            .build();
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Successfully built API authentication for CLIENT_CREDENTIAL grant type.");
+                    }
+                    return authentication;
+                default:
+                    throw new TokenRequestException("Unsupported authentication type: " + grantContext.getGrantType());
+            }
+        } catch (APIClientRequestException e) {
+            throw new TokenRequestException("Error building API Authentication for grant type: " +
+                    grantContext.getGrantType(), e);
+        }
+    }
+
+    private static String buildTokenRequestPayload(TokenRequestContext requestContext) throws TokenRequestException {
+
+        GrantContext grantContext = requestContext.getGrantContext();
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Building token request payload for grant type: " + grantContext.getGrantType());
+        }
+        switch (grantContext.getGrantType()) {
+            case CLIENT_CREDENTIAL:
+                JsonObject json = new JsonObject();
+                json.addProperty("grant_type", "client_credentials");
+                json.addProperty("client_id", grantContext.getProperty(Property.CLIENT_ID.getName()));
+                json.addProperty("client_secret", grantContext.getProperty(Property.CLIENT_SECRET.getName()));
+                json.addProperty("scope", grantContext.getProperty(Property.SCOPE.getName()));
+                String payload = json.toString();
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Successfully built token request payload for CLIENT_CREDENTIAL grant type.");
+                }
+                return payload;
+            default:
+                throw new TokenRequestException("Unsupported authentication type: " + grantContext.getGrantType());
+        }
+    }
+}
