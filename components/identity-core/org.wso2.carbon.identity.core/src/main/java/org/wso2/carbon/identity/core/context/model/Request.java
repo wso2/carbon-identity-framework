473cc1d98ad9b382a09bceda4ed040c89ef1ebf9
Ashan Thamara Palihakkara <75057725+ashanthamara@users.noreply.github.com>
2025-11-21 12:03:58 +0530

    Remove unused import

--- components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/context/model/Request.java
+++ components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/context/model/Request.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
