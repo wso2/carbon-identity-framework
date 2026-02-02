3762c40da01158c73f87055cfe87befe0433b1d3
nishagee <jeewanthadeherath04@gmail.com>
2026-02-02 15:29:21 +0530

    enhance ApprovalWorkflowMetadataProvider to include static fields and dynamic user claims for approval workflows

--- components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/main/java/org/wso2/carbon/identity/workflow/mgt/rule/ApprovalWorkflowMetadataProvider.java
+++ components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/main/java/org/wso2/carbon/identity/workflow/mgt/rule/ApprovalWorkflowMetadataProvider.java
@@ -31,19 +31,21 @@ import org.wso2.carbon.identity.rule.metadata.api.model.FieldDefinition;
 import org.wso2.carbon.identity.rule.metadata.api.model.FlowType;
 import org.wso2.carbon.identity.rule.metadata.api.model.InputValue;
 import org.wso2.carbon.identity.rule.metadata.api.model.Operator;
+import org.wso2.carbon.identity.rule.metadata.api.model.OptionsInputValue;
+import org.wso2.carbon.identity.rule.metadata.api.model.OptionsValue;
 import org.wso2.carbon.identity.rule.metadata.api.model.Value;
 import org.wso2.carbon.identity.rule.metadata.api.provider.RuleMetadataProvider;
 import org.wso2.carbon.identity.workflow.mgt.internal.WorkflowServiceDataHolder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * Approval workflow rule metadata provider.
- * This class is responsible for providing dynamic field definitions for user claims
- * that can be used in workflow approval rule expressions.
- * Claims are fetched dynamically at runtime for workflow flow types.
+ * This class is responsible for providing field definitions
+ * that can be used in approval workflow rule expressions.
  */
 public class ApprovalWorkflowMetadataProvider implements RuleMetadataProvider {
 
@@ -51,38 +53,98 @@ public class ApprovalWorkflowMetadataProvider implements RuleMetadataProvider {
 
     /**
      * Get the expression metadata for the given flow type.
-     * Returns field definitions for all available user claim URIs.
-     * Claims are fetched dynamically at runtime.
+     * Returns field definitions for approval workflow specific fields and dynamic user claim fields.
      *
      * @param flowType     Flow type.
      * @param tenantDomain Tenant domain.
-     * @return List of field definitions for user claims.
+     * @return List of field definitions for approval workflow, or empty list for other flow types.
      * @throws RuleMetadataException If an error occurred while getting the metadata.
      */
     @Override
     public List<FieldDefinition> getExpressionMeta(FlowType flowType, String tenantDomain)
             throws RuleMetadataException {
 
-        List<LocalClaim> localClaims = getAllowedUserClaims(tenantDomain);
-        if (localClaims.isEmpty()) {
+        if (flowType != FlowType.APPROVAL_WORKFLOW) {
             if (LOG.isDebugEnabled()) {
-                LOG.debug("No user claims found for tenant: " + tenantDomain);
+                LOG.debug("Skipping metadata generation for flow type: " + flowType + 
+                        ". This provider only supports APPROVAL_WORKFLOW.");
             }
             return Collections.emptyList();
         }
 
-        List<FieldDefinition> fieldDefinitions = new ArrayList<>();
-        for (LocalClaim localClaim : localClaims) {
-            FieldDefinition fieldDefinition = createFieldDefinitionForClaim(localClaim);
-            fieldDefinitions.add(fieldDefinition);
+        List<FieldDefinition> fieldDefinitions = new ArrayList<>(getStaticApprovalWorkflowFields());
+
+        List<LocalClaim> localClaims = getAllowedUserClaims(tenantDomain);
+        if (!localClaims.isEmpty()) {
+            for (LocalClaim localClaim : localClaims) {
+                FieldDefinition fieldDefinition = createFieldDefinitionForClaim(localClaim);
+                fieldDefinitions.add(fieldDefinition);
+            }
         }
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("Generated " + fieldDefinitions.size() + " field definitions for user claims for tenant: " +
-                    tenantDomain);
+            LOG.debug("Generated " + fieldDefinitions.size() + " field definitions for approval workflow for tenant: "
+                    + tenantDomain);
         }
 
         return fieldDefinitions;
+    }
+
+    /**
+     * Get the list of static, approval workflow specific field definitions.
+     *
+     * @return List of static field definitions.
+     */
+    private List<FieldDefinition> getStaticApprovalWorkflowFields() {
+
+        List<FieldDefinition> staticFields = new ArrayList<>();
+
+        staticFields.add(createRoleHasAddedUsersField());
+        staticFields.add(createRoleHasDeletedUsersField());
+
+        if (LOG.isDebugEnabled()) {
+        LOG.debug("Added " + staticFields.size() + " static approval workflow fields.");
+    }
+
+        return staticFields;
+    }
+
+    /**
+     * Create field definition for role.hasAddedUsers.
+     *
+     * @return FieldDefinition for role.hasAddedUsers.
+     */
+    private FieldDefinition createRoleHasAddedUsersField() {
+
+        Field field = new Field("role.hasAddedUsers", "role has added users");
+        List<Operator> operators = Collections.singletonList(new Operator("equals", "equals"));
+
+        List<OptionsValue> optionValues = Arrays.asList(
+                new OptionsValue("true", "true"),
+                new OptionsValue("false", "false")
+        );
+        Value valueMeta = new OptionsInputValue(Value.ValueType.STRING, optionValues);
+
+        return new FieldDefinition(field, operators, valueMeta);
+    }
+
+    /**
+     * Create field definition for role.hasDeletedUsers.
+     *
+     * @return FieldDefinition for role.hasDeletedUsers.
+     */
+    private FieldDefinition createRoleHasDeletedUsersField() {
+
+        Field field = new Field("role.hasDeletedUsers", "role has deleted users");
+        List<Operator> operators = Collections.singletonList(new Operator("equals", "equals"));
+
+        List<OptionsValue> optionValues = Arrays.asList(
+                new OptionsValue("true", "true"),
+                new OptionsValue("false", "false")
+        );
+        Value valueMeta = new OptionsInputValue(Value.ValueType.STRING, optionValues);
+
+        return new FieldDefinition(field, operators, valueMeta);
     }
 
     /**
@@ -144,7 +206,6 @@ public class ApprovalWorkflowMetadataProvider implements RuleMetadataProvider {
 
     /**
      * Get the display name of a claim.
-     * Returns the DisplayName property if available, otherwise falls back to the claim URI.
      *
      * @param localClaim Local claim.
      * @return Display name of the claim.
@@ -162,7 +223,6 @@ public class ApprovalWorkflowMetadataProvider implements RuleMetadataProvider {
 
     /**
      * Get the list of operators applicable for claim fields.
-     * Returns basic comparison operators: equals and notEquals.
      *
      * @return List of operators.
      */
@@ -170,12 +230,14 @@ public class ApprovalWorkflowMetadataProvider implements RuleMetadataProvider {
 
         List<Operator> operators = new ArrayList<>();
 
-        // Add equals operator.
         operators.add(new Operator("equals", "Equals"));
-        
-        // Add notEquals operator.
+
         operators.add(new Operator("notEquals", "Not Equals"));
+
+        operators.add(new Operator("contains", "Contains"));
 
         return operators;
     }
+
+
 }
