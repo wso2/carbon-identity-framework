f0da6df346f74a83f5b0322b3fc551fe7f222a7c
DilshanSenarath <74205483+DilshanSenarath@users.noreply.github.com>
2026-02-11 17:54:27 +0530

    fix the sonar qube issues

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/test/java/org/wso2/carbon/identity/application/authentication/framework/util/FrameworkUtilsTest.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/test/java/org/wso2/carbon/identity/application/authentication/framework/util/FrameworkUtilsTest.java
@@ -1905,11 +1905,11 @@ public class FrameworkUtilsTest extends IdentityBaseTest {
         int rememberMeTimeout = 432000; // 5 days in seconds.
         int maxSessionTimeout = 691200; // 8 days in seconds.
 
-        try (MockedStatic<SessionContextCache> mockedSessionContextCache = mockStatic(SessionContextCache.class);
+        try (MockedStatic<SessionContextCache> sessionContextCacheMockedStatic = mockStatic(SessionContextCache.class);
              MockedStatic<IdPManagementUtil> mockedIdPManagementUtil = mockStatic(IdPManagementUtil.class)) {
 
             SessionContextCache sessionContextCacheInstance = mock(SessionContextCache.class);
-            mockedSessionContextCache.when(SessionContextCache::getInstance)
+            sessionContextCacheMockedStatic.when(SessionContextCache::getInstance)
                     .thenReturn(sessionContextCacheInstance);
 
             mockedIdPManagementUtil.when(() -> IdPManagementUtil.getRememberMeTimeout(tenantDomain))
@@ -1946,11 +1946,11 @@ public class FrameworkUtilsTest extends IdentityBaseTest {
 
         int idleSessionTimeout = 1800; // 30 minutes in seconds.
 
-        try (MockedStatic<SessionContextCache> mockedSessionContextCache = mockStatic(SessionContextCache.class);
+        try (MockedStatic<SessionContextCache> sessionContextCacheMockedStatic = mockStatic(SessionContextCache.class);
              MockedStatic<IdPManagementUtil> mockedIdPManagementUtil = mockStatic(IdPManagementUtil.class)) {
 
             SessionContextCache sessionContextCacheInstance = mock(SessionContextCache.class);
-            mockedSessionContextCache.when(SessionContextCache::getInstance)
+            sessionContextCacheMockedStatic.when(SessionContextCache::getInstance)
                     .thenReturn(sessionContextCacheInstance);
 
             mockedIdPManagementUtil.when(() -> IdPManagementUtil.getIdleSessionTimeOut(tenantDomain))
