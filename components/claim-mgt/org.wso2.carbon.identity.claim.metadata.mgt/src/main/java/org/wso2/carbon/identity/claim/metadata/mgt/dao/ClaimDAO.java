8845bb1b7681fa3886ab114d83f052a8cb7fdaed
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2026-03-16 17:19:30 +0530

    Fix Oracle DB failure when claim property value is empty string
    Oracle treats empty strings as NULL, which violates the NOT NULL
    constraint on the PROPERTY_VALUE column in IDN_CLAIM_PROPERTY.
    When addClaimProperties() persists a claim property with an empty
    string value on Oracle, it fails with a constraint violation error.
    
    The fix detects Oracle at the start of addClaimProperties() and skips
    any claim property whose value is empty, preventing the constraint
    violation.
    
    Fixes https://github.com/wso2-enterprise/wso2-iam-internal/issues/6691

--- components/claim-mgt/org.wso2.carbon.identity.claim.metadata.mgt/src/main/java/org/wso2/carbon/identity/claim/metadata/mgt/dao/ClaimDAO.java
+++ components/claim-mgt/org.wso2.carbon.identity.claim.metadata.mgt/src/main/java/org/wso2/carbon/identity/claim/metadata/mgt/dao/ClaimDAO.java
@@ -28,6 +28,7 @@ import org.wso2.carbon.identity.claim.metadata.mgt.exception.ClaimMetadataClient
 import org.wso2.carbon.identity.claim.metadata.mgt.exception.ClaimMetadataException;
 import org.wso2.carbon.identity.claim.metadata.mgt.model.Claim;
 import org.wso2.carbon.identity.claim.metadata.mgt.util.SQLConstants;
+import org.wso2.carbon.identity.core.util.IdentityCoreConstants;
 import org.wso2.carbon.identity.core.util.IdentityDatabaseUtil;
 import org.wso2.carbon.utils.DBUtils;
 
@@ -239,9 +240,16 @@ public class ClaimDAO {
         if (claimId > 0 && claimProperties != null) {
             String query = SQLConstants.ADD_CLAIM_PROPERTY;
             try (PreparedStatement prepStmt = connection.prepareStatement(query);) {
+                boolean isOracle = StringUtils.containsIgnoreCase(
+                        connection.getMetaData().getDatabaseProductName(), IdentityCoreConstants.ORACLE);
                 prepStmt.setInt(1, claimId);
                 prepStmt.setInt(4, tenantId);
                 for (Map.Entry<String, String> property : claimProperties.entrySet()) {
+                    // Skip properties with empty values on Oracle, as it treats empty strings as NULL
+                    // which violates the NOT NULL constraint on PROPERTY_VALUE.
+                    if (isOracle && StringUtils.isEmpty(property.getValue())) {
+                        continue;
+                    }
                     if (StringUtils.equals(property.getKey(), SUB_ATTRIBUTES_PROPERTY)) {
                         String[] subAttributes = property.getValue().split(" ");
                         int subAttributeIndex = 0;
